6960acbb1f87cebb39a3a162bc2a35f40bf124f1
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2007-09-15 14:37:36 +0000

    test

---
+++ TestApp/OptionsState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "BasisState.h"
+
+class OptionsState : public BasisState
+{
+public:
+
+	bool mouseMoved( const OIS::MouseEvent &arg );
+	bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+
+public:
+	void enter(bool bIsChangeState);
+	void exit();
+	void windowResize(); // уведомление об изменении размеров окна рендера
+
+};
