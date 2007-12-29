5305dae525684579a1bde3eaedcbf1eaa70b038c
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2007-12-29 23:09:59 +0000

    better comments for align added

--- MyGUIEngine/include/MyGUI_AlignInfo.h
+++ MyGUIEngine/include/MyGUI_AlignInfo.h
@@ -17,18 +17,18 @@ namespace MyGUI
 
 	enum ALIGN_INFO
 	{
-		ALIGN_NONE						= FLAG_NONE,					// 
+		ALIGN_NONE						= FLAG_NONE,					// do not use it in your application
 		ALIGN_HCENTER					= FLAG_NONE,					// center the window horizontally
 		ALIGN_VCENTER					= FLAG_NONE,					// center the window vertically
 		ALIGN_CENTER					= ALIGN_HCENTER | ALIGN_VCENTER,		// center the window in the dead center
-		ALIGN_CENTER_PARENT		    = FLAG(0),						// это скорее флаг, ставит выравнивание по центру отца, по умолчанию относительно текущего положения
-		ALIGN_LEFT						= FLAG(1),						// justify left
-		ALIGN_RIGHT					= FLAG(2),						// justify right
-		ALIGN_HSTRETCH				    = ALIGN_LEFT | ALIGN_RIGHT,			// stretch to fill the entire parent window horizontally (?)
-		ALIGN_TOP						= FLAG(3),						// align from the top
-		ALIGN_BOTTOM					= FLAG(4),						// align from the bottom
-		ALIGN_VSTRETCH				    = ALIGN_TOP | ALIGN_BOTTOM,			// stretch to fill the entire parent window vertically (?)
-		ALIGN_STRETCH					= ALIGN_HSTRETCH | ALIGN_VSTRETCH,	 // stretch to fill the entire parent (?)
+		ALIGN_CENTER_PARENT		    = FLAG(0),						// do not use it in your application это скорее флаг, ставит выравнивание по центру отца, по умолчанию относительно текущего положения
+		ALIGN_LEFT						= FLAG(1),						// align from the left (and center the window vertically)
+		ALIGN_RIGHT					= FLAG(2),						// align from the left (and center the window vertically)
+		ALIGN_HSTRETCH				    = ALIGN_LEFT | ALIGN_RIGHT,			// stretch horizontally proportionate to parent window
+		ALIGN_TOP						= FLAG(3),						// align from the top (and center the window horizontally)
+		ALIGN_BOTTOM					= FLAG(4),						// align from the bottom (and center the window horizontally)
+		ALIGN_VSTRETCH				    = ALIGN_TOP | ALIGN_BOTTOM,			// stretch vertically proportionate to parent window
+		ALIGN_STRETCH					= ALIGN_HSTRETCH | ALIGN_VSTRETCH,	 // stretch proportionate to parent window
 	};
 
 	#define IS_ALIGN_NONE(align) (ALIGN_NONE == (align & ALIGN_NONE))
