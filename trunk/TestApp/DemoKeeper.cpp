195396f1b81dec77c7bcdabd9ca5edfeafa9bb71
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-03-18 21:48:29 +0000

    bug fix add setSceneManager method

--- trunk/TestApp/DemoKeeper.cpp
+++ trunk/TestApp/DemoKeeper.cpp
@@ -49,7 +49,7 @@ void DemoKeeper::start(MyGUI::Gui * _gui, size_t _width, size_t _height)
     mHeight = _height;
 
 
-	MyGUI::PointerManager::getInstance().setPointer("hand", null);
+	//MyGUI::PointerManager::getInstance().setPointer("hand", null);
 
 	/*MyGUI::FooBar* bar = mGUI->createWidget<MyGUI::FooBar>("FooBar", MyGUI::IntCoord(100, 100, 500, 50), MyGUI::ALIGN_BOTTOM, "Main");
 	bar->setLayout(MyGUI::FooBar::FBL_SNAP_BOTTOM);
