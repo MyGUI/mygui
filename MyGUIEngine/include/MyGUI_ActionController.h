4e6b0bf0516832641fa26780bf129adb356f051f
my_name <mynameco@users.noreply.github.com>
2008-03-22 14:49:20 +0000

    add lost files and merge =)

---
+++ MyGUIEngine/include/MyGUI_ActionController.h
@@ -0,0 +1,26 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		01/2008
+	@module
+*/
+#ifndef __MYGUI_ACTION_CONTROLLER_H__
+#define __MYGUI_ACTION_CONTROLLER_H__
+
+#include "MyGUI_Prerequest.h"
+
+namespace MyGUI
+{
+
+	namespace action
+	{
+
+		void actionWidgetHide(WidgetPtr _widget);
+
+		void actionWidgetDestroy(WidgetPtr _widget);
+
+	} // namespace action
+
+} // namespace MyGUI
+
+#endif // __MYGUI_ACTION_CONTROLLER_H__
