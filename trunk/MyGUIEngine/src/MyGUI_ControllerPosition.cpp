4bd49b12902b8de31ea0f081be92e2df64b1058c
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-03-30 00:18:43 +0000

    Test ControllerPosition added.

---
+++ trunk/MyGUIEngine/src/MyGUI_ControllerPosition.cpp
@@ -0,0 +1,87 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		01/2008
+	@module
+*/
+#include "MyGUI_Prerequest.h"
+#include "MyGUI_ControllerPosition.h"
+#include "MyGUI_Gui.h"
+#include "MyGUI_InputManager.h"
+#include "MyGUI_WidgetManager.h"
+#include "MyGUI_Widget.h"
+
+namespace MyGUI
+{
+
+	ControllerPosition::ControllerPosition()
+	{
+	}
+
+	ControllerPosition::ControllerPosition(float _speed) :
+		mSpeed(_speed), mAngle(0), mDelta1(0), mDelta2(0)
+	{
+	}
+
+	const std::string & ControllerPosition::getType()
+	{
+		static std::string type("ControllerPositionController");
+		return type;
+	}
+
+	void ControllerPosition::prepareItem(WidgetPtr _widget)
+	{
+		MYGUI_DEBUG_ASSERT(mSpeed > 0, "coef must be > 0");
+
+		IntCoord rect = _widget->getCoord();
+		int x = rect.width;
+		int y = rect.height;
+		mRadius = sqrt(float(x*x + y*y));
+		mAngle = asin(x/mRadius);
+
+		// вызываем пользовательский делегат для подготовки
+		eventPreAction(_widget);
+	}
+
+	void ControllerPosition::replaseItem(WidgetPtr _widget, ControllerItem * _item)
+	{
+		ControllerPosition * item = static_cast<ControllerPosition*>(_item);
+	}
+
+	bool ControllerPosition::addTime(WidgetPtr _widget, float _time)
+	{
+		IntCoord rect = _widget->getCoord();
+
+		if (mRadius > 0)
+		{
+			mDelta1 += -mRadius*_time*sin(mAngle);
+			mDelta2 += mRadius*_time*cos(mAngle);
+			while ( mDelta1 > 1 ){
+				rect += IntCoord(mDelta1, 0, 0, -abs(mDelta1));
+				mDelta1 -= 1;
+			}
+			while ( mDelta1 < -1 ){
+				rect += IntCoord(mDelta1, 0, 0, -abs(mDelta1));
+				mDelta1 += 1;
+			}
+			while ( mDelta2 > 1 ){
+				rect += IntCoord(0, mDelta2, -abs(mDelta2), 0);
+				mDelta2 -= 1;
+			}
+			while ( mDelta2 < -1 ){
+				rect += IntCoord(0, mDelta2, -abs(mDelta2), 0);
+				mDelta2 += 1;
+			}
+			_widget->setPosition(rect);
+			mRadius -= _time * mSpeed;
+			mAngle += _time * mSpeed;
+			return true;
+		}
+
+		// вызываем пользовательский елегат пост обработки
+		eventPostAction(_widget);
+
+		return false;
+	}
+
+} // namespace MyGUI
