f577da704d8b0a6196a3f0fb98c20f7eac1a0574
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-04-28 13:25:49 +0000

    LayerManager::setSceneManager mirrored to Gui comments updated

--- trunk/MyGUIEngine/include/MyGUI_Edit.h
+++ trunk/MyGUIEngine/include/MyGUI_Edit.h
@@ -33,57 +33,82 @@ namespace MyGUI
 		//!	@copydoc Widget::getWidgetType()
 		virtual const Ogre::String & getWidgetType() { return _getType(); }
 
-		// работа с текстом с тегами
+		/** Set edit text applying tags */
 		virtual void setCaption(const Ogre::UTFString & _caption);
+		/** Get edit text with tags */
 		virtual const Ogre::UTFString& getCaption();
 
-		// работа с текстом без тегов
+		/** Set edit text without tags */
 		inline void setOnlyText(const Ogre::UTFString & _text) {setText(TextIterator::toTagsString(_text), false);}
+		/** Get edit text without tags */
 		inline Ogre::UTFString getOnlyText() {return TextIterator::getOnlyText(getRealString());}
 
 		// возвращает выделение
+		/** Get selected text interval
+			@param _start of interval will be written here
+			@param _end of interval will be written here
+		*/
 		void getTextSelect(size_t & _start, size_t & _end);
+		// устанавливает выделение
+		/** Set selected text interval
+			@param _start of interval
+			@param _end of interval
+		*/
+		void setTextSelect(size_t _start, size_t _end);
 		// выделен ли текст
+		/** Is any text selected */
 		inline bool isTextSelect() {return ( (mStartSelect != ITEM_NONE) && (mStartSelect != mEndSelect) );}
 		// возвращает выделенный текст
+		/** Get selected text */
 		Ogre::UTFString getSelectedText();
 
+		/** Get text length excluding tags
+			@example "#00FF00Hello" length is 5
+		*/
 		inline size_t getTextLength() {return mTextLength;}
+		/** Get text cursor position */
 		inline size_t getTextCursor() {return mCursorPosition;}
 
+		/** Set text cursor position */
 		void setTextCursor(size_t _index);
-		// устанавливает выделение
-		void setTextSelect(size_t _start, size_t _end);
 
+		/** Set text align */
 		virtual void setTextAlign(Align _align);
-
 
 		//---------------------------------------------------------------//
 		// наружу выставляем инлайны со сбросом истории
 		//---------------------------------------------------------------//
 
 		// возвращает текст с тегами
+		/** Get _count characters with tags from _start position */
 		Ogre::UTFString getText(size_t _start, size_t _count);
 		// удаляет все что выделенно
+		/** Delete selected text */
 		inline bool deleteTextSelect() {return deleteTextSelect(false);}
 		// вставляет текст в указанную позицию
+		/** Inser text at _index position (text end by default) */
 		inline void insertText(const Ogre::UTFString & _text, size_t _index = ITEM_NONE) {insertText(_text, _index, false);}
 		// добавляет текст в конец
-		inline void addText(const Ogre::UTFString & _text) {addText(_text, false);}
-		// удаляет текст
+		/** Add text */
+		inline void addText(const Ogre::UTFString & _text) {insertText(_text, ITEM_NONE, false);}
+		/** Erase _count characters from _start position */
 		inline void eraseText(size_t _start, size_t _count = 1) {eraseText(_start, _count, false);}
 		// выделяет цветом выделение
+		/** Highlight selected text by _colour */
 		inline void setTextSelectColour(const Ogre::ColourValue & _colour)
 		{
 			setTextSelectColour(_colour, false);
 		}
 		// выделяет цветом диапазон
+		/** Highlight interval by _colour */
 		inline void setTextColour(size_t _start, size_t _count, const Ogre::ColourValue & _colour)
 		{
 			setTextColour(_start, _count, _colour, false);
 		}
 
+		/** Get edit read only mode flag */
 		inline bool getEditReadOnly() {return mModeReadOnly;}
+		/** Enable or disable edit read only mode */
 		inline void setEditReadOnly(bool _read)
 		{
 			mModeReadOnly = _read;
@@ -91,10 +116,14 @@ namespace MyGUI
 			commandResetHistory();
 		}
 
+		/** Get edit password mode flag */
 		inline bool getEditPassword() {return mModePassword;}
+		/** Enable or disable edit password mode */
 		void setEditPassword(bool _password);
 
+		/** Get edit multiline mode flag */
 		inline bool getEditMultiLine() {return mModeMultiline;}
+		/** Enable or disable edit multiline mode */
 		inline void setEditMultiLine(bool _multi)
 		{
 			mModeMultiline = _multi;
@@ -108,6 +137,7 @@ namespace MyGUI
 			commandResetHistory();
 		}
 
+		/** Enable or disable edit static mode */
 		inline void setEditStatic(bool _static)
 		{
 			mModeStatic = _static;
@@ -116,16 +146,17 @@ namespace MyGUI
 			else mWidgetUpper->setPointer(mOriginalPointer);
 		}
 
-		inline bool getEditStatic()
-		{
-			return mModeStatic;
-		}
+		/** Get edit static mode flag */
+		inline bool getEditStatic() {return mModeStatic;}
 
+		/** Set edit password character ('*' by default) */
 		void setPasswordChar(Char _char);
+		/** Set edit password character ('*' by default) */
 		inline void setPasswordChar(const Ogre::UTFString & _char)
 		{
 			if (false == _char.empty()) setPasswordChar(_char[0]);
 		}
+		/** Get edit password character */
 		inline Char getPasswordChar() {return mCharPassword;}
 
 		void setPosition(const IntCoord& _coord);
@@ -135,12 +166,14 @@ namespace MyGUI
 		inline void setPosition(int _left, int _top, int _width, int _height) {setPosition(IntCoord(_left, _top, _width, _height));}
 		inline void setSize(int _width, int _height) {setSize(IntSize(_width, _height));}
 
-		// event : нажат ентер, или сонтрол энтер в мультилайн
-		// signature : void method(MyGUI::WidgetPtr _sender)
+		/** Event : Enter pressed (Ctrl+entre in multiline mode).\n
+			signature : void method(MyGUI::WidgetPtr _sender)
+		*/
 		EventInfo_WidgetVoid eventEditSelectAccept;
 
-		// event : изменился текст
-		// signature : void method(MyGUI::WidgetPtr _sender)
+		/** Event : Text changed.\n
+			signature : void method(MyGUI::WidgetPtr _sender)
+		*/
 		EventInfo_WidgetVoid eventEditTextChange;
 
 	protected:
@@ -151,8 +184,6 @@ namespace MyGUI
 		bool deleteTextSelect(bool _history);
 		// вставляет текст в указанную позицию
 		void insertText(const Ogre::UTFString & _text, size_t _index, bool _history);
-		// добавляет текст в конец
-		inline void addText(const Ogre::UTFString & _text, bool _history) {insertText(_text, ITEM_NONE, _history);}
 		// удаляет текст
 		void eraseText(size_t _start, size_t _count, bool _history);
 		// выделяет цветом выделение
