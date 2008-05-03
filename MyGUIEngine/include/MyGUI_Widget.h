f2c8c93c8208edf072adab7166157ceadb93f788
George Evmenov <Altren@users.noreply.github.com>
2008-05-03 11:43:54 +0000

    RenderBox background transparency support added. Widget, Window: Documentation update (now all widgets documented)

--- MyGUIEngine/include/MyGUI_Widget.h
+++ MyGUIEngine/include/MyGUI_Widget.h
@@ -51,34 +51,48 @@ namespace MyGUI
 	public:
 
 		// методы и шаблоны дл€ создани€ виджета
+		/** Create child widget
+			@param _type widget type
+			@param _skin widget skin
+			@param _coord int coordinates of widget (_left, _top, _width, _height)
+			@param _align widget align (possible values can be found in enum ALIGN_INFO)
+			@param _name if needed (you can use it for finding widget by name later)
+		*/
 		inline WidgetPtr createWidgetT(const Ogre::String & _type, const Ogre::String & _skin, const IntCoord& _coord, Align _align, const Ogre::String & _name = "")
 		{
 			return _createWidget(_type, _skin, _coord, _align, "", _name);
 		}
+		/** See Gui::createWidgetT */
 		inline WidgetPtr createWidgetT(const Ogre::String & _type, const Ogre::String & _skin, int _left, int _top, int _width, int _height, Align _align, const Ogre::String & _name = "")
 		{
 			return createWidgetT(_type, _skin, IntCoord(_left, _top, _width, _height), _align, _name);
 		}
 
+		/** Create widget using coordinates relative to parent. see Gui::createWidgetT */
 		WidgetPtr createWidgetRealT(const Ogre::String & _type, const Ogre::String & _skin, const FloatCoord& _coord, Align _align, const Ogre::String & _name = "");
+		/** Create widget using coordinates relative to parent. see Gui::createWidgetT */
 		inline WidgetPtr createWidgetRealT(const Ogre::String & _type, const Ogre::String & _skin, float _left, float _top, float _width, float _height, Align _align, const Ogre::String & _name = "")
 		{
 			return createWidgetRealT(_type, _skin, FloatCoord(_left, _top, _width, _height), _align, _name);
 		}
 
-		// шаблоны дл€ создани€ виджета по типу
+		// templates for creating widgets by type
+		/** Same as Widget::createWidgetT but return T* instead of WidgetPtr */
 		template <class T> inline T* createWidget(const Ogre::String & _skin, const IntCoord& _coord, Align _align, const Ogre::String & _name = "")
 		{
 			return static_cast<T*>(createWidgetT(T::_getType(), _skin, _coord, _align, _name));
 		}
+		/** Same as Widget::createWidgetT but return T* instead of WidgetPtr */
 		template <class T> inline T* createWidget(const Ogre::String & _skin, int _left, int _top, int _width, int _height, Align _align, const Ogre::String & _name = "")
 		{
 			return static_cast<T*>(createWidgetT(T::_getType(), _skin, IntCoord(_left, _top, _width, _height), _align, _name));
 		}
+		/** Same as Widget::createWidgetRealT but return T* instead of WidgetPtr */
 		template <class T> inline T* createWidgetReal(const Ogre::String & _skin, const FloatCoord& _coord, Align _align, const Ogre::String & _name = "")
 		{
 			return static_cast<T*>(createWidgetRealT(T::_getType(), _skin, _coord, _align, _name));
 		}
+		/** Same as Widget::createWidgetRealT but return T* instead of WidgetPtr */
 		template <class T> inline T* createWidgetReal(const Ogre::String & _skin, float _left, float _top, float _width, float _height, Align _align, const Ogre::String & _name = "")
 		{
 			return static_cast<T*>(createWidgetRealT(T::_getType(), _skin, _left, _top, _width, _height, _align, _name));
@@ -91,17 +105,25 @@ namespace MyGUI
 		//! Get widget type
 		virtual const Ogre::String & getWidgetType() { return _getType(); }
 
+		/** Set widget position (position of left top corner)*/
 		virtual void setPosition(const IntPoint& _pos);
+		/** Set widget position and size*/
 		virtual void setPosition(const IntCoord& _coord);
+		/** Set widget size */
 		virtual void setSize(const IntSize& _size);
 
+		/** See Widget::setPosition(const IntPoint& _pos) */
 		inline void setPosition(int _left, int _top) {setPosition(IntPoint(_left, _top));}
+		/** See Widget::setPosition(const IntCoord& _coord) */
 		inline void setPosition(int _left, int _top, int _width, int _height) {setPosition(IntCoord(_left, _top, _width, _height));}
+		/** See Widget::setSize(const IntSize& _size) */
 		inline void setSize(int _width, int _height) {setSize(IntSize(_width, _height));}
 
 		void _updateAbsolutePoint();
 
+		/** Show widget */
 		virtual void show();
+		/** Hide widget */
 		virtual void hide();
 
 		// дл€ внутреннего использовани€
@@ -109,28 +131,44 @@ namespace MyGUI
 		void _inheritedHide();
 		inline bool _isInheritedShow() {return mInheritedShow;}
 
+		/** Set widget caption */
 		virtual void setCaption(const Ogre::UTFString & _caption);
+		/** Get widget caption */
 		virtual const Ogre::UTFString & getCaption();
 
+		/** Set widget text colour */
 		virtual void setColour(const Ogre::ColourValue & _colour);
+		/** Get widget text colour */
 		virtual const Ogre::ColourValue & getColour();
 
+		/** Set widget text font */
 		virtual void setFontName(const Ogre::String & _font);
+		/** Get widget text font name */
 		virtual const Ogre::String & getFontName();
 
+		/** Set widget text font height */
 		virtual void setFontHeight(uint16 _height);
+		/** Get widget text font height */
 		virtual Ogre::ushort getFontHeight();
 
+		/** Set widget text align */
 		virtual void setTextAlign(Align _align);
+		/** Get widget text align */
 		virtual Align getTextAlign();
 
+		/** Get text coordinates */
 		IntCoord getTextCoord();
+		/** Get text size in pixels */
 		IntSize getTextSize();
 
+		/** Set widget opacity */
 		void setAlpha(float _alpha);
+		/** Get widget opacity */
 		inline float getAlpha() {return mAlpha;};
 		inline float _getRealAlpha() {return mRealAlpha;}
+		/** Get inherits alpha mode flag */
 		inline bool isInheritsAlpha() {return mInheritsAlpha;}
+		/** Enable or disable inherits alpha mode */
 		inline void setInheritsAlpha(bool _inherits)
 		{
 			mInheritsAlpha = _inherits;
@@ -138,6 +176,7 @@ namespace MyGUI
 		}
 		void _updateAlpha();
 
+		/** Set widget's state */
 		void setState(const Ogre::String & _state);
 		void _setUVSet(const FloatRect& _rect);
 
@@ -145,16 +184,18 @@ namespace MyGUI
 		virtual const Ogre::String& _getTextureName();
 
 		// €вл€емс€ ли мы рутовым виджетом
+		/** Is this widget is root widget (root == without parents) */
 		inline bool isRootWidget() {return null == mParent;}
 
 		// закрываем метод базового класса
+		/** Get parent widget */
 		inline WidgetPtr getParent() {return static_cast<WidgetPtr>(mParent);}
 
 		// дл€ поддержки окон напр€мую не €вл€ющиес€ детьми
 		inline WidgetPtr _getOwner() {return mOwner;}
 		inline void _setOwner(WidgetPtr _widget) { if (isRootWidget()) mOwner = _widget; }
 
-		// return copy
+		/** Get copy of child widgets vector */
 		virtual VectorWidgetPtr getChilds();
 
 		// наследуемс€ он LayerInfo
@@ -162,18 +203,24 @@ namespace MyGUI
 		virtual void _attachToLayerItemKeeper(LayerItemKeeper * _item);
 		virtual void _detachFromLayerItemKeeper();
 
+		/** Is need key focus */
 		inline bool isNeedKeyFocus() {return mNeedKeyFocus;}
+		/** Set need key focus flag */
 		inline void setNeedKeyFocus(bool _need) {mNeedKeyFocus = _need;}
+		/** Is need mouse focus */
 		inline bool isNeedMouseFocus() {return mNeedMouseFocus;}
+		/** Set need mouse focus flag */
 		inline void setNeedMouseFocus(bool _need) {mNeedMouseFocus = _need;}
 
+		/** Is widget enabled */
 		inline bool isEnabled() {return mEnabled;}
+		/** Enable or disable widget */
 		void setEnabled(bool _enabled, bool _silent = false);
 
-		// возвращает размер клиентской зоны либо всего окна
+		/** Get rect where child widgets placed */
 		virtual const IntCoord& getClientRect();
 
-		// курсор дл€ этого виджета
+		/** Get mouse pointer name for this widget */
 		inline const std::string& getPointer()
 		{
 			if (false == mEnabled) {
@@ -183,6 +230,7 @@ namespace MyGUI
 			return mPointer;
 		}
 
+		/** Set mouse pointer for this widget */
 		inline void setPointer(const std::string& _pointer)
 		{
 			mPointer = _pointer;
@@ -191,8 +239,7 @@ namespace MyGUI
 		// дает приоритет виджету при пиккинге
 		void _forcePeek(WidgetPtr _widget);
 
-		// возвращает им€ леера, к которому приаттачен виджет
-		// актуально только дл€ рутового виджета
+		/** Get widget's layer, return "" if widget is not root widget (root == without parents) */
 		const std::string& getLayerName();
 
 		inline WidgetCreator * _getWidgetCreator()
@@ -205,7 +252,9 @@ namespace MyGUI
 		// метод дл€ установлени€ стейта айтема
 		virtual void _setDragItemInfo(size_t _index, bool _set, bool _accept);
 
+      /** Set drag'n'drop mode flag */
 		inline void setNeedDragDrop(bool _need) {mNeedDragDrop = _need;}
+      /** Get drag'n'drop mode flag */
 		inline bool getNeedDragDrop() {return mNeedDragDrop;}
 
 	protected:
