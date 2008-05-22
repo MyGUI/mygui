0c2368f1451189c4efa91573d719fd4ecf34df11
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-05-22 11:44:42 +0000

    update font manager

--- trunk/MyGUIEngine/src/MyGUI_FontManager.cpp
+++ trunk/MyGUIEngine/src/MyGUI_FontManager.cpp
@@ -9,6 +9,8 @@
 #include "MyGUI_FontManager.h"
 #include "MyGUI_Common.h"
 #include "MyGUI_XmlDocument.h"
+#include <OgreImageCodec.h>
+#include <OgreFont.h>
 
 namespace MyGUI
 {
@@ -95,6 +97,70 @@ namespace MyGUI
         const Ogre::NameValuePairList* params)
 	{
 		return new Font(this, name, handle, group, isManual, loader);
+	}
+
+	void FontManager::saveFontTexture(const std::string & _font, const std::string & _file)
+	{
+		FontPtr font = FontManager::getInstance().getByName( _font );
+		if (font.isNull()) MYGUI_EXCEPT("Could not find font " << _font);
+		font->load();
+		Ogre::TexturePtr texture = font->getTextureFont();
+
+		Ogre::uchar * buff = (Ogre::uchar *)texture->getBuffer()->lock(0, texture->getWidth() * texture->getHeight() * 2, Ogre::HardwareBuffer::HBL_READ_ONLY);
+
+		Ogre::ImageCodec::ImageData *imgData = new Ogre::ImageCodec::ImageData();
+		imgData->width = texture->getWidth();
+		imgData->height = texture->getHeight();
+		imgData->depth = 1;
+		imgData->format = Ogre::PF_A8R8G8B8;
+
+		// Allocate buffer 
+		Ogre::uchar* pBuffer = new Ogre::uchar[texture->getWidth() * texture->getHeight() * 4];
+
+		for (size_t y=0; y < texture->getHeight(); ++y) {
+			for (size_t x=0; x < texture->getWidth(); ++x) {
+				*pBuffer = *buff;
+				pBuffer ++;
+				*pBuffer = *buff;
+				pBuffer ++;
+				*pBuffer = *buff;
+				pBuffer ++;
+				buff ++;
+
+				*pBuffer = *buff;
+				pBuffer ++;
+				buff ++;
+			}
+		}
+
+		//texture->getBuffer()->unlock();
+
+		// Wrap buffer in a memory stream
+		Ogre::DataStreamPtr stream(new Ogre::MemoryDataStream(pBuffer, texture->getWidth() * texture->getHeight() * 4, false));
+
+		// Need to flip the read data over in Y though
+		//Ogre::Image img;
+		//img.loadRawData(stream, texture->getWidth(), texture->getHeight(), imgData->format );
+		//img.flipAroundX();
+
+		Ogre::MemoryDataStreamPtr streamFlipped(new Ogre::MemoryDataStream(pBuffer, stream->size(), false));
+
+		// Get codec 
+		size_t pos = _file.find_last_of(".");
+		Ogre::String extension;
+		MYGUI_ASSERT(pos != Ogre::String::npos, "Unable to determine image type for '" << _file << "' - invalid extension.");
+
+		while( pos != _file.length() - 1 )
+			extension += _file[++pos];
+
+		// Get the codec
+		Ogre::Codec * pCodec = Ogre::Codec::getCodec(extension);
+
+		// Write out
+		Ogre::Codec::CodecDataPtr ptr(imgData);
+		pCodec->codeToFile(streamFlipped, _file, ptr);
+
+		delete [] pBuffer;
 	}
 
 } // namespace MyGUI
