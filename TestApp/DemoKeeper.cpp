a930d419155831c131d807e60d0b1688aacab1cc
George Evmenov <Altren@users.noreply.github.com>
2008-06-25 14:55:23 +0000

    Edit: fixed select word on doubleclick Multilist: void sortByColumn(_column, _backward); implemented

--- TestApp/DemoKeeper.cpp
+++ TestApp/DemoKeeper.cpp
@@ -21,13 +21,25 @@ void DemoKeeper::start(MyGUI::Gui * _gui, size_t _width, size_t _height)
 
 	//MyGUI::FontManager::getInstance().saveFontTexture("MyGUI_CoreFont.18", "MyGUI_CoreFont.18.png");
 
-	MyGUI::EditPtr edit = mGUI->createWidget<MyGUI::Edit>("EditStretch", MyGUI::IntCoord(100, 100, 340, 26), MyGUI::ALIGN_DEFAULT, "Main");
+	/*MyGUI::EditPtr edit = mGUI->createWidget<MyGUI::Edit>("EditStretch", MyGUI::IntCoord(100, 100, 340, 26), MyGUI::ALIGN_DEFAULT, "Main");
 	edit->setTextAlign(MyGUI::ALIGN_HCENTER | MyGUI::ALIGN_VCENTER);
 	edit->setCaption("ComplexProgress");//*/
 
 	/*MyGUI::ButtonPtr button = mGUI->createWidget<MyGUI::Button>("ButtonSmall", MyGUI::IntCoord(100, 140, 100, 26), MyGUI::ALIGN_DEFAULT, "Main");
 	button->setTextAlign(MyGUI::ALIGN_HCENTER | MyGUI::ALIGN_VCENTER);
 	button->setCaption("ComplexProgress");//*/
+
+   MyGUI::MultiListPtr mlist = mGUI->createWidget<MyGUI::MultiList>("MultiList", MyGUI::IntCoord(100, 100, 340, 340), MyGUI::ALIGN_DEFAULT, "Main");
+   mlist->addColumn(100, "columnA");
+	mlist->addColumn(100, "columnB");
+	mlist->addItem("1");
+	mlist->addItem("3");
+	mlist->addItem("2");
+	mlist->addItem("4");
+   mlist->setSubItem(1,0,"d");
+	mlist->setSubItem(1,1,"b");
+	mlist->setSubItem(1,2,"a");
+	mlist->setSubItem(1,3,"c");
 
 }
 
