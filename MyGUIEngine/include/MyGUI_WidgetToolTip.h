7fc53585a3baf8f8d5ee9ce0e6cae1f6103e8577
my_name <mynameco@users.noreply.github.com>
2008-07-17 12:39:20 +0000

    fogot

---
+++ MyGUIEngine/include/MyGUI_WidgetToolTip.h
@@ -0,0 +1,35 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2008
+	@module
+*/
+#ifndef __MYGUI_WIDGET_TOOLTIP_H__
+#define __MYGUI_WIDGET_TOOLTIP_H__
+
+#include "MyGUI_Prerequest.h"
+#include "MyGUI_Types.h"
+#include "MyGUI_WidgetToolTip.h"
+
+namespace MyGUI
+{
+
+	enum ToolTipType
+	{
+		TOOLTIP_SHOW,
+		TOOLTIP_HIDE
+	};
+
+	struct ToolTipInfo
+	{
+		ToolTipInfo(ToolTipType _type, size_t _index, IntPoint _point) :
+			type(_type), index(_index), point(_point) { }
+
+		ToolTipType type;
+		size_t index;
+		IntPoint point;
+	};
+
+} // namespace MyGUI
+
+#endif //__MYGUI_WIDGET_TOOLTIP_H__
