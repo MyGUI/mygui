686acd1f5b1a5c62dc15f8a7e8207e84405edf1c
my_name <mynameco@users.noreply.github.com>
2008-07-20 23:41:34 +0000

    add

---
+++ Demo_ItemBox/ItemBoxH.h
@@ -0,0 +1,19 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2008
+	@module
+*/
+#ifndef __ITEM_BOXH_H__
+#define __ITEM_BOXH_H__
+
+#include <MyGUI.h>
+#include "ItemBoxV.h"
+
+class ItemBoxH : public ItemBoxV
+{
+public:
+	ItemBoxH();
+};
+
+#endif // __ITEM_BOXH_H__
