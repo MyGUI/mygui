af55982c9c1aaeb59ff53e2d5784f7eef549108e
my_name <my.name.post@gmail.com>
2008-08-18 02:15:59 +0000

    relocate

---
+++ Demos/Demo_ScrollView/DemoKeeper.h
@@ -0,0 +1,20 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		05/2008
+	@module
+*/
+#ifndef __DEMO_KEEPER_H__
+#define __DEMO_KEEPER_H__
+
+#include <MyGUI.h>
+
+class DemoKeeper
+{
+
+public:
+	void start();
+	void end();
+};
+
+#endif // __DEMO_KEEPER_H__
