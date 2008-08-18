af7e0e38541e5d5b587c8829756c83558cf157b3
my_name <my.name.post@gmail.com>
2008-08-18 17:05:56 +0000

    merge

--- Demos/Demo_PluginStrangeButton/DemoKeeper.cpp
+++ Demos/Demo_PluginStrangeButton/DemoKeeper.cpp
@@ -11,18 +11,29 @@ void DemoKeeper::start()
 {
 	using namespace MyGUI;
 	const IntSize & view = Gui::getInstance().getViewSize();
-	const IntSize size(600, 300);
+	const IntSize size(300, 26);
 
-	WindowPtr window = Gui::getInstance().createWidget<Window>("WindowCS", IntCoord((view.width - size.width) / 2, (view.height - size.height) / 2, size.width, size.height), ALIGN_DEFAULT, "Main");
-	window->setMinSize(200, 100);
-	window->setCaption("ScrollView demo");
-	ScrollViewPtr scroll_view = window->createWidget<ScrollView>("ScrollView", IntCoord(2, 2, window->getClientCoord().width-2, window->getClientCoord().height-2), ALIGN_STRETCH);
+	// если группа ресурсов не указанна, то ищется в корне приложения
+#ifdef _DEBUG
+	PluginManager::getInstance().loadPlugin("Plugin_StrangeButton_d.dll");
+#else
+	PluginManager::getInstance().loadPlugin("Plugin_StrangeButton.dll");
+#endif
 
-	scroll_view->setCanvasSize(512, 256);
-	StaticImagePtr image = scroll_view->createWidget<StaticImage>("StaticImage", IntCoord(0, 0, 512, 256), ALIGN_DEFAULT);
-	image->setImageTexture("Ogre.png");
+	ButtonPtr button = castWidget<Button>(Gui::getInstance().createWidgetT("StrangeButton", "Button", IntCoord((view.width - size.width) / 2, (view.height - size.height) / 2, size.width, size.height), ALIGN_DEFAULT, "Main"));
+	button->setCaption("Plugin StrangeButton demo");
+
 }
 
 void DemoKeeper::end()
 {
+	using namespace MyGUI;
+
+	Gui::getInstance().destroyAllChildWidget();
+
+#ifdef _DEBUG
+	PluginManager::getInstance().unloadPlugin("Plugin_StrangeButton_d.dll");
+#else
+	PluginManager::getInstance().unloadPlugin("Plugin_StrangeButton.dll");
+#endif
 }
