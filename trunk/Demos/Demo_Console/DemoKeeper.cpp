813037eb0924d40fe79a8dcaf986f71c42528fed
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-08-18 18:35:53 +0000

    update Demo_Console

--- trunk/Demos/Demo_Console/DemoKeeper.cpp
+++ trunk/Demos/Demo_Console/DemoKeeper.cpp
@@ -9,20 +9,56 @@
 
 void DemoKeeper::start()
 {
-	/*using namespace MyGUI;
-	const IntSize & view = Gui::getInstance().getViewSize();
-	const IntSize size(600, 300);
+	// загружаем ресурсы для демо
+	// потом сделать и для мака
+	Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../../Media/Demos/Demo_Console", "FileSystem", "General");
 
-	WindowPtr window = Gui::getInstance().createWidget<Window>("WindowCS", IntCoord((view.width - size.width) / 2, (view.height - size.height) / 2, size.width, size.height), ALIGN_DEFAULT, "Main");
-	window->setMinSize(200, 100);
-	window->setCaption("ScrollView demo");
-	ScrollViewPtr scroll_view = window->createWidget<ScrollView>("ScrollView", IntCoord(2, 2, window->getClientCoord().width-2, window->getClientCoord().height-2), ALIGN_STRETCH);
+	mConsole.initialise();
 
-	scroll_view->setCanvasSize(512, 256);
-	StaticImagePtr image = scroll_view->createWidget<StaticImage>("StaticImage", IntCoord(0, 0, 512, 256), ALIGN_DEFAULT);
-	image->setImageTexture("Ogre.png");*/
+	mConsole.registerConsoleDelegate("colour", MyGUI::newDelegate(this, &DemoKeeper::command), "colour red green blue alpha");
+	mConsole.registerConsoleDelegate("show", MyGUI::newDelegate(this, &DemoKeeper::command), "show true | false");
 }
 
 void DemoKeeper::end()
 {
+	mConsole.shutdown();
+}
+
+void DemoKeeper::pressConsoleKey()
+{
+	mConsole.isShow() ? mConsole.hide() : mConsole.show();
+}
+
+void DemoKeeper::command(const Ogre::UTFString & _key, const Ogre::UTFString & _value)
+{
+	static Ogre::ColourValue colour;
+	static bool show = false;
+
+	if (_key == "colour") {
+		if (_value.empty()) mConsole.addToConsole(mConsole.getConsoleStringCurrent(), _key, MyGUI::utility::toString(colour));
+		else {
+			if ( ! MyGUI::utility::parseComplex(_value, colour.r, colour.g, colour.b, colour.a)) {
+				mConsole.addToConsole(mConsole.getConsoleStringError(), _key, _value);
+				mConsole.printCommandFormat(_key);
+			}
+			else {
+				mConsole.addToConsole(mConsole.getConsoleStringSuccess(), _key, _value);
+			}
+		}
+	}
+	else if (_key == "show") {
+		if (_value.empty()) {
+			mConsole.addToConsole(mConsole.getConsoleStringCurrent(), _key, MyGUI::utility::toString(show));
+		}
+		else {
+			if ( ! MyGUI::utility::parseComplex(_value, show)) {
+				mConsole.addToConsole(mConsole.getConsoleStringError(), _key, _value);
+				mConsole.printCommandFormat(_key);
+			}
+			else {
+				mConsole.addToConsole(mConsole.getConsoleStringSuccess(), _key, _value);
+			}
+		}
+	}
+
 }
