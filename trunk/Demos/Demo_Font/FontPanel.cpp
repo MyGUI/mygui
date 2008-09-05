fc463e61a658671ccf527910eeef605395e51f6d
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-09-05 15:29:09 +0000

    add demo font

---
+++ trunk/Demos/Demo_Font/FontPanel.cpp
@@ -0,0 +1,49 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		08/2008
+	@module
+*/
+
+#include "FontPanel.h"
+
+FontPanel::FontPanel() :
+	BaseLayout("FontPanel.layout")
+{
+}
+
+void FontPanel::initialise()
+{
+	loadLayout();
+
+	/*assignWidget(mListHistory, "list_History");
+	assignWidget(mComboCommand, "combo_Command");
+	assignWidget(mButtonSubmit, "button_Submit");
+
+	if (mMainWidget->getWidgetType() == "Window") {
+		static_cast<MyGUI::WindowPtr>(mMainWidget)->eventWindowButtonPressed = newDelegate(this, &Console::notifyWindowButtonPressed);
+	}
+
+	mStringCurrent = mMainWidget->getUserString("Current");
+	mStringError = mMainWidget->getUserString("Error");
+	mStringSuccess = mMainWidget->getUserString("Success");
+	mStringUnknow = mMainWidget->getUserString("Unknow");
+	mStringFormat = mMainWidget->getUserString("Format");
+
+	mAutocomleted = false;
+
+	mComboCommand->eventComboAccept = newDelegate(this, &Console::notifyComboAccept);
+	mComboCommand->eventEditTextChange = newDelegate(this, &Console::notifyCommandPrint);
+	mComboCommand->eventKeyButtonPressed = newDelegate(this, &Console::notifyButtonPressed);
+	mButtonSubmit->eventMouseButtonClick = newDelegate(this, &Console::notifyMouseButtonClick);*/
+}
+
+void FontPanel::show()
+{
+	mMainWidget->show();
+}
+
+void FontPanel::hide()
+{
+	mMainWidget->hide();
+}
