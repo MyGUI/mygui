740cddf2ded7886f51e912cc68efbc97727530df
my_name <mynameco@users.noreply.github.com>
2008-09-28 13:37:20 +0000

    fogot

---
+++ MyGUIEngine/src/MyGUI_IResource.cpp
@@ -0,0 +1,14 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		09/2008
+	@module
+*/
+#include "MyGUI_IResource.h"
+
+namespace MyGUI
+{
+
+	MYGUI_RTTI_BASE_IMPLEMENT( IResource );
+
+} // namespace MyGUI
