6a4d401340d783786157d9825e62f3a53341fc45
my_name <my.name.post@gmail.com>
2008-09-28 14:21:37 +0000

    update

--- MyGUIEngine/include/MyGUI_ISubWidgetText.h
+++ MyGUIEngine/include/MyGUI_ISubWidgetText.h
@@ -20,7 +20,7 @@ namespace MyGUI
 	public:
 		ISubWidgetText(const IntCoord& _coord, Align _align, ICroppedRectangle * _parent) :
 			ISubWidget(_coord, _align, _parent) { }
-			virtual ~ISubWidgetText() { }
+		virtual ~ISubWidgetText() { }
 
 		virtual bool firstQueue() { return false; }
 
