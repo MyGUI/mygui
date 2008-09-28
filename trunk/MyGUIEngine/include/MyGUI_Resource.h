c6d91c832b9ce4327b43c2c3e3d8961af7c2c832
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-09-28 12:34:35 +0000

    tmp

--- trunk/MyGUIEngine/include/MyGUI_Resource.h
+++ trunk/MyGUIEngine/include/MyGUI_Resource.h
@@ -4,35 +4,32 @@
 	@date		09/2008
 	@module
 */
-#ifndef __MYGUI_RESOURCE_H__
-#define __MYGUI_RESOURCE_H__
+#ifndef __MYGUI_I_RESOURCE_H__
+#define __MYGUI_I_RESOURCE_H__
 
 #include "MyGUI_Prerequest.h"
 
 namespace MyGUI
 {
 
-	class Resource;
-	typedef Resource * ResourcePtr;
+	class IResource;
+	typedef IResource * IResourcePtr;
 
 	class ResourceManager;
 
-	class _MyGUIExport Resource
+	class _MyGUIExport IResource
 	{
 		// для удаления
 		friend class ResourceManager;
 
+		MYGUI_RTTI_BASE_HEADER;
+
 	protected:
-		Resource() { }
-		virtual ~Resource() { }
+		IResource() { }
+		virtual ~IResource() { }
 
-	public:
-		virtual const std::string & getTypeName() = 0;
-
-		template <typename T>
-		inline T * castType() { return static_cast<T*>(this); }
 	};
 
 } // namespace MyGUI
 
-#endif // __MYGUI_RESOURCE_H__
+#endif // __MYGUI_I_RESOURCE_H__
