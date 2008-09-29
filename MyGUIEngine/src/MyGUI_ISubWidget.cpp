d606fcbc0b0d827b4fc6270a6661b86b9621e25a
my_name <mynameco@users.noreply.github.com>
2008-09-29 06:49:01 +0000

    refactoring

--- MyGUIEngine/src/MyGUI_ISubWidget.cpp
+++ MyGUIEngine/src/MyGUI_ISubWidget.cpp
@@ -12,7 +12,12 @@ namespace MyGUI
 {
 
 	MYGUI_RTTI_BASE_IMPLEMENT( ISubWidget );
+	ISubWidget::~ISubWidget() { }
+
 	MYGUI_RTTI_CHILD_IMPLEMENT( ISubWidgetRect, ISubWidget );
+	ISubWidgetRect::~ISubWidgetRect() { }
+
 	MYGUI_RTTI_CHILD_IMPLEMENT( ISubWidgetText, ISubWidget );
+	ISubWidgetText::~ISubWidgetText() { }
 
 } // namespace MyGUI
