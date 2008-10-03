23a19cee4cd7b59edcc31751f6c0e4b569150454
my_name <my.name.post@gmail.com>
2008-10-03 16:51:32 +0000

    update render out

--- MyGUIEngine/src/MyGUI_RenderOut.cpp
+++ MyGUIEngine/src/MyGUI_RenderOut.cpp
@@ -8,46 +8,22 @@
 #include "MyGUI_RenderOut.h"
 #include "MyGUI_Utility.h"
 
-#include <OgreFontManager.h>
-#include <OgreOverlayManager.h>
-#include <OgreRoot.h>
-#include <OgreOverlayContainer.h>
-#include <OgreRenderWindow.h>
+#include "MyGUI_Gui.h"
+#include "MyGUI_FontManager.h"
+#include "MyGUI_LayerManager.h"
+#include "MyGUI_SkinManager.h"
+#include "MyGUI_Widget.h"
 
 namespace MyGUI
 {
 	namespace implement
 	{
 
-		const size_t __DEBUG_COUNT_LINE = 30;
-		// показывать ли тень текста
-		const bool __DEBUG_SHOW_SHADOW = 1;
-		// шрифт по умолчанию
-		const char * __DEBUG_FONT_DEFAULT = "BlueHighway";
-		// размер шрифта
-		const float __DEBUG_FONT_SIZE = 16;
-		// цвет шрифта
-		const char * __DEBUG_FONT_COLOUR = "1.0 1.0 1.0";
-		// цвет шрифта
-		const char * __DEBUG_FONT_SHADOW_COLOUR = "0.0 0.0 0.0";
-		// высота оверлея отладки
-		const unsigned short __DEBUG_OVERLAY_ZORDER = 649;
-		// имя оверлея отладки
-		const char * __DEBUG_OVERLAY_NAME = "__debugOverlay";
-		// имя елемента
-		const char * __DEBUG_OVERLAY_ELEMENT_NAME = "__debugOverlayElement";
-		// имя елемента тени
-		const char *__DEBUG_OVERLAY_ELEMENT_SHADOW_NAME = "__debugOverlayElementShadow";
-		// смещение от края
-		const float __DEBUG_OVERLAY_OFFSET = 10;
-		// ширина поля вывода
-		const float __DEBUG_OVERLAY_INFO_WIDTH = 500;
-
 		// структура информации об одной строке
 		struct info
 		{
-			info() : num(0), count(1)  {}
-			info(size_t _num, const std::string & _line) : num(_num), count(1), line(_line) {}
+			info() : num(0), count(1)  { }
+			info(size_t _num, const std::string & _line) : num(_num), count(1), line(_line) { }
 
 			size_t num;
 			size_t count;
@@ -63,75 +39,49 @@ namespace MyGUI
 			static size_t num = 0;
 			// очередь всех наших строк
 			static DequeInfo lines;
-			// это наши оверлеи
-			static Ogre::OverlayContainer * overlayDebugInfo = 0;
-			static Ogre::OverlayContainer * overlayDebugInfoShadow = 0;
-			static Ogre::Overlay * overlay = 0;
 
-			// если первый раз, то создаем оверлей
-			if (num == 0) {
+			const int offset = 10;
+			const int count_lines = 20;
+			static const std::string font = "Default";
+			static const std::string layer = "Statistic";
+			static const std::string skin = "StaticText";
 
-				Ogre::RenderWindow * win = Ogre::Root::getSingleton().getAutoCreatedWindow();
-				if (win == 0) return;
+			static WidgetPtr widget = null;
+			static WidgetPtr widget_shadow = null;
 
-				// ищем шрифт
-				Ogre::ResourceManager::ResourceMapIterator fonts = Ogre::FontManager::getSingleton().getResourceIterator();
-				assert(fonts.hasMoreElements()); // шрифтов чета нет =(
+			if (widget == null) {
+				Gui * gui = Gui::getInstancePtr();
+				if (gui == null) return;
 
-				// первый шрифт
-				std::string fontName = fonts.getNext().getPointer()->getName();
-				// ищем дефолтный, если нет то будет первый
-				while (fonts.hasMoreElements()) {
-					const std::string & name = fonts.getNext().getPointer()->getName();
-					if (name == __DEBUG_FONT_DEFAULT) fontName = __DEBUG_FONT_DEFAULT;
-				};
+				const IntSize & size = gui->getViewSize();
 
-				// создаем оверлей, если не был создан
-				Ogre::OverlayManager &overlayManager = Ogre::OverlayManager::getSingleton();
-				overlay = overlayManager.create(__DEBUG_OVERLAY_NAME);
-				overlay->setZOrder(__DEBUG_OVERLAY_ZORDER);
+				if (!LayerManager::getInstance().isExist(layer)) return;
+				if (!SkinManager::getInstance().isExist(skin)) return;
+				if (!FontManager::getInstance().isExist(font)) return;
 
-				if (__DEBUG_SHOW_SHADOW) {
-					// текстовое поле,  тень
-					overlayDebugInfoShadow = static_cast< Ogre::OverlayContainer*>(overlayManager.createOverlayElement("TextArea", __DEBUG_OVERLAY_ELEMENT_SHADOW_NAME, false));
-					overlayDebugInfoShadow->setMetricsMode(Ogre::GMM_PIXELS);
-					overlayDebugInfoShadow->setLeft(__DEBUG_OVERLAY_OFFSET + 1);
-					overlayDebugInfoShadow->setTop(__DEBUG_OVERLAY_OFFSET + 1);
-					overlayDebugInfoShadow->setWidth(win->getWidth()-__DEBUG_OVERLAY_OFFSET-1);
-					overlayDebugInfoShadow->setHeight(win->getHeight()-__DEBUG_OVERLAY_OFFSET-1);
-					overlayDebugInfoShadow->setParameter("font_name", fontName);
-					overlayDebugInfoShadow->setParameter("char_height", utility::toString(__DEBUG_FONT_SIZE) );
-					overlayDebugInfoShadow->setParameter("colour_top", __DEBUG_FONT_SHADOW_COLOUR);
-					overlayDebugInfoShadow->setParameter("colour_bottom", __DEBUG_FONT_SHADOW_COLOUR);
-					overlay->add2D(overlayDebugInfoShadow); // добавляем контейнер на оверлей
-				}
-				// текстовое поле
-				overlayDebugInfo = static_cast< Ogre::OverlayContainer*>(overlayManager.createOverlayElement("TextArea", __DEBUG_OVERLAY_ELEMENT_NAME, false));
-				overlayDebugInfo->setMetricsMode(Ogre::GMM_PIXELS);
-				overlayDebugInfo->setLeft(__DEBUG_OVERLAY_OFFSET);
-				overlayDebugInfo->setTop(__DEBUG_OVERLAY_OFFSET);
-				overlayDebugInfo->setWidth(win->getWidth()-__DEBUG_OVERLAY_OFFSET);
-				overlayDebugInfo->setHeight(win->getHeight()-__DEBUG_OVERLAY_OFFSET);
-				overlayDebugInfo->setParameter("font_name", fontName);
-				overlayDebugInfo->setParameter("char_height", utility::toString(__DEBUG_FONT_SIZE) );
-				overlayDebugInfo->setParameter("colour_top", __DEBUG_FONT_COLOUR);
-				overlayDebugInfo->setParameter("colour_bottom", __DEBUG_FONT_COLOUR);
 
-				overlay->add2D(overlayDebugInfo); // добавляем контейнер на оверлей
-				overlay->show();
+				widget_shadow = gui->createWidget<Widget>(skin, IntCoord(offset + 1, offset + 1, size.width - offset - offset, size.height - offset - offset), Align::Stretch, layer);
+				widget_shadow->setNeedMouseFocus(false);
+				widget_shadow->setTextAlign(Align::Default);
+				widget_shadow->setColour(Ogre::ColourValue::Black);
 
+				widget = gui->createWidget<Widget>(skin, IntCoord(offset, offset, size.width - offset - offset, size.height - offset - offset), Align::Stretch, layer);
+				widget->setNeedMouseFocus(false);
+				widget->setTextAlign(Align::Default);
+				widget->setColour(Ogre::ColourValue::White);
 			}
 
 			if (lines.empty()) { // первый раз просто добавляем
 				lines.push_back(info(num++, _value));
 
-			} else { // не первый раз мы тут
+			}
+			else { // не первый раз мы тут
 				// сравниваем последнюю строку
 				if (lines.back().line == _value) lines.back().count ++;
 				else {
 					lines.push_back(info(num++, _value));
 					// удаляем лишнее
-					if (lines.size() > __DEBUG_COUNT_LINE) lines.pop_front();
+					if (lines.size() > count_lines) lines.pop_front();
 				}
 
 			}
@@ -145,8 +95,9 @@ namespace MyGUI
 			}
 
 			// непосредственный вывод
-			if (overlayDebugInfoShadow) overlayDebugInfoShadow->setCaption(str_out);
-			overlayDebugInfo->setCaption(str_out);
+			widget_shadow->setCaption(str_out);
+			widget->setCaption(str_out);
 		}
-	} // namespace implement
+	}
+
 } // namespace MyGUI
