1ee08c6b508f7f850a1c1f8636491ce068daa6ca
Ozzy__ <Ozzy__@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-10-08 19:42:46 +0000

    Add DependencePluginGetter

---
+++ trunk/MyGUI_Editor/Common/DependencePluginGetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using Core;
+
+namespace Common
+{
+    public static class DependencePluginGetter
+    {
+        public static Guid[] GetPlugins(Guid _project)
+        {
+            XmlNodeList list = CoreFacade.SettingsProvider.Root.SelectNodes(
+                    string.Format("project[@ID=\"{0}\"]/plugin", _project));
+            Guid[] guids;
+            if(list == null) guids = new Guid[0];
+            else
+            {
+                guids = new Guid[list.Count];
+                for(int i = 0; i < list.Count; i++)
+                    guids[i] = new Guid(list[i].InnerText);                    
+            }
+            return guids;
+        }
+    }
+}
