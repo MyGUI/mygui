f8040bf3dcb21aad30f15c2bcafb6f599d8904a3
Seregvan <Seregvan@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-10-08 20:52:29 +0000

    Папка Main вместо MyGUI_SkinEditor

---
+++ MyGUI_Editor/Main/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Main;
+
+namespace MyGUI_SkinEditor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
