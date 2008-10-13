83a8d9ccb9028e44cd9123cd2f62944a76439c3b
George Evmenov <Altren@users.noreply.github.com>
2008-10-13 00:55:13 +0000

    widget factories fix

--- MyGUIEngine/include/MyGUI_IWidgetFactory.h
+++ MyGUIEngine/include/MyGUI_IWidgetFactory.h
@@ -12,6 +12,9 @@
 #include "MyGUI_WidgetDefines.h"
 #include "MyGUI_ICroppedRectangle.h"
 
+#include "MyGUI_WidgetManager.h"
+#include "MyGUI_SkinManager.h"
+
 namespace MyGUI
 {
 	class _MyGUIExport IWidgetFactory
@@ -30,17 +33,43 @@ namespace MyGUI
 		class _MyGUIExport BaseWidgetFactory : public IWidgetFactory
 		{
 		public:
-			BaseWidgetFactory();
-			virtual ~BaseWidgetFactory();
+			BaseWidgetFactory()
+			{
+				// регестрируем себя
+				MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
+				manager.registerFactory(this);
+			}
 
-			const std::string & getTypeName();
-			WidgetPtr createWidget(const std::string& _skin, const IntCoord& _coord, Align _align, ICroppedRectangle * _parent, IWidgetCreator * _creator, const std::string& _name);
-			bool isFalseType(WidgetPtr _ptr, const std::string &_key);
+			~BaseWidgetFactory()
+			{
+				// удаляем себя
+				MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
+				manager.unregisterFactory(this);
+			}
+
+			const std::string & getTypeName()
+			{
+				return T::getClassTypeName();
+			}
+
+			WidgetPtr createWidget(const std::string& _skin, const IntCoord& _coord, Align _align, ICroppedRectangle * _parent, IWidgetCreator * _creator, const std::string& _name)
+			{
+				return new T(_coord, _align, SkinManager::getInstance().getSkin(_skin), _parent, _creator, _name);
+			}
+
+			bool isFalseType(WidgetPtr _ptr, const std::string &_key)
+			{
+				if (!_ptr->isType<T>()) {
+					MYGUI_LOG(Error, "Property '" << _key << "' is not supported by '" << _ptr->getTypeName() << "' widget");
+					return true;
+				}
+				return false;
+			}
 		};
 
 	} // namespace factory
 } // namespace MyGUI
 
-#include "MyGUI_IWidgetFactory.cpp"
+//#include "MyGUI_IWidgetFactory.cpp"
 
 #endif // __MYGUI_I_WIDGET_FACTORY_H__
--- MyGUIEngine/src/MyGUI_IWidgetFactory.cpp
+++ MyGUIEngine/src/MyGUI_IWidgetFactory.cpp
@@ -1,60 +1,60 @@
-/*!
-	@file
-	@author		Georgiy Evmenov
-	@date		10/2008
-	@module
-*/
-#ifndef __MYGUI_I_WIDGET_FACTORY_CPP__
-#define __MYGUI_I_WIDGET_FACTORY_CPP__
-
-#include "MyGUI_IWidgetFactory.h"
-#include "MyGUI_WidgetManager.h"
-#include "MyGUI_SkinManager.h"
-
-namespace MyGUI
-{
-	namespace factory
-	{
-
-		template <typename T>
-		BaseWidgetFactory<T>::BaseWidgetFactory()
-		{
-			// регестрируем себя
-			MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
-			manager.registerFactory(this);
-		}
-
-		template <typename T>
-		BaseWidgetFactory<T>::~BaseWidgetFactory()
-		{
-			// удаляем себя
-			MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
-			manager.unregisterFactory(this);
-		}
-
-		template <typename T>
-		const std::string & BaseWidgetFactory<T>::getTypeName()
-		{
-			return T::getClassTypeName();
-		}
-
-		template <typename T>
-		WidgetPtr BaseWidgetFactory<T>::createWidget(const std::string& _skin, const IntCoord& _coord, Align _align, ICroppedRectangle * _parent, IWidgetCreator * _creator, const std::string& _name)
-		{
-			return new T(_coord, _align, SkinManager::getInstance().getSkin(_skin), _parent, _creator, _name);
-		}
-		
-		template <typename T>
-		bool BaseWidgetFactory<T>::isFalseType(WidgetPtr _ptr, const std::string &_key)
-		{
-			if (!_ptr->isType<T>()) {
-				MYGUI_LOG(Error, "Property '" << _key << "' is not supported by '" << _ptr->getTypeName() << "' widget");
-				return true;
-			}
-			return false;
-		}
-
-	} // namespace factory
-} // namespace MyGUI
-
-#endif // __MYGUI_I_WIDGET_FACTORY_CPP__
+///*!
+//	@file
+//	@author		Georgiy Evmenov
+//	@date		10/2008
+//	@module
+//*/
+//#ifndef __MYGUI_I_WIDGET_FACTORY_CPP__
+//#define __MYGUI_I_WIDGET_FACTORY_CPP__
+//
+//#include "MyGUI_IWidgetFactory.h"
+//#include "MyGUI_WidgetManager.h"
+//#include "MyGUI_SkinManager.h"
+//
+//namespace MyGUI
+//{
+//	namespace factory
+//	{
+//
+//		template <typename T>
+//		BaseWidgetFactory<T>::BaseWidgetFactory()
+//		{
+//			// регестрируем себя
+//			MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
+//			manager.registerFactory(this);
+//		}
+//
+//		template <typename T>
+//		BaseWidgetFactory<T>::~BaseWidgetFactory()
+//		{
+//			// удаляем себя
+//			MyGUI::WidgetManager & manager = MyGUI::WidgetManager::getInstance();
+//			manager.unregisterFactory(this);
+//		}
+//
+//		template <typename T>
+//		const std::string & BaseWidgetFactory<T>::getTypeName()
+//		{
+//			return T::getClassTypeName();
+//		}
+//
+//		template <typename T>
+//		WidgetPtr BaseWidgetFactory<T>::createWidget(const std::string& _skin, const IntCoord& _coord, Align _align, ICroppedRectangle * _parent, IWidgetCreator * _creator, const std::string& _name)
+//		{
+//			return new T(_coord, _align, SkinManager::getInstance().getSkin(_skin), _parent, _creator, _name);
+//		}
+//		
+//		template <typename T>
+//		bool BaseWidgetFactory<T>::isFalseType(WidgetPtr _ptr, const std::string &_key)
+//		{
+//			if (!_ptr->isType<T>()) {
+//				MYGUI_LOG(Error, "Property '" << _key << "' is not supported by '" << _ptr->getTypeName() << "' widget");
+//				return true;
+//			}
+//			return false;
+//		}
+//
+//	} // namespace factory
+//} // namespace MyGUI
+//
+//#endif // __MYGUI_I_WIDGET_FACTORY_CPP__
