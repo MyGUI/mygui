fef359d47d1f8f6a27d69b5a0ea59ae679874799
my_name <mynameco@users.noreply.github.com>
2008-10-20 13:19:58 +0000

    fix texture not found

--- MyGUIEngine/include/MyGUI_ResourcePath.h
+++ MyGUIEngine/include/MyGUI_ResourcePath.h
@@ -15,9 +15,13 @@
 
 namespace MyGUI
 {
+	// перенести в ресурс менеджер, и добавить екзист
 	namespace helper
 	{
 		typedef std::vector<Ogre::String> VectorString;
+
+		// возвращает тру если есть
+		bool _MyGUIExport isFileExist(const Ogre::String & _mask, const Ogre::String & _group = Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 
 		// возвращает вектор путей ресурсов по маске
 		VectorString _MyGUIExport getVectorResourcePath(const Ogre::String & _mask, const Ogre::String & _group = Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
