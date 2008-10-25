228dad5e909ecdc9a54e5572878b6b0df9ac1841
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-10-25 16:29:02 +0000

    add unit test

---
+++ trunk/UnitTests/UnitTest_MultiList/DemoKeeper.cpp
@@ -0,0 +1,44 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		10/2008
+	@module
+*/
+#include "DemoKeeper.h"
+#include "UnitTest_List.h"
+
+namespace demo
+{
+
+	unittest::UnitTest_List * unittest_list = 0;
+
+	void DemoKeeper::createScene()
+	{
+        base::BaseManager::getInstance().addResourceLocation("../../Media/Wallpapers");
+        base::BaseManager::getInstance().setWallpaper("wallpaper0.jpg");
+
+		const MyGUI::IntSize & view = MyGUI::Gui::getInstance().getViewSize();
+		const MyGUI::IntSize size(100, 100);
+
+		unittest_list = new unittest::UnitTest_List();
+
+	}
+
+	void DemoKeeper::destroyScene()
+	{
+		delete unittest_list;
+		unittest_list = 0;
+	}
+
+	bool DemoKeeper::frameStarted(const Ogre::FrameEvent& evt)
+	{
+		if (unittest_list) unittest_list->nextFrame();
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+		::Sleep(1);
+#endif
+
+		return base::BaseManager::frameStarted(evt);
+	}
+
+} // namespace demo
