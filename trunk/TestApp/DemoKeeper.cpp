a8e49f6e5f0503edddd9c7fee3a242de620187ef
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-10-27 15:58:46 +0000

    fix LE

--- trunk/TestApp/DemoKeeper.cpp
+++ trunk/TestApp/DemoKeeper.cpp
@@ -11,6 +11,16 @@ namespace demo
 
     void DemoKeeper::createScene()
     {
+		MyGUI::helper::addResourceLocation("D:/MyGUI_Source/trunk/Media/TestApp", "FileSystem", "General", false, false);
+
+		MyGUI::ResourceManager::getInstance().load("test.xml");
+
+		//MyGUI::helper::addResourceLocation("D:/ww/WOT/Media/GUIskin/Wot", "FileSystem", "General", false, true);
+		//MyGUI::helper::addResourceLocation("D:/ww/WOT/Media/images", "FileSystem", "General", false, true);
+		//MyGUI::helper::addResourceLocation("D:/ww/WOT/Media/Resources", "FileSystem", "General", false, true);
+
+		MyGUI::StaticImagePtr image = MyGUI::Gui::getInstance().createWidgetReal<MyGUI::StaticImage>("StaticImage", MyGUI::FloatCoord(0, 0, 1, 1), MyGUI::Align::Default, "Main");
+		image->setImageTexture("panteonA.png");
 	}
  
     void DemoKeeper::destroyScene()
