0a1ad3404b3b3d62541a79be7ad8e2bc667fe3e7
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-11-01 00:37:18 +0000

    update paths

--- trunk/Tools/ImageSetViewer/Controller.cpp
+++ trunk/Tools/ImageSetViewer/Controller.cpp
@@ -11,7 +11,7 @@ namespace editor
  
     void Controller::createScene()
     {
-        base::BaseManager::getInstance().addResourceLocation("../../Media/Editors/ImageSetViewer");
+        base::BaseManager::getInstance().addResourceLocation("../../Media/Tools/ImageSetViewer");
         base::BaseManager::getInstance().addResourceLocation("../../Media/Wallpapers");
         base::BaseManager::getInstance().setWallpaper("wallpaper0.jpg");
 
