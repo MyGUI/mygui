507508628da045b96926ab8dec33547b817be03a
my_name <mynameco@users.noreply.github.com>
2008-11-16 14:59:32 +0000

    refactoring delegates

--- TestApp/DemoKeeper.cpp
+++ TestApp/DemoKeeper.cpp
@@ -12,114 +12,103 @@
 #include <fstream>
 #include <sstream>
 #include <assert.h>
+#include <assert.h>
 
-/*	template<typename T>
-	class Enumerator
-	{
-	private:
-		Enumerator() { }
+#include <gcroot.h>
 
-	public:
-		explicit Enumerator(const T & _container) :
-			m_first(true),
-			m_current(_container.begin()),
-			m_end(_container.end())
-		{
-		}
-
-		Enumerator(typename T::const_iterator _first, typename T::const_iterator _end) :
-			m_first(true),
-			m_current(_first),
-			m_end(_end)
-		{
-		}
-
-		bool next()
-		{
-			if (m_current == m_end) return false;
-			else if (m_first) {
-				m_first = false;
-				return true;
-			}
-			++ m_current;
-			if (m_current == m_end) return false;
-			return true;
-		}
-
-		typename T::const_reference operator->() const { assert(m_current != m_end); return (*m_current); }
-		typename T::const_reference current() { assert(m_current != m_end); return (*m_current); }
-
-	private:
-		typename T::const_iterator m_current;
-		typename T::const_iterator m_end;
-		bool m_first;
-	};
-
-class Class
-{
-public:
-	int test() { return 11; }
-	int const_test() const { return 12; }
-};*/
 
 
 namespace demo
 {
+
+	struct Param
+	{
+		Param(int _value)
+		{
+			value = _value;
+		}
+		int value;
+	};
+
+	/*value struct Param2
+	{
+		Param2(Param _value)
+		{
+			value = _value.value; 
+		}
+		static operator Param (Param2 _value)
+		{
+			return Param(_value.value);
+		}
+
+		int value;
+	};*/
+
+
+	delegate void HandleDelegate(Param _value);
+
+	template <typename T>
+	class Base
+	{
+	public:
+		typedef void (*Func)(T);
+
+		void Initialise(System::MulticastDelegate ^ _delegate)
+		{
+			m_delegate = _delegate;
+		}
+
+		void Invoke(T p1)
+		{
+			System::MulticastDelegate^ invoke = (System::MulticastDelegate^)m_delegate;
+			System::Type^ type = invoke->GetType();
+			System::Reflection::MethodInfo^ mInfo = type->GetMethod("Invoke", gcnew cli::array<System::Type^> {T::typeid });
+			Func foo = (Func)System::Runtime::InteropServices::Marshal::GetFunctionPointerForDelegate(invoke).ToPointer();
+			foo(p1);
+			//mInfo->Invoke(invoke, gcnew cli::array<System::Object^, 1> { (System::Object^)Param(p1) });
+		}
+
+	private:
+		gcroot<System::MulticastDelegate^> m_delegate;
+	};
+
+
+	/*template <typename T>
+	class Test
+	{
+	public:
+		System::MulticastDelegate ^ m_delegate;
+	};*/
+
+	value struct Export
+	{
+
+		static void foo(Param _value)
+		{
+			int test = 0;
+		}
+
+	};
+
+	void test()
+	{
+	}
 
     void DemoKeeper::createScene()
     {
 		MyGUI::helper::addResourceLocation("D:/MyGUI_Source/trunk/Media/TestApp", "FileSystem", "General", false, false);
 		MyGUI::helper::addResourceLocation("D:/MyGUI_Source/trunk/Media/TestApp/2.zip", "Zip", "General", false, false);
 
-		/*typedef Class * ClassPtr;
-		typedef std::vector<Class> VectorClass;
-		typedef std::vector<ClassPtr> VectorClassPtr;
+		Base<Param> base;
+		base.Initialise(gcnew HandleDelegate(Export::foo));
 
-		typedef Enumerator<VectorClass> EnumeratorClass;
-		typedef Enumerator<VectorClassPtr> EnumeratorClassPtr;
+		base.Invoke(Param(101));
+		
+		MyGUI::delegates::CDelegate0 eventTest;
 
-		VectorClass vector1;
-		VectorClassPtr vector2;
-
-		Class value;
-		vector1.push_back(value);
-		vector2.push_back(&value);
-
-		EnumeratorClass enum1(vector1);
-		EnumeratorClassPtr enum2(vector2);
-
-		while (enum1.next()) {
-			//int test = enum1.current().test();
-			int const_test = enum1.current().const_test();
-		};
-
-		while (enum2.next()) {
-			int test = enum2.current()->test();
-			int const_test = enum2.current()->const_test();
-		};
-
-
-		typedef std::vector<std::string> VectorString;
-		typedef Enumerator<VectorString> EnumeratorVectorString;
-		VectorString vec;
-		vec.push_back("value");
-		//EnumeratorVectorString enum_vec(vec.begin(), vec.end());
-		EnumeratorVectorString enum_vec(vec);
-		while (enum_vec.next()) {
-			std::string value = enum_vec.current();
-		};*/
-
-		std::string locale = ::setlocale( LC_ALL, "" );
-
-		std::wstring utf16 = L"_русский.xml";
-		std::string utf8 = MyGUI::convert::wide_to_utf8(utf16);
-
-		MyGUI::xml::xmlDocument doc;
-		if (!doc.open(utf8, "General")) {
-			throw new std::exception("ERROR");
-		}
-
-
+		eventTest = MyGUI::newDelegate(test);
+		eventTest();
+		
 	}
  
     void DemoKeeper::destroyScene()
