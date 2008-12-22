c71ac64107de59a7115ace588ec2a54c29bc994e
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2008-12-22 07:38:10 +0000

    update blender

---
+++ trunk/TestApp/KinematicalObject.h
@@ -0,0 +1,109 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		12/2008
+	@module
+*/
+#ifndef __KINEMATICAL_OBJECT_H__
+#define __KINEMATICAL_OBJECT_H__
+
+#include "IBase.h"
+#include "AnimationGraph.h"
+#include "TimeContext.h"
+#include  "AnimatebleObject.h"
+
+namespace sim
+{
+
+	class KinematicalObject : public anim::IAnimationLink, public IBase
+	{
+	public:
+		MYGUI_RTTI_CHILD_HEADER(KinematicalObject, IBase);
+
+		KinematicalObject(const std::string& _name) :
+			mName(_name),
+			mWalk(false)
+		{
+		}
+		virtual const std::string& getName() { return mName; }
+
+		virtual void initialise()
+		{
+			AnimatebleObject * anim = this->queryType<AnimatebleObject>();
+			anim->addExternalLink(this);
+			sim::AnimationGraph * graph1 = anim->createAnimationGraph("anim1.xml");
+			anim->startGraph(graph1);
+
+			float time = context::TimeContext::getCurrentTime();
+			eventExitEventStart1(time);
+		}
+
+		virtual anim::DelegateLinkEvent * getLinkEvent(const std::string& _name)
+		{
+			if (_name == "exit_eventStart1") {
+				return &eventExitEventStart1;
+			}
+			else if (_name == "exit_eventStop1") {
+				return &eventExitEventStop1;
+			}
+			if (_name == "exit_eventStart2") {
+				return &eventExitEventStart2;
+			}
+			else if (_name == "exit_eventStop2") {
+				return &eventExitEventStop2;
+			}
+			if (_name == "exit_eventStart3") {
+				return &eventExitEventStart3;
+			}
+			else if (_name == "exit_eventStop3") {
+				return &eventExitEventStop3;
+			}
+			else {
+				MYGUI_EXCEPT("link '" << _name << "' not found");
+			}
+		}
+		virtual anim::DelegateLinkValue * getLinkValue(const std::string& _name)
+		{
+			MYGUI_EXCEPT("link '" << _name << "' not found");
+		}
+		virtual void setLinkEvent(anim::DelegateLinkEvent * _delegate, const std::string& _name)
+		{
+			MYGUI_EXCEPT("link '" << _name << "' not found");
+		}
+		virtual void setLinkValue(anim::DelegateLinkValue * _delegate, const std::string& _name)
+		{
+			MYGUI_EXCEPT("link '" << _name << "' not found");
+		}
+		virtual ~KinematicalObject() { }
+
+		anim::DelegateLinkEvent eventExitEventStart1;
+		anim::DelegateLinkEvent eventExitEventStop1;
+		anim::DelegateLinkEvent eventExitEventStart2;
+		anim::DelegateLinkEvent eventExitEventStop2;
+		anim::DelegateLinkEvent eventExitEventStart3;
+		anim::DelegateLinkEvent eventExitEventStop3;
+
+		void keyPressed( const OIS::KeyEvent &arg )
+		{
+			float time = context::TimeContext::getCurrentTime();
+			if (arg.key == OIS::KC_W) eventExitEventStart2(time);
+			else if (arg.key == OIS::KC_SPACE) {
+				//eventExitEventStop3(time);
+				eventExitEventStart3(time);
+			}
+		}
+
+		void keyReleased( const OIS::KeyEvent &arg )
+		{
+			float time = context::TimeContext::getCurrentTime();
+			if (arg.key == OIS::KC_W) eventExitEventStart1(time);
+		}
+
+	private:
+		std::string mName;
+		bool mWalk;
+	};
+
+} // namespace sim
+
+#endif // __KINEMATICAL_OBJECT_H__
--- trunk/TestApp/LoopController.h
+++ trunk/TestApp/LoopController.h
@@ -21,24 +21,49 @@ namespace anim
 		LoopController(sim::IBase * _owner, MyGUI::xml::xmlNodePtr _node, const VectorState& _states) :
 			m_startTime(0),
 			m_weight(1),
-			m_length(0)
+			m_length(0),
+			mCount(-1)
 		{
 			mName = _node->findAttribute("id");
 			m_length = AnimationFactory::getTime(_node->findAttribute("time"), _states);
+			std::string count;
+			if (_node->findAttribute("count", count)) {
+				mCount = MyGUI::utility::parseSizeT(count);
+			}
 		}
 
-		virtual void update(float _currentTime)
+		virtual void update(float _time)
 		{
 			if (m_startTime == 0) return;
-			while (m_startTime + m_length < _currentTime)  _currentTime -= m_length;
-			float pos = (_currentTime - m_startTime) / m_length;
-			eventExitValue(_currentTime, pos);
+			size_t count = 1;
+			while (m_startTime + m_length < _time) {
+				if (mCount != -1) {
+					if (count < mCount) {
+						count++;
+					}
+					else {
+						eventStop(_time);
+						return;
+					}
+				}
+				_time -= m_length;
+			}
+			float pos = (_time - m_startTime) / m_length;
+			eventExitValue(_time, pos);
 		}
 		virtual const std::string& getName() { return mName; }
 
 		virtual DelegateLinkEvent * getLinkEvent(const std::string& _name)
 		{
-			MYGUI_EXCEPT("link '" << _name << "' not found");
+			if (_name == "exit_start") {
+				return &eventExitStart;
+			}
+			else if (_name == "exit_stop") {
+				return &eventExitStop;
+			}
+			else {
+				MYGUI_EXCEPT("link '" << _name << "' not found");
+			}
 		}
 		virtual DelegateLinkValue * getLinkValue(const std::string& _name)
 		{
@@ -82,22 +107,27 @@ namespace anim
 		{
 			m_startTime = _time;
 			eventExitWeight(_time, m_weight);
+			eventExitStart(_time);
 		}
 		void eventStop(float _time)
 		{
 			m_startTime = 0;
 			eventExitWeight(_time, 0);
+			eventExitStop(_time);
 		}
 
 	private:
 		DelegateLinkValue eventExitValue;
 		DelegateLinkValue eventExitWeight;
+		DelegateLinkEvent eventExitStart;
+		DelegateLinkEvent eventExitStop;
 
 	private:
 		float m_length;
 		float m_startTime;
 		float m_weight;
 		std::string mName;
+		size_t mCount;
 	};
 
 } // namespace anim
