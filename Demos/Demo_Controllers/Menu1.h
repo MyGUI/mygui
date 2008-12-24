d0f4699c10728350e5bab729043b7eaa315355ee
my_name <mynameco@users.noreply.github.com>
2008-12-24 22:38:09 +0000

    update demo

--- Demos/Demo_Controllers/Menu1.h
+++ Demos/Demo_Controllers/Menu1.h
@@ -13,11 +13,22 @@
 namespace demo
 {
 
+	struct ControllerType
+	{
+		enum EnumType { Jamp, Accelerated, Slowed, Inertional, MAX };
+		ControllerType(EnumType _value = MAX) : value(_value) { }
+		friend bool operator == (ControllerType const & a, ControllerType const & b) { return a.value == b.value; }
+		friend bool operator != (ControllerType const & a, ControllerType const & b) { return a.value != b.value; }
+	private:
+		int value;
+	};
+
 	class Menu1 : public wraps::BaseLayout
 	{
+
 	public:
 		Menu1();
-		void initialise(const std::string& _layout);
+		void initialise(const std::string& _layout, ControllerType _type);
 		virtual void shutdown() { FrameAdvise(false); }
 
 		MyGUI::WidgetPtr getClient() { return mainWidget()->getClientWidget(); }
@@ -25,11 +36,12 @@ namespace demo
 		void show();
 		void hide();
 
-		MyGUI::delegates::CDelegate1<size_t> eventButtonPress;
+		MyGUI::delegates::CDelegate1<ControllerType> eventButtonPress;
 
 	private:
 		void notifyMouseButtonClick(MyGUI::WidgetPtr _sender);
 		void notifyFrameEvent(float _time);
+		MyGUI::ControllerPosition * getController(const MyGUI::IntPoint & _point);
 
 		void FrameAdvise(bool _advise)
 		{
@@ -54,7 +66,7 @@ namespace demo
 		MyGUI::ButtonPtr mButton3;
 		MyGUI::ButtonPtr mButton4;
 		float mCountTime;
-
+		ControllerType mType;
 	};
 
 } // namespace demo
