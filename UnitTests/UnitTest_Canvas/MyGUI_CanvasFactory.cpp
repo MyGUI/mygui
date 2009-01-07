8ce61519fbbfb6957f0770fddbf57ef7f8242be4
Alexander Ptakhin <me@aptakhin.name>
2009-01-07 13:41:26 +0000

    Fixes previous Canvas commit.

---
+++ UnitTests/UnitTest_Canvas/MyGUI_CanvasFactory.cpp
@@ -0,0 +1,22 @@
+/*!
+	@file
+	@author		Alexander Ptakhin
+	@date		01/2009
+	@module
+*/
+#include "MyGUI_CanvasFactory.h"
+
+namespace MyGUI
+{
+	namespace factory
+	{
+		/*CanvasFactory::CanvasFactory()
+		{
+		}
+
+		CanvasFactory::~CanvasFactory()
+		{
+		}*/
+
+	} // namespace factory
+} // namespace MyGUI
