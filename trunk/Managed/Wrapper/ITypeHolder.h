c4d8c87f0d0e927e3e3cc0547fb12819c647436e
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2009-01-16 05:23:32 +0000

    update Managed projects

--- trunk/Managed/Wrapper/ITypeHolder.h
+++ trunk/Managed/Wrapper/ITypeHolder.h
@@ -16,6 +16,7 @@ namespace wrapper
 	{
 	public:
 		virtual std::string getTypeDescription(const std::string& _type) = 0;
+		virtual std::string getMemberName(const std::string& _name) = 0;
 	};
 
 } // namespace wrapper
