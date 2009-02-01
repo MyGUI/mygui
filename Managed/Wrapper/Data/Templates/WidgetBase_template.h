656a67477fe34bc47c40e1a2d05bd484a756b8b4
my_name <my.name.post@gmail.com>
2009-02-01 14:22:05 +0000

    add templates

---
+++ Managed/Wrapper/Data/Templates/WidgetBase_template.h
@@ -0,0 +1,27 @@
+﻿/*!
+	@file
+	@author		Generate utility by Albert Semenov
+	@date
+	@module
+*/
+#pragma once
+
+#include "../MMyGUI_Core.h"
+
+namespace MMyGUI
+{
+
+	public ref class #{ThisType}
+	{
+
+		//--------------------------------------------------------------------
+		// объявление типов и конструкторов
+		MMYGUI_DECLARE_BASE( #{ThisType} );
+
+		#{IncludeManual}
+
+		//InsertPoint
+
+	};
+
+} // namespace MMyGUI
---
+++ Managed/Wrapper/Data/Templates/WidgetDerived_template.h
@@ -0,0 +1,26 @@
+﻿/*!
+	@file
+	@author		Generate utility by Albert Semenov
+	@date		01/2009
+	@module
+*/
+#pragma once
+
+#include "MMyGUI_Widget.h"
+#{Include}
+
+namespace MMyGUI
+{
+
+	public ref class #{ThisType} : public #{BaseType}
+	{
+
+		//--------------------------------------------------------------------
+		// объявление типов и конструкторов
+		MMYGUI_DECLARE_DERIVED( #{ThisType}, #{BaseType} );
+
+		//InsertPoint
+
+	};
+
+} // namespace MMyGUI
