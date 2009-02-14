2ccdde85175ba282f56125f9875bec5fcd1010d7
my_name <mynameco@users.noreply.github.com>
2009-02-14 14:52:52 +0000

    update Managed projects

---
+++ Managed/MyGUI.Export/ExportDefine.h
@@ -0,0 +1,16 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		11/2007
+	@module
+*/
+#ifndef __EXPORT_DEFINE_H__
+#define __EXPORT_DEFINE_H__
+
+#define EXPORT_MYGUI extern "C"
+
+namespace ExportMyGUI
+{
+} // namespace ExportMyGUI
+
+#endif // __EXPORT_DEFINE_H__
