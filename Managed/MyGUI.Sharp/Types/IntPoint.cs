528cef349dba5ceafae64262398f6e5ccae56ec0
my_name <my.name.post@gmail.com>
2009-02-14 13:30:03 +0000

    update Managed projects

---
+++ Managed/MyGUI.Sharp/Types/IntPoint.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGUI.Sharp
+{
+    public struct IntPoint
+    {
+        public int left;
+        public int top;
+
+        public IntPoint(int _left, int _top)
+        {
+            left = _left;
+            top = _top;
+        }
+    }
+}
