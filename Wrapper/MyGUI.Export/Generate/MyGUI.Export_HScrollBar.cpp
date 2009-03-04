f1284900120c385cabb1c06b2d42ab182f7ae95a
my_name <mynameco@users.noreply.github.com>
2009-03-04 01:44:40 +0000

    relocation Sharp project

---
+++ Wrapper/MyGUI.Export/Generate/MyGUI.Export_HScrollBar.cpp
@@ -0,0 +1,31 @@
+﻿/*!
+	@file
+	@author		Generate utility by Albert Semenov
+	@date		01/2009
+	@module
+*/
+
+#include "../ExportDefine.h"
+#include "../ExportMarshaling.h"
+#include "../ExportMarshalingWidget.h"
+#include "../ExportMarshalingType.h"
+#include <MyGUI.h>
+
+namespace Export
+{
+
+	//InsertPoint
+
+   
+
+
+   
+
+
+   
+
+
+   
+
+
+}
