5b2d81a27f7497d5350516bceca62df89ac82e4f
my_name <mynameco@users.noreply.github.com>
2009-03-04 00:57:48 +0000

    update Sharp projects

--- Managed/MyGUI.Sharp/Core/Gui.cs
+++ Managed/MyGUI.Sharp/Core/Gui.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace MyGUI.Sharp
 {
@@ -160,12 +161,139 @@ namespace MyGUI.Sharp
             return LoadLayout(_file, _parent, "");
         }
 
+        [DllImport("MyGUI.Export.dll", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        private static extern string ExportGui_GetPath(
+            [MarshalAs(UnmanagedType.LPStr)]  string _filename );
+
         public List<Widget> LoadLayout(string _file, Widget _parent, string _prefix)
         {
-            //FIXME
-            return new List<Widget>();
+            List<Widget> widgets = new List<Widget>();
+            string filename = ExportGui_GetPath(_file);
+
+            XmlDocument mfDocument = new XmlDocument();
+            mfDocument.Load(filename);
+
+            foreach (XmlNode node in mfDocument)
+            {
+                if ("MyGUI" != node.Name) continue;
+                if ("Layout" != node.Attributes["type"].Value) continue;
+
+                foreach (XmlNode widget_node in node)
+                {
+                    if ("Widget" != widget_node.Name) continue;
+                    ParseWidget(widgets, widget_node, _parent, null, _prefix);
+                }
+            }
+
+            return widgets;
         }
 
+        private void ParseWidget(List<Widget> _widgets, XmlNode _node, Widget _parent, Widget _root, string _prefix)
+        {
+            string style = "", type = "", skin = "", name = "", layer = "", align = "", position = "";
+
+            foreach (XmlAttribute attribute in _node.Attributes)
+            {
+                switch (attribute.Name)
+                {
+                    case "style": style = attribute.Value; break;
+                    case "type": type = attribute.Value; break;
+                    case "skin": skin = attribute.Value; break;
+                    case "name": name = attribute.Value; break;
+                    case "layer": layer = attribute.Value; break;
+                    case "align": align = attribute.Value; break;
+                    case "position": position = attribute.Value; break;
+                }
+
+                if (type == "Sheet") type = "TabItem";
+            }
+
+            if ((0 == type.Length) || (0 == skin.Length))
+            {
+                return;
+            }
+
+            if ((0 != name.Length) && (0 != _prefix.Length)) name = _prefix + name;
+
+            string[] coord = position.Split();
+
+            WidgetStyle wstyle = WidgetStyle.Overlapped;
+            if (_parent != null)
+            {
+                layer = "";
+                wstyle = (style != string.Empty) ? (WidgetStyle)Enum.Parse(typeof(WidgetStyle), style) : WidgetStyle.Child;
+            }
+
+            Align walign = Align.Default;
+            if (align != string.Empty)
+            {
+                walign = Align.Center;
+                string[] mass = align.Split();
+                foreach (string item in mass)
+                    walign |= (Align)Enum.Parse(typeof(Align), item);
+            }
+
+            Widget widget = (Widget)mMapCreator[type](
+                _parent == null ? IntPtr.Zero : _parent.GetNative(),
+                wstyle,
+                skin,
+                ((coord.Length == 4) ? new IntCoord(int.Parse(coord[0]), int.Parse(coord[1]), int.Parse(coord[2]), int.Parse(coord[3])) : new IntCoord()),
+                walign,
+                layer,
+                name);
+
+            if (null == _root)
+            {
+                _root = widget;
+                _widgets.Add(_root);
+            }
+
+            foreach (XmlNode node in _node)
+            {
+                if ("Widget" == node.Name)
+                {
+                    ParseWidget(_widgets, node, widget, _root, _prefix);
+                }
+                else if ("Property" == node.Name)
+                {
+                    string key = "", value = "";
+                    foreach (XmlAttribute attribute in node.Attributes)
+                    {
+                        if ("key" == attribute.Name) key = attribute.Value;
+                        else if ("value" == attribute.Name) value = attribute.Value;
+                    }
+                    if ((0 == key.Length) || (0 == value.Length)) continue;
+
+                    SetProperty(widget, key, value);
+                }
+                else if ("UserString" == node.Name)
+                {
+                    string key = "", value = "";
+                    foreach (XmlAttribute attribute in node.Attributes)
+                    {
+                        if ("key" == attribute.Name) key = attribute.Value;
+                        else if ("value" == attribute.Name) value = attribute.Value;
+                    }
+                    if ((0 == key.Length) || (0 == value.Length)) continue;
+                    widget.SetUserString(key, value);
+
+                    if (EventParserUserData != null) EventParserUserData(widget, key, value);
+                }
+            }
+
+        }
+
+        [DllImport("MyGUI.Export.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ExportGui_SetProperty(
+            IntPtr _widget,
+            [MarshalAs(UnmanagedType.LPStr)]  string _key ,
+            [MarshalAs(UnmanagedType.LPStr)]  string _value );
+
+        void SetProperty(Widget _widget, string _key, string _value)
+        {
+            ExportGui_SetProperty(_widget.GetNative(), _key, _value);
+        }
 
         #endregion
 
