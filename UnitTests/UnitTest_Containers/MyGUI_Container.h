9a82c51be11e3640b51b1b273a560c46f328568e
Alexander Ptakhin <me@aptakhin.name>
2009-04-10 18:51:50 +0000

    FlowContainer night update. Special XML-loading added.

--- UnitTests/UnitTest_Containers/MyGUI_Container.h
+++ UnitTests/UnitTest_Containers/MyGUI_Container.h
@@ -20,9 +20,8 @@ namespace MyGUI
 	protected:
 		Container( WidgetStyle _style, const IntCoord& _coord, Align _align, const WidgetSkinInfoPtr _info, WidgetPtr _parent, ICroppedRectangle * _croppedParent, IWidgetCreator * _creator, const std::string & _name );
 
+		// Ovarlap.
 		WidgetPtr baseCreateWidget( WidgetStyle _style, const std::string & _type, const std::string & _skin, const IntCoord& _coord, Align _align, const std::string & _layer, const std::string & _name );
-
-		//virtual void
 
 		virtual void add( WidgetPtr widget ) = 0;
 
@@ -33,7 +32,7 @@ namespace MyGUI
 		//! @copydoc Widget::setSize(const IntSize& _size)
 		void setSize(const IntSize& _size);
 
-		//! @copydoc setCoord(const IntCoord& _coord)
+		//! @copydoc Widget::setCoord(const IntCoord& _coord)
 		void setCoord(const IntCoord& _coord);
 
 	public:	
