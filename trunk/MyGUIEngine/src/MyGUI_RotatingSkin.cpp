117c0f2c55daa518b2024c4239dc775cbd2866f7
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2009-05-03 16:35:55 +0000

    RotatingSkin: custom center and fixes

--- trunk/MyGUIEngine/src/MyGUI_RotatingSkin.cpp
+++ trunk/MyGUIEngine/src/MyGUI_RotatingSkin.cpp
@@ -39,10 +39,23 @@ namespace MyGUI
 	{
 	}
 
-	void RotatingSkin::setAngle(double _angle)
+	void RotatingSkin::setAngle(float _angle)
 	{
 		mAngle = _angle;
 		if (nullptr != mRenderItem) mRenderItem->outOfDate();
+	}
+
+	void RotatingSkin::setCenter(const IntPoint &_center, bool _local)
+	{
+		mCenterPos = _center;
+		mLocalCenter = _local;
+		recalculateAngles();
+		if (nullptr != mRenderItem) mRenderItem->outOfDate();
+	}
+
+	IntPoint RotatingSkin::getCenter(bool _local) const
+	{
+		return mCenterPos + (_local ? IntPoint() : mCroppedParent->getAbsolutePosition());
 	}
 
 	void RotatingSkin::doRender()
@@ -54,63 +67,48 @@ namespace MyGUI
 
 		float vertex_z = mRenderItem->getMaximumDepth();
 
-		float vertex_left_base = ((mRenderItem->getPixScaleX() * (float)(mCurrentCoord.left + mCroppedParent->getAbsoluteLeft()) + mRenderItem->getHOffset()) * 2) - 1;
-		float vertex_width_base = (mRenderItem->getPixScaleX() * (float)mCurrentCoord.width * 2);
-		float vertex_top_base = -(((mRenderItem->getPixScaleY() * (float)(mCurrentCoord.top + mCroppedParent->getAbsoluteTop()) + mRenderItem->getVOffset()) * 2) - 1);
-		float vertex_height_base = - (mRenderItem->getPixScaleY() * (float)mCurrentCoord.height * 2);
+		float vertex_left_base = ((mRenderItem->getPixScaleX() * (float)(mCurrentCoord.left + mCroppedParent->getAbsoluteLeft() + mCenterPos.left) + mRenderItem->getHOffset()) * 2) - 1;
+		float vertex_top_base = -(((mRenderItem->getPixScaleY() * (float)(mCurrentCoord.top + mCroppedParent->getAbsoluteTop() + mCenterPos.top) + mRenderItem->getVOffset()) * 2) - 1);
+
+		// FIXME: do it only when size changes
+		recalculateAngles();
 
 		float s = sin(mAngle);
 		float c = cos(mAngle);
 
 		// first triangle - left top
-		_vertex[0].x = vertex_left_base;
-		_vertex[0].y = vertex_top_base;
+		_vertex[0].x = vertex_left_base + cos(mAngle + mBaseAngles[0]) * mBaseDistances[0] * mRenderItem->getPixScaleX() * -2;
+		_vertex[0].y = vertex_top_base + sin(mAngle + mBaseAngles[0]) * mBaseDistances[0] * mRenderItem->getPixScaleY() * -2;
 		_vertex[0].z = vertex_z;
 		_vertex[0].colour = mCurrentAlpha;
 		_vertex[0].u = mCurrentTexture.left;
 		_vertex[0].v = mCurrentTexture.top;
 
-
 		// first triangle - left bottom
-		_vertex[1].x = vertex_left_base - s * vertex_height_base;
-		_vertex[1].y = vertex_top_base + c * vertex_height_base;
+		_vertex[1].x = vertex_left_base + cos(mAngle + mBaseAngles[1]) * mBaseDistances[1] * mRenderItem->getPixScaleX() * -2;
+		_vertex[1].y = vertex_top_base + sin(mAngle + mBaseAngles[1]) * mBaseDistances[1] * mRenderItem->getPixScaleY() * -2;
 		_vertex[1].z = vertex_z;
 		_vertex[1].colour = mCurrentAlpha;
 		_vertex[1].u = mCurrentTexture.left;
 		_vertex[1].v = mCurrentTexture.bottom;
 
 		// first triangle - right top
-		_vertex[2].x = vertex_left_base + c * vertex_width_base;
-		_vertex[2].y = vertex_top_base + s * vertex_width_base;
+		_vertex[2].x = vertex_left_base + cos(mAngle + mBaseAngles[3]) * mBaseDistances[3] * mRenderItem->getPixScaleX() * -2;
+		_vertex[2].y = vertex_top_base + sin(mAngle + mBaseAngles[3]) * mBaseDistances[3] * mRenderItem->getPixScaleY() * -2;
 		_vertex[2].z = vertex_z;
 		_vertex[2].colour = mCurrentAlpha;
 		_vertex[2].u = mCurrentTexture.right;
 		_vertex[2].v = mCurrentTexture.top;
 
 		// second triangle - right top
-		_vertex[3].x = vertex_left_base + c * vertex_width_base;
-		_vertex[3].y = vertex_top_base + s * vertex_width_base;
-		_vertex[3].z = vertex_z;
-		_vertex[3].colour = mCurrentAlpha;
-		_vertex[3].u = mCurrentTexture.right;
-		_vertex[3].v = mCurrentTexture.top;
+		_vertex[3] = _vertex[2];
 
 		// second triangle = left bottom
-		_vertex[4].x = vertex_left_base - s * vertex_height_base;
-		_vertex[4].y = vertex_top_base + c * vertex_height_base;
-		_vertex[4].z = vertex_z;
-		_vertex[4].colour = mCurrentAlpha;
-		_vertex[4].u = mCurrentTexture.left;
-		_vertex[4].v = mCurrentTexture.bottom;
+		_vertex[4] = _vertex[1];
 
-		// FIXME
-		float ang = atan2(vertex_height_base, vertex_width_base);
-		s = sin(mAngle + ang);
-		c = cos(mAngle + ang);
-		float diag = sqrt(vertex_width_base*vertex_width_base + vertex_height_base*vertex_height_base);
 		// second triangle - right botton
-		_vertex[5].x = vertex_left_base + c * diag;
-		_vertex[5].y = vertex_top_base + s * diag;
+		_vertex[5].x = vertex_left_base + cos(mAngle + mBaseAngles[2]) * mBaseDistances[2] * mRenderItem->getPixScaleX() * -2;
+		_vertex[5].y = vertex_top_base + sin(mAngle + mBaseAngles[2]) * mBaseDistances[2] * mRenderItem->getPixScaleY() * -2;
 		_vertex[5].z = vertex_z;
 		_vertex[5].colour = mCurrentAlpha;
 		_vertex[5].u = mCurrentTexture.right;
@@ -122,6 +120,37 @@ namespace MyGUI
 	StateInfo * RotatingSkin::createStateData(xml::ElementPtr _node, xml::ElementPtr _root, Version _version)
 	{
 		return SubSkin::createStateData(_node, _root, _version);
+	}
+
+	inline float len(float x, float y) { return sqrt(x*x + y*y); };
+
+	void RotatingSkin::recalculateAngles()
+	{
+#ifndef M_PI
+			const float M_PI = 3.141593;
+#endif
+		float left_base = 0;
+		float top_base = 0;
+
+		if (!mLocalCenter)
+		{
+			left_base = (float)mCurrentCoord.width;
+			top_base = (float)mCurrentCoord.height;
+		}
+
+		float width_base = (float)mCurrentCoord.width;
+		float height_base = (float)mCurrentCoord.height;
+
+		mBaseAngles[0] = atan2((float)           - mCenterPos.left,             - mCenterPos.top);
+		mBaseAngles[1] = atan2((float)           - mCenterPos.left, height_base - mCenterPos.top);
+		mBaseAngles[2] = atan2((float)width_base - mCenterPos.left, height_base - mCenterPos.top);
+		mBaseAngles[3] = atan2((float)width_base - mCenterPos.left,             - mCenterPos.top);
+
+		mBaseDistances[0] = len((float)           - mCenterPos.left,             - mCenterPos.top);
+		mBaseDistances[1] = len((float)           - mCenterPos.left, height_base - mCenterPos.top);
+		mBaseDistances[2] = len((float)width_base - mCenterPos.left, height_base - mCenterPos.top);
+		mBaseDistances[3] = len((float)width_base - mCenterPos.left,             - mCenterPos.top);
+
 	}
 
 } // namespace MyGUI
