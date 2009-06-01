015513c37c1279f542738b88e1b903b58f23a0d7
George Evmenov <altren@gmail.com>
2009-06-01 17:26:21 +0000

    getline fix update svn ignores

--- MyGUIEngine/src/MyGUI_XmlDocument.cpp
+++ MyGUIEngine/src/MyGUI_XmlDocument.cpp
@@ -394,9 +394,9 @@ namespace MyGUI
 			while (!_stream.eof())
 			{
 				// берем новую строку
-				std::getline(_stream, read, (char)0x0D);
+				std::getline(_stream, read, '\n');
 				if (read.empty()) continue;
-				if (read[0] == (char)0x0A) read.erase(0, 1);
+				if (read[read.size()-1] == '\r') read.erase(read.size()-1, 1);
 				if (read.empty()) continue;
 
 				mLine ++;
