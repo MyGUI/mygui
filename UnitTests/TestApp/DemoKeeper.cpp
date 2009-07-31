6ef306674c611d57165d3c7878718ab973224f95
George Evmenov <altren@gmail.com>
2009-07-31 13:32:37 +0000

    cut TextureAnimators from TestApp

--- UnitTests/TestApp/DemoKeeper.cpp
+++ UnitTests/TestApp/DemoKeeper.cpp
@@ -6,137 +6,18 @@
 */
 #include "DemoKeeper.h"
 #include "Base/Main.h"
-#include "MyGUI_RTTLayer.h"
-#include "MyGUI_RTTLayerNode.h"
-#include "MyGUI_LayerNodeAnimation.h"
-#include "WobbleNodeAnimator.h"
-#include "FadeNodeAnimator.h"
 #include "BaseLayout/BaseLayout.h"
 
 namespace demo
 {
-	MyGUI::WindowPtr widget = nullptr;
-
-	void notifyWindowButtonPressed(MyGUI::WindowPtr _sender, const std::string& _name)
-	{
-		if (_name == "close")
-		{
-			MyGUI::WidgetManager::getInstance().destroyWidget(_sender);
-			widget = nullptr;
-		}
-		else if (_name == "check")
-		{
-			const MyGUI::IntCoord coord(0, 0, 1024, 768);
-			const MyGUI::IntSize size(300, 300);
-
-			if (widget->getCoord().width != coord.width)
-			{
-				widget->setCoord(coord);
-			}
-			else
-			{
-				widget->setCoord(coord.width / 2 - size.width / 2, coord.height / 2 - size.height / 2, size.width, size.height);
-			}
-		}
-	}
-
     void DemoKeeper::createScene()
     {
-		MyGUI::FactoryManager::getInstance().registryFactory<WobbleNodeAnimator>("NodeAnimator");
-		MyGUI::FactoryManager::getInstance().registryFactory<FadeNodeAnimator>("NodeAnimator");
-
-		MyGUI::FactoryManager::getInstance().registryFactory<MyGUI::RTTLayer>("Layer");
-
 		this->addResourceLocation("../../Media/UnitTests/TestApp");
-		mGUI->load("test_layer.xml");
-
-		widget = mGUI->createWidget<MyGUI::Window>("WindowCSMX", MyGUI::IntCoord(56, 16, 300, 300), MyGUI::Align::Default, "RTT_Test");
-		widget->setCaption("Vertext mode");
-
-		widget->eventWindowButtonPressed = MyGUI::newDelegate(notifyWindowButtonPressed);
-
 	}
 
     void DemoKeeper::destroyScene()
     {
     }
-
-	bool DemoKeeper::keyPressed( const OIS::KeyEvent &arg )
-	{
-		if (arg.key == OIS::KC_H)
-		{
-			widget = mGUI->createWidget<MyGUI::Window>("WindowCSMX", MyGUI::IntCoord(56, 16, 300, 300), MyGUI::Align::Default, "RTT_Test");
-			widget->setCaption("Vertext mode");
-			widget->eventWindowButtonPressed = MyGUI::newDelegate(notifyWindowButtonPressed);
-		}
-
-		if (widget == nullptr) return BaseManager::keyPressed( arg );
-
-		if (arg.key == OIS::KC_1)
-		{
-			mCamera->setPolygonMode(Ogre::PM_SOLID);
-		}
-		else if (arg.key == OIS::KC_2)
-		{
-			mCamera->setPolygonMode(Ogre::PM_WIREFRAME);
-		}
-		else if (arg.key == OIS::KC_3)
-		{
-			mCamera->setPolygonMode(Ogre::PM_POINTS);
-		}
-		else if (arg.key == OIS::KC_4)
-		{
-			MyGUI::LayerManager::EnumeratorLayer layer = MyGUI::LayerManager::getInstance().getEnumerator();
-			while(layer.next())
-			{
-				if (layer->getName() == "RTT_Test")
-				{
-					//layer->castType<MyGUI::RTTLayer>()->setLayerNodeAnimation(&gCustomLayerNodeAnimation);
-				}
-			}
-		}
-		else if (arg.key == OIS::KC_SPACE)
-		{
-			MyGUI::LayerManager::EnumeratorLayer layer = MyGUI::LayerManager::getInstance().getEnumerator();
-			while(layer.next())
-			{
-				if (layer->getName() == "RTT_Test")
-				{
-					MyGUI::EnumeratorILayerNode node = layer->getEnumerator();
-					while(node.next())
-					{
-						MyGUI::RTTLayerNode* rttnode = node->castType<MyGUI::RTTLayerNode>(false);
-						if (rttnode != nullptr)
-						{
-							rttnode->setCacheUsing(!rttnode->getCacheUsing());
-
-							if (rttnode->getCacheUsing())
-							{
-								/*if (rttnode->getLayerNodeAnimation() != nullptr)
-								{
-									//rttnode->setLayerNodeAnimation(nullptr);
-									widget->setCaption("RTT mode");
-								}
-								else
-								{
-									//rttnode->setLayerNodeAnimation(&gCustomLayerNodeAnimation);
-									widget->setCaption("Abstract mode");
-								}*/
-							}
-							else
-							{
-								widget->setCaption("Vertext mode");
-							}
-
-						}
-					}
-				}
-			}
-		}
-
-		return BaseManager::keyPressed( arg );
-	}
-
 } // namespace demo
 
 MYGUI_APP(demo::DemoKeeper)
