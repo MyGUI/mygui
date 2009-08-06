f5a287f5a7f1d916869328cd761033c035a6d3f7
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2009-08-06 22:29:58 +0000

    remove dependency on Ogre in demos rename CMake var that tries to copy dll's from ogre

--- trunk/Common/precompiled.h
+++ trunk/Common/precompiled.h
@@ -1,7 +1,7 @@
 #ifdef MYGUI_OGRE_PLATFORM
-#    define MYGUI_CUSTOM_ALLOCATOR
-#    include <Ogre.h>
-#include <OIS.h>
+//#  define MYGUI_CUSTOM_ALLOCATOR
+//#  include <Ogre.h>
+#  include <OIS.h>
 #elif  MYGUI_DIRECTX_PLATFORM
 #elif  MYGUI_OPENGL_PLATFORM
 #endif
