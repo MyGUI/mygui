1bfac6a8ec0c6afbda3fd2e0b6836496d8aa0036
George Evmenov <Altren@users.noreply.github.com>
2009-09-10 00:07:11 +0000

    add resources.xml

--- CMake/InstallResources.cmake
+++ CMake/InstallResources.cmake
@@ -16,6 +16,10 @@ elseif (UNIX)
   set(MYGUI_PLUGIN_DIR_DBG "../../lib/MYGUI")
 endif ()
 
+# create resources.xml
+configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/debug/resources.xml)
+configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/release/resources.xml)
+# create resources.cfg
 configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/debug/resources.cfg)
 configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/release/resources.cfg)
 # create plugins.cfg
@@ -24,36 +28,45 @@ configure_file(${MYGUI_TEMPLATES_DIR}/plugins.cfg.in ${MYGUI_BINARY_DIR}/bin/rel
 
 # install resource files
 if (MYGUI_INSTALL_SAMPLES OR MYGUI_INSTALL_TOOLS)
-  install(FILES 
-    ${MYGUI_BINARY_DIR}/bin/debug/resources.cfg
-    ${MYGUI_BINARY_DIR}/bin/debug/plugins.cfg
-    DESTINATION "bin${MYGUI_DEBUG_PATH}" CONFIGURATIONS Debug
-  )
-  install(FILES 
-    ${MYGUI_BINARY_DIR}/bin/release/resources.cfg
-    ${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
-    DESTINATION "bin${MYGUI_RELEASE_PATH}" CONFIGURATIONS Release None ""
-  )
-  install(FILES 
-    ${MYGUI_BINARY_DIR}/bin/release/resources.cfg
-    ${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
-    DESTINATION "bin${MYGUI_RELWDBG_PATH}" CONFIGURATIONS RelWithDebInfo
-  )
-  install(FILES 
-    ${MYGUI_BINARY_DIR}/bin/release/resources.cfg
-    ${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
-    DESTINATION "bin${MYGUI_MINSIZE_PATH}" CONFIGURATIONS MinSizeRel
-  )
+	install(FILES
+		${MYGUI_BINARY_DIR}/bin/debug/resources.xml
+		${MYGUI_BINARY_DIR}/bin/debug/resources.cfg
+		${MYGUI_BINARY_DIR}/bin/debug/plugins.cfg
+		DESTINATION "bin${MYGUI_DEBUG_PATH}" CONFIGURATIONS Debug
+	)
+	install(FILES
+		${MYGUI_BINARY_DIR}/bin/release/resources.xml
+		${MYGUI_BINARY_DIR}/bin/release/resources.cfg
+		${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
+		DESTINATION "bin${MYGUI_RELEASE_PATH}" CONFIGURATIONS Release None ""
+	)
+	install(FILES
+		${MYGUI_BINARY_DIR}/bin/release/resources.xml
+		${MYGUI_BINARY_DIR}/bin/release/resources.cfg
+		${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
+		DESTINATION "bin${MYGUI_RELWDBG_PATH}" CONFIGURATIONS RelWithDebInfo
+	)
+	install(FILES
+		${MYGUI_BINARY_DIR}/bin/release/resources.xml
+		${MYGUI_BINARY_DIR}/bin/release/resources.cfg
+		${MYGUI_BINARY_DIR}/bin/release/plugins.cfg
+		DESTINATION "bin${MYGUI_MINSIZE_PATH}" CONFIGURATIONS MinSizeRel
+	)
 endif ()
 
 if (WIN32)
+  # create resources.cfg
+  configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/debug/resources.xml)
+  configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/release/resources.xml)
+  configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/relwithdebinfo/resources.xml)
+  configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/minsizerel/resources.xml)
   # create resources.cfg
   configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/debug/resources.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/release/resources.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/relwithdebinfo/resources.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/minsizerel/resources.cfg)
   # create plugins.cfg
-  configure_file(${MYGUI_TEMPLATES_DIR}/plugins_d.cfg.in ${MYGUI_BINARY_DIR}/bin/debug/plugins_d.cfg)
+  configure_file(${MYGUI_TEMPLATES_DIR}/plugins_d.cfg.in ${MYGUI_BINARY_DIR}/bin/debug/plugins.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/plugins.cfg.in ${MYGUI_BINARY_DIR}/bin/release/plugins.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/plugins.cfg.in ${MYGUI_BINARY_DIR}/bin/relwithdebinfo/plugins.cfg)
   configure_file(${MYGUI_TEMPLATES_DIR}/plugins.cfg.in ${MYGUI_BINARY_DIR}/bin/minsizerel/plugins.cfg)
@@ -62,6 +75,8 @@ else() # other OS only need one cfg file
   if (MYGUI_BUILD_TYPE STREQUAL "debug" AND NOT APPLE)
     set(MYGUI_CFG_SUFFIX "_d")
   endif ()
+  # create resources.xml
+  configure_file(${MYGUI_TEMPLATES_DIR}/resources.xml.in ${MYGUI_BINARY_DIR}/bin/resources.xml)
   # create resources.cfg
   configure_file(${MYGUI_TEMPLATES_DIR}/resources.cfg.in ${MYGUI_BINARY_DIR}/bin/resources.cfg)
   # create plugins.cfg
