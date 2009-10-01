cecb363b1620be586d3266f55f32ed77cde89a90
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2009-10-01 01:30:44 +0000

    fix utf8 input

--- trunk/Common/Input/OIS/InputManager.cpp
+++ trunk/Common/Input/OIS/InputManager.cpp
@@ -23,8 +23,73 @@
 #include "precompiled.h"
 #include "InputManager.h"
 
+#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
+#include <windows.h>
+#endif
+
 namespace input
 {
+
+#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
+
+	MyGUI::Char translateWin32Text(MyGUI::KeyCode kc)
+	{
+		static WCHAR deadKey = 0;
+
+		BYTE keyState[256];
+		HKL  layout = GetKeyboardLayout(0);
+		if ( GetKeyboardState(keyState) == 0 )
+			return 0;
+
+		int code = *((int*)&kc);
+		unsigned int vk = MapVirtualKeyEx((UINT)code, 3, layout);
+		if ( vk == 0 )
+			return 0;
+
+		WCHAR buff[3] = { 0, 0, 0 };
+		int ascii = ToUnicodeEx(vk, (UINT)code, keyState, buff, 3, 0, layout);
+		if (ascii == 1 && deadKey != '\0' )
+		{
+			// A dead key is stored and we have just converted a character key
+			// Combine the two into a single character
+			WCHAR wcBuff[3] = { buff[0], deadKey, '\0' };
+			WCHAR out[3];
+
+			deadKey = '\0';
+			if(FoldStringW(MAP_PRECOMPOSED, (LPWSTR)wcBuff, 3, (LPWSTR)out, 3))
+				return out[0];
+		}
+		else if (ascii == 1)
+		{
+			// We have a single character
+			deadKey = '\0';
+			return buff[0];
+		}
+		else if(ascii == 2)
+		{
+			// Convert a non-combining diacritical mark into a combining diacritical mark
+			// Combining versions range from 0x300 to 0x36F; only 5 (for French) have been mapped below
+			// http://www.fileformat.info/info/unicode/block/combining_diacritical_marks/images.htm
+			switch(buff[0])	{
+			case 0x5E: // Circumflex accent: â
+				deadKey = 0x302; break;
+			case 0x60: // Grave accent: à
+				deadKey = 0x300; break;
+			case 0xA8: // Diaeresis: ü
+				deadKey = 0x308; break;
+			case 0xB4: // Acute accent: é
+				deadKey = 0x301; break;
+			case 0xB8: // Cedilla: ç
+				deadKey = 0x327; break;
+			default:
+				deadKey = buff[0]; break;
+			}
+		}
+
+		return 0;
+	}
+
+#endif
 
 	InputManager::InputManager() :
 		mInputManager(0),
@@ -93,7 +158,14 @@ namespace input
 
 	bool InputManager::keyPressed(const OIS::KeyEvent& _arg)
 	{
-		injectKeyPress(MyGUI::KeyCode::Enum(_arg.key), (MyGUI::Char)_arg.text);
+		MyGUI::Char text = (MyGUI::Char)_arg.text;
+		MyGUI::KeyCode key = MyGUI::KeyCode::Enum(_arg.key);
+
+#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
+		text = translateWin32Text(key);
+#endif
+
+		injectKeyPress(key, text);
 		return true;
 	}
 
