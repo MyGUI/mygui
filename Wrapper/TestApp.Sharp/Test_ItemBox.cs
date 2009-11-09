2f72297a516e19c2f4e69dac9dab3cf2f4eecec8
my_name <mynameco@users.noreply.github.com>
2009-11-09 06:30:54 +0000

    update wrappers

--- Wrapper/TestApp.Sharp/Test_ItemBox.cs
+++ Wrapper/TestApp.Sharp/Test_ItemBox.cs
@@ -9,7 +9,7 @@ namespace TestApp.Sharp
     {
         public static void Test()
         {
-            ItemBox box = Gui.Instance.CreateWidget<ItemBox>("ItemBox", new IntCoord(20, 320, 100, 100), Align.Default, "Main");
+            ItemBox box = Gui.Instance.CreateWidget<ItemBox>("ItemBoxV", new IntCoord(20, 320, 100, 100), Align.Default, "Main");
             box.EventNotifyItem += new ItemBox.HandleNotifyItem(box_EventNotifyItem);
             box.EventMouseItemActivate += new ItemBox.HandleMouseItemActivate(box_EventMouseItemActivate);
             box.EventChangeItemPosition += new ItemBox.HandleChangeItemPosition(box_EventChangeItemPosition);
