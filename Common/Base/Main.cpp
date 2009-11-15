56c45634419aa80dbf256d801cda3222eefa396e
my_name <mynameco@users.noreply.github.com>
2009-11-15 20:25:45 +0000

    fix

--- Common/Base/Main.cpp
+++ Common/Base/Main.cpp
@@ -7,7 +7,7 @@
 #include "precompiled.h"
 #include "DemoKeeper.h"
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#if WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
@@ -15,7 +15,7 @@
 extern "C" {
 #endif
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#if WIN32
 	int main(int argc, char **argv);
 	INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT argc) { return main(1, &strCmdLine); }
 	void OutException(const char * _caption, const char * _message) { ::MessageBox( NULL, _message, _caption, MB_OK | MB_ICONERROR | MB_TASKMODAL); }
