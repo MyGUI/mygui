5b817e161b70ca20c1a3f0dc3e78da2df4039a86
my_name <my.name.post@gmail.com>
2009-11-18 17:08:17 +0000

    update demos

---
+++ Demos/Demo_Pointers/ResourcePointerContext.cpp
@@ -0,0 +1,17 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		11/2009
+	@module
+*/
+
+#include "ResourcePointerContext.h"
+
+namespace demo
+{
+
+	void ResourcePointerContext::deserialization(MyGUI::xml::ElementPtr _node, MyGUI::Version _version)
+	{
+	}
+
+}
