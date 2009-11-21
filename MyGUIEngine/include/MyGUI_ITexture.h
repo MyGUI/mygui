1891bc7bc53dbc5756e2156838c88d29d3ecaa3f
my_name <my.name.post@gmail.com>
2009-11-21 19:25:20 +0000

    update texture interface

--- MyGUIEngine/include/MyGUI_ITexture.h
+++ MyGUIEngine/include/MyGUI_ITexture.h
@@ -63,6 +63,8 @@ namespace MyGUI
 		virtual TextureUsage getUsage() = 0;
 		virtual size_t getNumElemBytes() = 0;
 
+		virtual IRenderTarget* getRenderTarget() { return nullptr; }
+
 	};
 
 } // namespace MyGUI
