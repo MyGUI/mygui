3e05f78138f91ed5bfb13f3aa552d0db1ef62189
my_name <mynameco@users.noreply.github.com>
2009-11-25 19:05:13 +0000

    add unit test

--- UnitTests/UnitTest_Layers/DemoKeeper.cpp
+++ UnitTests/UnitTest_Layers/DemoKeeper.cpp
@@ -42,23 +42,31 @@ namespace demo
 	void DemoKeeper::setupResources()
 	{
 		base::BaseManager::setupResources();
-		addResourceLocation(getRootMedia() + "/UnitTests/UnitTest_TextureAnimations");
+		//addResourceLocation(getRootMedia() + "/UnitTests/UnitTest_TextureAnimations");
+		addResourceLocation(getRootMedia() + "/UnitTests/UnitTest_Layers");
 		addResourceLocation(getRootMedia() + "/Common/Scene");
 	}
 
     void DemoKeeper::createScene()
     {
-		Ogre::MeshManager::getSingleton().createPlane(
-			"FloorPlane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, 
-			Ogre::Plane(Ogre::Vector3::UNIT_Y, 0), 512, 512, 1, 1, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
+		Ogre::Entity* entity = getSceneManager()->createEntity("Plane01.mesh", "Plane01.mesh");
+		Ogre::SceneNode* node = getSceneManager()->getRootSceneNode()->createChildSceneNode();
+		node->attachObject(entity);
+
+		/*Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+		Ogre::MeshManager::getSingleton().createPlane("FloorPlane",
+			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plane,
+			512, 512, 1, 1, true, 1, 1, 1, Ogre::Vector3::UNIT_Z);
 
 		Ogre::Entity* entity = getSceneManager()->createEntity("FloorPlane", "FloorPlane");
 		entity->setMaterialName("Ground");
 		Ogre::SceneNode* node = getSceneManager()->getRootSceneNode()->createChildSceneNode();
-		node->attachObject(entity);
+		node->attachObject(entity);*/
 
-		getCamera()->setPosition(0, 512, -512);
+		getCamera()->setPosition(0, 150, 30);
 		getCamera()->lookAt(0, 0, 0);
+		getCamera()->getViewport()->setBackgroundColour(Ogre::ColourValue::Blue);
+		
 
 		//createDefaultScene();
 		//MyGUI::FactoryManager::getInstance().registryFactory<WobbleNodeAnimator>("NodeAnimator");
@@ -68,14 +76,24 @@ namespace demo
 
 		getGUI()->load("test_layer.xml");
 
-		widget = getGUI()->createWidget<MyGUI::Window>("WindowCSMX", MyGUI::IntCoord(0, 0, 512, 512), MyGUI::Align::Default, "RTT_Test");
+		widget = getGUI()->createWidget<MyGUI::Window>("WindowCSMX", MyGUI::IntCoord(150, 150, 200, 200), MyGUI::Align::Default, "RTT_Test");
 		widget->setCaption("Caption");
 
 		//widget->eventWindowButtonPressed = MyGUI::newDelegate(notifyWindowButtonPressed);
 
 		MyGUI::IntSize size(100, 24);
 		MyGUI::ButtonPtr button = widget->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord((widget->getClientCoord().width - size.width) / 2, (widget->getClientCoord().height - size.height) / 2, size.width, size.height), MyGUI::Align::Default);
-		button->setCaption(L"…а кнопко");
+		button->setCaption(L"…а хнопко");
+
+		MyGUI::ILayer* layer_g = MyGUI::LayerManager::getInstance().getByName("RTT_Test", false);
+		if (layer_g != nullptr)
+		{
+			MyGUI::RTTLayer* layer = layer_g->castType<MyGUI::RTTLayer>();
+			if (layer != nullptr)
+			{
+				layer->setEntity(entity->getName(), "Plane");
+			}
+		}
 
 	}
 
