b897b41bff576e4ec90d3716f7b29d7ebc45b310
my_name <my.name.post@gmail.com>
2009-11-25 19:05:13 +0000

    add unit test

--- UnitTests/UnitTest_Layers/MyGUI_RTTLayer.h
+++ UnitTests/UnitTest_Layers/MyGUI_RTTLayer.h
@@ -11,6 +11,7 @@
 #include "MyGUI_Prerequest.h"
 #include "MyGUI_Types.h"
 #include "MyGUI_OverlappedLayer.h"
+#include <Ogre.h>
 
 namespace MyGUI
 {
@@ -29,8 +30,24 @@ namespace MyGUI
 
 		virtual ILayerItem* getLayerItemByPoint(int _left, int _top);
 
+		virtual IntPoint getPosition(int _left, int _top);
+
+		void setEntity(const std::string& _name, const std::string& _material);
+
+	private:
+		void clear();
+		bool isIntersectMesh(IntPoint& _point, const Ogre::Ray& _ray);
+		void getCoordByTriangle(IntPoint& _point, Ogre::Vector3 _position, const Ogre::Vector3& _corner0, const Ogre::Vector3& _corner1, const Ogre::Vector3& _corner2);
+
 	private:
 		MyGUI::ITexture* mTexture;
+		std::string mEntityName;
+
+		Ogre::Vector2* mTextureCoords;
+		Ogre::Vector3* mVertices;
+		unsigned long* mIndices;
+		size_t mVertexCount;
+		size_t mIndexCount;
 	};
 
 } // namespace MyGUI
