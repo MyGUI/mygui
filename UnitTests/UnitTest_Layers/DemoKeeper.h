52dc90e39ca95c660d312060ef47836e973a66bc
my_name <my.name.post@gmail.com>
2009-12-02 21:13:28 +0000

    update test

--- UnitTests/UnitTest_Layers/DemoKeeper.h
+++ UnitTests/UnitTest_Layers/DemoKeeper.h
@@ -8,19 +8,26 @@
 #define __DEMO_KEEPER_H__
 
 #include "Base/BaseManager.h"
+#include "KeyboardPanel.h"
 
 namespace demo
 {
 
-	class DemoKeeper : public base::BaseManager
+	class DemoKeeper :
+		public base::BaseManager
 	{
 	public:
+		DemoKeeper();
+
 		virtual void createScene();
 		virtual void destroyScene();
 
 	private:
 		virtual void injectKeyPress(MyGUI::KeyCode _key, MyGUI::Char _text);
 		void setupResources();
+
+	private:
+		KeyboardPanel* mKeyboardPanel;
 
 	};
 
---
+++ UnitTests/UnitTest_Layers/KeyboardPanel.h
@@ -0,0 +1,40 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		11/2009
+	@module
+*/
+#ifndef __KEYBOARD_PANEL_H__
+#define __KEYBOARD_PANEL_H__
+
+#include <MyGUI.h>
+#include "BaseLayout/BaseLayout.h"
+
+namespace demo
+{
+
+	ATTRIBUTE_CLASS_LAYOUT(KeyboardPanel, "Keyboard.layout");
+	class KeyboardPanel :
+		public wraps::BaseLayout
+	{
+	public:
+		KeyboardPanel();
+		virtual ~KeyboardPanel();
+
+	private:
+		void notifyMouseButtonClick(MyGUI::WidgetPtr _sender);
+
+	private:
+		ATTRIBUTE_FIELD_WIDGET_NAME(KeyboardPanel, mButtonEngine, "ButtonEngine");
+		MyGUI::Button* mButtonEngine;
+		ATTRIBUTE_FIELD_WIDGET_NAME(KeyboardPanel, mButtonGun, "ButtonGun");
+		MyGUI::Button* mButtonGun;
+		ATTRIBUTE_FIELD_WIDGET_NAME(KeyboardPanel, mButtonAmmo, "ButtonAmmo");
+		MyGUI::Button* mButtonAmmo;
+		ATTRIBUTE_FIELD_WIDGET_NAME(KeyboardPanel, mButtonArmor, "ButtonArmor");
+		MyGUI::Button* mButtonArmor;
+	};
+
+} // namespace demo
+
+#endif // __KEYBOARD_PANEL_H__
