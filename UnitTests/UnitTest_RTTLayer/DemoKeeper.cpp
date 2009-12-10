09befdcc43a9ff760caa19bece924a38b51684df
my_name <mynameco@users.noreply.github.com>
2009-12-10 14:03:41 +0000

    add new unit test UnitTest_RTTLayer

---
+++ UnitTests/UnitTest_RTTLayer/DemoKeeper.cpp
@@ -0,0 +1,39 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		12/2009
+	@module
+*/
+#include "DemoKeeper.h"
+#include "Base/Main.h"
+#include "MyGUI_RTTLayer.h"
+
+namespace demo
+{
+
+	DemoKeeper::DemoKeeper()
+	{
+	}
+
+	void DemoKeeper::setupResources()
+	{
+		base::BaseManager::setupResources();
+		addResourceLocation(getRootMedia() + "/UnitTests/UnitTest_RTTLayer");
+	}
+
+    void DemoKeeper::createScene()
+    {
+		MyGUI::FactoryManager::getInstance().registryFactory<MyGUI::RTTLayer>("Layer");
+
+		getGUI()->load("Layers.xml");
+		getGUI()->load("Layers.layout");
+	}
+
+    void DemoKeeper::destroyScene()
+    {
+		MyGUI::FactoryManager::getInstance().unregistryFactory<MyGUI::RTTLayer>("Layer");
+    }
+
+} // namespace demo
+
+MYGUI_APP(demo::DemoKeeper)
