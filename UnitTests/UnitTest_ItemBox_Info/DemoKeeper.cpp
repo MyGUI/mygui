4a337e37e7f93497f2b91709491d36920e0d677f
my_name <mynameco@users.noreply.github.com>
2010-01-07 11:28:42 +0000

    update StackPanel

--- UnitTests/UnitTest_ItemBox_Info/DemoKeeper.cpp
+++ UnitTests/UnitTest_ItemBox_Info/DemoKeeper.cpp
@@ -144,32 +144,33 @@ namespace demo
 
 		MyGUI::StackPanel* panel = window->createWidget<MyGUI::StackPanel>("Panel", MyGUI::IntCoord(), MyGUI::Align::Default);
 		panel->setFlowToDirection(MyGUI::Align::Bottom);
-		panel->setThickness(MyGUI::IntRect(10, 10, 10, 10));
+		//panel->setThickness(MyGUI::IntRect(10, 10, 10, 10));
 		panel->setPadding(MyGUI::IntRect(10, 10, 10, 10));
 		panel->setSpacer(10);
 
 		MyGUI::StackPanel* panel2 = panel->createWidget<MyGUI::StackPanel>("Panel", MyGUI::IntCoord(), MyGUI::Align::Stretch);
 		panel2->setFlowToDirection(MyGUI::Align::Right);
+		panel2->setPadding(MyGUI::IntRect(10, 10, 10, 10));
 
 		MyGUI::StaticImage* image = panel2->createWidget<MyGUI::StaticImage>("StaticImage", MyGUI::IntCoord(), MyGUI::Align::Default);
 		image->setItemResource("pic_CoreMessageIcon");
 
-		MyGUI::StaticText* text = panel2->createWidget<MyGUI::StaticText>("Button", MyGUI::IntCoord(), MyGUI::Align::Default);
+		MyGUI::StaticText* text = panel2->createWidget<MyGUI::StaticText>("Button", MyGUI::IntCoord(), MyGUI::Align::Stretch);
 		text->setCaption("Error loading file name, retry again.");
 		text->setTextAlign(MyGUI::Align::Center);
-		//text->setThickness(MyGUI::IntRect(10, 10, 10, 10));
 
 		MyGUI::StackPanel* panel3 = panel->createWidget<MyGUI::StackPanel>("Panel", MyGUI::IntCoord(), MyGUI::Align::Center);
 		panel3->setFlowToDirection(MyGUI::Align::Right);
 		panel3->setUniform(true);
 		panel3->setSpacer(10);
+		panel3->setPadding(MyGUI::IntRect(10, 10, 10, 10));
 
 		MyGUI::Button* button1 = panel3->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(), MyGUI::Align::Stretch);
 		button1->setCaption("Ok");
 		button1->setPadding(MyGUI::IntRect(10, 2, 10, 2));
 
 		MyGUI::Button* button2 = panel3->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(), MyGUI::Align::Stretch);
-		button2->setCaption("Cancel");
+		button2->setCaption("Cancel35413452352345235234");
 		button2->setPadding(MyGUI::IntRect(10, 2, 10, 2));
 
 		window->setSizeToContent(true);
