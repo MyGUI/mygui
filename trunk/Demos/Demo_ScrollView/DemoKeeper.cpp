7e3330d6bb7ef4f304787337c99acfc41ef45b07
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-01-14 13:57:41 +0000

    fix for setMinSize interface change

--- trunk/Demos/Demo_ScrollView/DemoKeeper.cpp
+++ trunk/Demos/Demo_ScrollView/DemoKeeper.cpp
@@ -28,11 +28,11 @@ namespace demo
 		const MyGUI::IntSize size(450, 450);
 
 		MyGUI::Window* window = MyGUI::Gui::getInstance().createWidget<MyGUI::Window>("WindowCS", MyGUI::IntCoord((view.width - size.width) / 2, (view.height - size.height) / 2, size.width, size.height), MyGUI::Align::Default, "Main");
-		window->setMinSize(150, 150);
+		window->setMinSize(MyGUI::IntSize(150, 150));
 		window->setCaption("ScrollView demo");
 		MyGUI::ScrollView* scroll_view = window->createWidget<MyGUI::ScrollView>("ScrollView", MyGUI::IntCoord(2, 2, window->getClientCoord().width-2, window->getClientCoord().height-2), MyGUI::Align::Stretch);
 
-		scroll_view->setCanvasSize(256, 256);
+		scroll_view->setCanvasSize(MyGUI::IntSize(256, 256));
 		MyGUI::StaticImage* image = scroll_view->createWidget<MyGUI::StaticImage>("StaticImage", MyGUI::IntCoord(0, 0, 256, 256), MyGUI::Align::Default);
 		image->setImageTexture("Crystal_Clear_View.png");
 	}
