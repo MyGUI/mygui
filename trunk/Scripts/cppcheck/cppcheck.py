28b274fcb6a4199e090319c1a41d71855731a9ab
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-02-07 01:35:32 +0000

    fix cppcheck script

--- trunk/Scripts/cppcheck/cppcheck.py
+++ trunk/Scripts/cppcheck/cppcheck.py
@@ -2,22 +2,22 @@
 import os
 
 def checkFolderSources(folder) :
-	os.system("cppcheck --enable=all -I MyGUIEngine/Scripts/cppheck " + folder)
+	os.system("cppcheck --enable=all -I Scripts/cppcheck " + folder)
 
 
-os.system("cppcheck --enable=all -I Scripts/cppheck -I MyGUIEngine/include MyGUIEngine/src")
 #checkFolderSources('MyGUIEngine')
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I MyGUIEngine/include MyGUIEngine/src")
 
 checkFolderSources('Demos')
 checkFolderSources('Tools')
 checkFolderSources('UnitTests')
 checkFolderSources('Common')
 #checkFolderSources('Platforms/OpenGL')
-os.system("cppcheck --enable=all -I MyGUIEngine/Scripts/cppheck -I Platforms/OpenGL/OpenGLPlatform/include Platforms/OpenGL/OpenGLPlatform/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/OpenGL/OpenGLPlatform/include Platforms/OpenGL/OpenGLPlatform/src")
 #checkFolderSources('Platforms/Ogre')
-os.system("cppcheck --enable=all -I MyGUIEngine/Scripts/cppheck -I Platforms/Ogre/OgrePlatform/include Platforms/Ogre/OgrePlatform/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/Ogre/OgrePlatform/include Platforms/Ogre/OgrePlatform/src")
 #checkFolderSources('Platforms/DirectX')
-os.system("cppcheck --enable=all -I MyGUIEngine/Scripts/cppheck -I Platforms/DirectX/DirectXPlatform/include Platforms/DirectX/DirectXPlatform/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/DirectX/DirectXPlatform/include Platforms/DirectX/DirectXPlatform/src")
 checkFolderSources('Platforms/OpenGL')
 checkFolderSources('Platforms/OGRE')
 checkFolderSources('Platforms/DirectX')
