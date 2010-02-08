5a4093616f5627c06890a8ddf25d9eecfec2de05
George Evmenov <Altren@users.noreply.github.com>
2010-02-08 17:45:47 +0000

    update cppcheck script and related files

---
+++ Scripts/cppcheck/MyGUI_Common.h
@@ -0,0 +1,38 @@
+#ifndef __MYGUI_COMMON_H__
+#define __MYGUI_COMMON_H__
+
+#include "MyGUI_Prerequest.h"
+
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+#include <vector>
+#include <deque>
+#include <exception>
+#include <string.h>
+#include <math.h>
+
+//#ifdef MYGUI_CUSTOM_ALLOCATOR
+//#    include "MyGUI_CustomAllocator.h"
+//#else // MYGUI_CUSTOM_ALLOCATOR
+#    include "MyGUI_Allocator.h"
+//#endif // MYGUI_CUSTOM_ALLOCATOR
+
+#include "MyGUI_Macros.h"
+#include "MyGUI_LogManager.h"
+#include "MyGUI_Instance.h"
+#include "MyGUI_Types.h"
+#include "MyGUI_RenderOut.h"
+#include "MyGUI_Utility.h"
+#include "MyGUI_InputDefine.h"
+#include "MyGUI_Version.h"
+#include "MyGUI_WidgetStyle.h"
+#include "MyGUI_SizePolicy.h"
+#include "MyGUI_FlowDirection.h"
+#include "MyGUI_UString.h"
+#include "MyGUI_Diagnostic.h"
+#include "MyGUI_Delegate.h"
+
+#endif // __MYGUI_COMMON_H__
+
