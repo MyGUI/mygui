1b1b6e24bbcf1d7a1f73c560148522bf4af856bf
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-02-20 20:03:14 +0000

    fix InputManager for samples

--- tags/MyGUI3.0/Common/precompiled.h
+++ tags/MyGUI3.0/Common/precompiled.h
@@ -1,7 +1,15 @@
 #ifdef MYGUI_OGRE_PLATFORM
 //#  define MYGUI_CUSTOM_ALLOCATOR
 //#  include <Ogre.h>
+
+#ifdef MYGUI_SAMPLES_INPUT_OIS
 #  include <OIS.h>
+#elif MYGUI_SAMPLES_INPUT_WIN32
+//#  include <OIS.h>
+#elif MYGUI_SAMPLES_INPUT_WIN32_OIS
+#  include <OIS.h>
+#endif
+
 #elif  MYGUI_DIRECTX_PLATFORM
 #elif  MYGUI_OPENGL_PLATFORM
 #endif
