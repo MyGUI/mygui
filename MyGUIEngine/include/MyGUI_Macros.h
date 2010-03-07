527e7a148b4d284a92735ae774759325c0dc8e33
George Evmenov <Altren@users.noreply.github.com>
2010-03-07 23:52:39 +0000

    change finding Ogre: use original FingOGRE.cmake that lies in OGRE_SOURCE directory, also OGRE_BUILD might be used if build is not in same directory as OGRE_SOURCE

--- MyGUIEngine/include/MyGUI_Macros.h
+++ MyGUIEngine/include/MyGUI_Macros.h
@@ -22,11 +22,13 @@
 #ifndef __MYGUI_MACROS_H__
 #define __MYGUI_MACROS_H__
 
+#include <limits>
+
 namespace MyGUI
 {
 
-	const size_t ITEM_NONE = ~(size_t)0;
-	const int DEFAULT ((int)-1);
+	const size_t ITEM_NONE = std::numeric_limits<size_t>::max();
+	const int DEFAULT = -1;
 	const float ALPHA_MAX = 1.0f;
 	const float ALPHA_MIN = 0.0f;
 
@@ -34,7 +36,7 @@ namespace MyGUI
 	#define MYGUI_FLAG_NONE  0
 	#define MYGUI_FLAG(num)  (1<<(num))
 
-	const int MAX_COORD = 2147483647;
+	const int MAX_COORD = std::numeric_limits<int>::max();
 
 } // namespace MyGUI
 
