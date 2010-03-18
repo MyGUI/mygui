ff959fbc844ce4bd352d6548a63a8eb89a73900a
my_name <mynameco@users.noreply.github.com>
2010-03-18 23:33:31 +0000

    add ToolTipManager and minor refactoring containers

--- MyGUIEngine/include/MyGUI_ListCtrl.h
+++ MyGUIEngine/include/MyGUI_ListCtrl.h
@@ -208,7 +208,8 @@ namespace MyGUI
 		void notifyMouseWheel(Widget* _sender, int _rel);
 		//void notifyEventMouseRootFocusChanged(Widget* _sender, EventInfo* _info, FocusChangedEventArgs* _args);
 		void notifyMouseButtonDoubleClick(Widget* _sender);
-		void _requestGetContainer(Widget* _sender, Widget*& _container, size_t& _index);
+		//void _requestGetContainer(Widget* _sender, Widget*& _container, size_t& _index);
+		virtual size_t _getItemIndex(Widget* _item);
 		void notifyMouseDrag(Widget* _sender, int _left, int _top);
 		void notifyMouseButtonPressed(Widget* _sender, int _left, int _top, MouseButton _id);
 		void notifyMouseButtonReleased(Widget* _sender, int _left, int _top, MouseButton _id);
