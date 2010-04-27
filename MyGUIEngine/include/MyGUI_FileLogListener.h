f51303f61ee94ceb79c9d6c82d5c06936c942391
my_name <mynameco@users.noreply.github.com>
2010-04-27 21:26:58 +0000

    add log system files

---
+++ MyGUIEngine/include/MyGUI_FileLogListener.h
@@ -0,0 +1,51 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		04/2010
+*/
+/*
+	This file is part of MyGUI.
+
+	MyGUI is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	MyGUI is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with MyGUI.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __MYGUI_FILE_LOG_LISTENER_H__
+#define __MYGUI_FILE_LOG_LISTENER_H__
+
+#include "MyGUI_Prerequest.h"
+#include "MyGUI_ILogListener.h"
+#include <fstream>
+//#include <iostream>
+#include <string>
+
+namespace MyGUI
+{
+	class MYGUI_EXPORT FileLogListener :
+		public ILogListener
+	{
+	public:
+		FileLogListener(const std::string& _fileName);
+		virtual ~FileLogListener() { }
+
+		virtual void open();
+		virtual void close();
+		virtual void flush();
+		virtual void log(const std::string& _section, LogLevel _level, const struct tm* _time, const std::string& _message, const char* _file, int _line);
+
+	private:
+		std::ofstream mStream;
+		std::string mFileName;
+	};
+}
+
+#endif // __MYGUI_FILE_LOG_LISTENER_H__
