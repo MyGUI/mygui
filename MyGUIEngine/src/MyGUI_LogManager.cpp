2e005d0a2e07780ab144afd650e3fcabdbf958dd
George Evmenov <Altren@users.noreply.github.com>
2010-05-12 22:40:14 +0000

    Ta-Da!

--- MyGUIEngine/src/MyGUI_LogManager.cpp
+++ MyGUIEngine/src/MyGUI_LogManager.cpp
@@ -121,7 +121,7 @@ namespace MyGUI
 			mConsole->setEnable(_value);
 	}
 
-	bool LogManager::getSTDOutputEnabled()
+	bool LogManager::getSTDOutputEnabled() const
 	{
 		return mConsoleEnable;
 	}
@@ -134,7 +134,7 @@ namespace MyGUI
 			mFilter->setLoggingLevel(_value);
 	}
 
-	LogLevel LogManager::getLoggingLevel()
+	LogLevel LogManager::getLoggingLevel() const
 	{
 		return mLevel;
 	}
