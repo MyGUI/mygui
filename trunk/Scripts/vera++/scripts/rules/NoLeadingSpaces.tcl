013f47252d137a21b43b5aa8a3c43409210b3429
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-05-31 19:53:48 +0000

    update NoLeadingSpaces vera++ rule

--- trunk/Scripts/vera++/scripts/rules/NoLeadingSpaces.tcl
+++ trunk/Scripts/vera++/scripts/rules/NoLeadingSpaces.tcl
@@ -4,7 +4,7 @@ foreach f [getSourceFileNames] {
     set lineNumber 1
     foreach line [getAllLines $f] {
 
-        if [regexp {^ } $line] {
+        if [regexp {^\t* } $line] {
             report $f $lineNumber "leading spaces used instead tabs"
         }
 
