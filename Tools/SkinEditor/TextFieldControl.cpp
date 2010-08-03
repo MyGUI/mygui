06f96314b78851bd05d4625abe3f50504a62904b
my_name <my.name.post@gmail.com>
2010-08-03 14:12:24 +0000

    add text field

---
+++ Tools/SkinEditor/TextFieldControl.cpp
@@ -0,0 +1,21 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		08/2010
+*/
+#include "precompiled.h"
+#include "TextFieldControl.h"
+
+namespace tools
+{
+
+	TextFieldControl::TextFieldControl() :
+		wraps::BaseLayout("TextFieldControl.layout")
+	{
+	}
+
+	TextFieldControl::~TextFieldControl()
+	{
+	}
+
+} // namespace tools
