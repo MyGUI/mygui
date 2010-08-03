a35a633a3023fad1b955c8872284857ad2832ede
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-03 16:24:16 +0000

    update text field

--- trunk/Tools/SkinEditor/TextFieldControl.cpp
+++ trunk/Tools/SkinEditor/TextFieldControl.cpp
@@ -10,12 +10,48 @@ namespace tools
 {
 
 	TextFieldControl::TextFieldControl() :
-		wraps::BaseLayout("TextFieldControl.layout")
+		wraps::BaseLayout("TextFieldControl.layout"),
+		mText(nullptr),
+		mOk(nullptr),
+		mCancel(nullptr)
 	{
+		assignWidget(mText, "Text");
+		assignWidget(mOk, "Ok");
+		assignWidget(mCancel, "Cancel");
+
+		mOk->eventMouseButtonClick += MyGUI::newDelegate(this, &TextFieldControl::notifyOk);
+		mCancel->eventMouseButtonClick += MyGUI::newDelegate(this, &TextFieldControl::notifyCancel);
 	}
 
 	TextFieldControl::~TextFieldControl()
 	{
+		mOk->eventMouseButtonClick -= MyGUI::newDelegate(this, &TextFieldControl::notifyOk);
+		mCancel->eventMouseButtonClick -= MyGUI::newDelegate(this, &TextFieldControl::notifyCancel);
+	}
+
+	void TextFieldControl::notifyOk(MyGUI::Widget* _sender)
+	{
+	}
+
+	void TextFieldControl::notifyCancel(MyGUI::Widget* _sender)
+	{
+	}
+
+	void TextFieldControl::setCaption(const MyGUI::UString& _value)
+	{
+		MyGUI::Window* window = mMainWidget->castType<MyGUI::Window>(false);
+		if (window != nullptr)
+			window->setCaption(_value);
+	}
+
+	void TextFieldControl::setTextField(const MyGUI::UString& _value)
+	{
+		mText->setCaption(_value);
+	}
+
+	const MyGUI::UString& TextFieldControl::getTextField()
+	{
+		return mText->getCaption();
 	}
 
 } // namespace tools
