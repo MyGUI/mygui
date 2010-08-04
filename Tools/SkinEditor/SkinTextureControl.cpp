9224d6f883d27c35ab775bd358fae5253bc08629
my_name <my.name.post@gmail.com>
2010-08-04 11:31:00 +0000

    SE: add texture control

---
+++ Tools/SkinEditor/SkinTextureControl.cpp
@@ -0,0 +1,21 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		08/2010
+*/
+#include "precompiled.h"
+#include "SkinTextureControl.h"
+
+namespace tools
+{
+
+	SkinTextureControl::SkinTextureControl(MyGUI::Widget* _parent) :
+		wraps::BaseLayout("SkinTextureControl.layout", _parent)
+	{
+	}
+
+	SkinTextureControl::~SkinTextureControl()
+	{
+	}
+
+} // namespace tools
