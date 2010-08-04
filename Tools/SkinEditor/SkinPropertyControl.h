bc28a59166fbded75910ed4d17c3202afa058e95
my_name <mynameco@users.noreply.github.com>
2010-08-04 00:52:07 +0000

    SE: update

--- Tools/SkinEditor/SkinPropertyControl.h
+++ Tools/SkinEditor/SkinPropertyControl.h
@@ -18,6 +18,11 @@ namespace tools
 		SkinPropertyControl(MyGUI::Widget* _parent);
 		virtual ~SkinPropertyControl();
 
+	private:
+		void fillTextures();
+
+	private:
+		MyGUI::ComboBox* mTextures;
 	};
 
 } // namespace tools
