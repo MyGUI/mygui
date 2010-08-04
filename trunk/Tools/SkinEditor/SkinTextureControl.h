c3de6da62b603c551f1b9c21c6cd4932732d452f
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-04 12:17:36 +0000

    SE: add texture panel

--- trunk/Tools/SkinEditor/SkinTextureControl.h
+++ trunk/Tools/SkinEditor/SkinTextureControl.h
@@ -7,6 +7,8 @@
 #define __SKIN_TEXTURE_CONTROL_H__
 
 #include "BaseLayout/BaseLayout.h"
+#include "Property.h"
+#include "SkinItem.h"
 
 namespace tools
 {
@@ -18,6 +20,28 @@ namespace tools
 		SkinTextureControl(MyGUI::Widget* _parent);
 		virtual ~SkinTextureControl();
 
+	private:
+		void notifyChangeSelection();
+		void notifyChangeProperty(Property* _sender, const MyGUI::UString& _owner);
+		void notifyComboChangePosition(MyGUI::ComboBox* _sender, size_t _index);
+
+		void unadvice();
+		void advice();
+
+		void updateAllProperties();
+		void updateTexture();
+
+		void fillColours(MyGUI::ComboBox* _combo);
+		void updateColour(MyGUI::ComboBox* _sender);
+
+	private:
+		MyGUI::ScrollView* mView;
+		MyGUI::StaticImage* mTexture;
+		MyGUI::ComboBox* mBackgroundColour;
+		MyGUI::Widget* mBackground;
+
+		SkinItem* mCurrentSkin;
+		MyGUI::UString mTypeName;
 	};
 
 } // namespace tools
