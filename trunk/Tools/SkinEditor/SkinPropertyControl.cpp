6d4ecab05cb9f3cccb728abb57867e340beea6ed
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-04 00:52:07 +0000

    SE: update

--- trunk/Tools/SkinEditor/SkinPropertyControl.cpp
+++ trunk/Tools/SkinEditor/SkinPropertyControl.cpp
@@ -10,12 +10,28 @@ namespace tools
 {
 
 	SkinPropertyControl::SkinPropertyControl(MyGUI::Widget* _parent) :
-		wraps::BaseLayout("SkinPropertyControl.layout", _parent)
+		wraps::BaseLayout("SkinPropertyControl.layout", _parent),
+		mTextures(nullptr)
 	{
+		assignWidget(mTextures, "Textures");
+
+		fillTextures();
 	}
 
 	SkinPropertyControl::~SkinPropertyControl()
 	{
 	}
 
+	void SkinPropertyControl::fillTextures()
+	{
+		mTextures->removeAllItems();
+
+		MyGUI::VectorString paths = MyGUI::DataManager::getInstance().getDataListNames("*.png");
+		for (MyGUI::VectorString::iterator iter=paths.begin(); iter!=paths.end(); ++iter)
+			mTextures->addItem(*iter);
+
+		paths = MyGUI::DataManager::getInstance().getDataListNames("*.jpg");
+		for (MyGUI::VectorString::iterator iter=paths.begin(); iter!=paths.end(); ++iter)
+			mTextures->addItem(*iter);
+	}
 } // namespace tools
