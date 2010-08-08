ab1c01631752c50dee5d3d4734ef3e67e10702c0
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-08 19:58:29 +0000

    SE: refactoring selector control

--- trunk/Tools/SkinEditor/PositionSelectorControl.cpp
+++ trunk/Tools/SkinEditor/PositionSelectorControl.cpp
@@ -9,82 +9,13 @@
 namespace tools
 {
 
-	RegionSelectorControl2::RegionSelectorControl2(MyGUI::Widget* _parent) :
-		wraps::BaseLayout("PositionSelectorControl.layout", _parent),
-		mScaleValue(1)
+	PositionSelectorControl::PositionSelectorControl(MyGUI::Widget* _parent) :
+		SelectorControl("PositionSelectorControl.layout", _parent)
 	{
-		mCoordReal = mMainWidget->getCoord();
-
-		MyGUI::Window* window = mMainWidget->castType<MyGUI::Window>(false);
-		if (window != nullptr)
-			window->eventWindowChangeCoord += MyGUI::newDelegate(this, &RegionSelectorControl2::notifyWindowChangeCoord);
 	}
 
-	RegionSelectorControl2::~RegionSelectorControl2()
+	PositionSelectorControl::~PositionSelectorControl()
 	{
-		MyGUI::Window* window = mMainWidget->castType<MyGUI::Window>(false);
-		if (window != nullptr)
-			window->eventWindowChangeCoord -= MyGUI::newDelegate(this, &RegionSelectorControl2::notifyWindowChangeCoord);
-	}
-
-	void RegionSelectorControl2::setVisible(bool _value)
-	{
-		mMainWidget->setVisible(_value);
-	}
-
-	void RegionSelectorControl2::setSize(const MyGUI::IntSize& _value)
-	{
-		mCoordValue = _value;
-		updateCoord();
-	}
-
-	void RegionSelectorControl2::setPosition(const MyGUI::IntPoint& _value)
-	{
-		mCoordValue = _value;
-		updateCoord();
-	}
-
-	void RegionSelectorControl2::setScale(double _value)
-	{
-		mScaleValue = _value;
-		updateCoord();
-	}
-
-	void RegionSelectorControl2::updateCoord()
-	{
-		mCoordReal.left = (int)((double)mCoordValue.left * mScaleValue);
-		mCoordReal.top = (int)((double)mCoordValue.top * mScaleValue);
-		mCoordReal.width = (int)((double)mCoordValue.width * mScaleValue);
-		mCoordReal.height = (int)((double)mCoordValue.height * mScaleValue);
-
-		mMainWidget->setCoord(mCoordReal);
-	}
-
-	void RegionSelectorControl2::notifyWindowChangeCoord(MyGUI::Window* _sender)
-	{
-		MyGUI::IntCoord coord = _sender->getCoord();
-
-		mCoordValue.left = (int)((double)coord.left / mScaleValue);
-		mCoordValue.top = (int)((double)coord.top / mScaleValue);
-
-		if (mCoordReal.left != coord.left && mCoordReal.width != coord.width)
-			mCoordValue.width = (int)((double)coord.right() / mScaleValue) - mCoordValue.left;
-		else
-			mCoordValue.width = (int)((double)coord.width / mScaleValue);
-
-		if (mCoordReal.top != coord.top && mCoordReal.height != coord.height)
-			mCoordValue.height = (int)((double)coord.bottom() / mScaleValue) - mCoordValue.top;
-		else
-			mCoordValue.height = (int)((double)coord.height / mScaleValue);
-
-		updateCoord();
-
-		eventChangePosition();
-	}
-
-	MyGUI::IntPoint RegionSelectorControl2::getPosition()
-	{
-		return mCoordValue.point();
 	}
 
 } // namespace tools
--- trunk/Tools/SkinEditor/PositionSelectorControl.h
+++ trunk/Tools/SkinEditor/PositionSelectorControl.h
@@ -3,43 +3,22 @@
 	@author		Albert Semenov
 	@date		08/2010
 */
-#ifndef __REGION_SELECTOR_CONTROL_2_H__
-#define __REGION_SELECTOR_CONTROL_2_H__
+#ifndef __POSITION_SELECTOR_CONTROL_H__
+#define __POSITION_SELECTOR_CONTROL_H__
 
-#include "BaseLayout/BaseLayout.h"
+#include "SelectorControl.h"
 
 namespace tools
 {
 
-	typedef MyGUI::delegates::CMultiDelegate0 EventHandle_ChangePosition;
-
-	class RegionSelectorControl2 :
-		public wraps::BaseLayout
+	class PositionSelectorControl :
+		public SelectorControl
 	{
 	public:
-		RegionSelectorControl2(MyGUI::Widget* _parent);
-		virtual ~RegionSelectorControl2();
-
-		void setVisible(bool _value);
-		void setScale(double _value);
-
-		void setPosition(const MyGUI::IntPoint& _value);
-		void setSize(const MyGUI::IntSize& _value);
-		MyGUI::IntPoint getPosition();
-
-		EventHandle_ChangePosition eventChangePosition;
-
-	private:
-		void notifyWindowChangeCoord(MyGUI::Window* _sender);
-
-		void updateCoord();
-
-	private:
-		MyGUI::IntCoord mCoordValue;
-		MyGUI::IntCoord mCoordReal;
-		double mScaleValue;
+		PositionSelectorControl(MyGUI::Widget* _parent);
+		virtual ~PositionSelectorControl();
 	};
 
 } // namespace tools
 
-#endif // __REGION_SELECTOR_CONTROL_H__
+#endif // __POSITION_SELECTOR_CONTROL_H__
