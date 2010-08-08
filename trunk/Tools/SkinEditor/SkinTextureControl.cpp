e011877ff8d9004c7b962b0bc9e16452ceff7722
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-08 19:24:23 +0000

    SE: refactoring texture control

--- trunk/Tools/SkinEditor/SkinTextureControl.cpp
+++ trunk/Tools/SkinEditor/SkinTextureControl.cpp
@@ -11,28 +11,11 @@ namespace tools
 {
 
 	SkinTextureControl::SkinTextureControl(MyGUI::Widget* _parent) :
-		wraps::BaseLayout("SkinTextureControl.layout", _parent),
+		TextureControl(_parent),
 		mTypeName("SkinTextureControl"),
-		mView(nullptr),
-		mTexture(nullptr),
-		mBackgroundColour(nullptr),
-		mBackground(nullptr),
-		mRegionSelectorControl(nullptr),
-		mScale(nullptr),
-		mScaleValue(1)
+		mRegionSelectorControl(nullptr)
 	{
-		assignWidget(mView, "View");
-		assignWidget(mTexture, "Texture");
-		assignWidget(mBackgroundColour, "BackgroundColour");
-		assignWidget(mBackground, "Background");
-		assignWidget(mScale, "Scale");
 		assignBase(mRegionSelectorControl, "Texture");
-
-		fillColours(mBackgroundColour);
-		mBackgroundColour->eventComboChangePosition += MyGUI::newDelegate(this, &SkinTextureControl::notifyComboChangePosition);
-
-		fillScale();
-		mScale->eventComboChangePosition += MyGUI::newDelegate(this, &SkinTextureControl::notifyComboChangePosition);
 
 		mRegionSelectorControl->eventChangePosition += MyGUI::newDelegate(this, &SkinTextureControl::notifyChangePosition);
 
@@ -44,9 +27,6 @@ namespace tools
 		shutdownAdvisor();
 
 		mRegionSelectorControl->eventChangePosition -= MyGUI::newDelegate(this, &SkinTextureControl::notifyChangePosition);
-
-		mBackgroundColour->eventComboChangePosition -= MyGUI::newDelegate(this, &SkinTextureControl::notifyComboChangePosition);
-		mScale->eventComboChangePosition -= MyGUI::newDelegate(this, &SkinTextureControl::notifyComboChangePosition);
 	}
 
 	void SkinTextureControl::updateSkinProperty(Property* _sender, const MyGUI::UString& _owner)
@@ -77,10 +57,7 @@ namespace tools
 				texture = prop->getValue();
 		}
 
-		mTextureSize = MyGUI::texture_utility::getTextureSize(texture);
-		mTexture->setImageTexture(texture);
-
-		updateScale();
+		setTextureName(texture);
 	}
 
 	void SkinTextureControl::updateCoord()
@@ -105,78 +82,6 @@ namespace tools
 		{
 			mRegionSelectorControl->setVisible(false);
 		}
-	}
-
-	void SkinTextureControl::fillColours(MyGUI::ComboBox* _combo)
-	{
-		_combo->removeAllItems();
-
-		_combo->addItem("Zero", MyGUI::Colour::Zero);
-		_combo->addItem("Black", MyGUI::Colour::Black);
-		_combo->addItem("White", MyGUI::Colour::White);
-		_combo->addItem("Red", MyGUI::Colour::Red);
-		_combo->addItem("Green", MyGUI::Colour::Green);
-		_combo->addItem("Blue", MyGUI::Colour::Blue);
-
-		_combo->setIndexSelected(0);
-
-		updateColour(_combo);
-	}
-
-	void SkinTextureControl::notifyComboChangePosition(MyGUI::ComboBox* _sender, size_t _index)
-	{
-		updateColour(_sender);
-	}
-
-	void SkinTextureControl::updateColour(MyGUI::ComboBox* _sender)
-	{
-		if (_sender == mBackgroundColour)
-		{
-			size_t index = mBackgroundColour->getIndexSelected();
-			if (index != MyGUI::ITEM_NONE)
-			{
-				MyGUI::Colour colour = *mBackgroundColour->getItemDataAt<MyGUI::Colour>(index);
-				mBackground->setColour(colour);
-				mBackground->setAlpha(colour.alpha);
-			}
-		}
-		else if (_sender == mScale)
-		{
-			size_t index = mScale->getIndexSelected();
-			if (index != MyGUI::ITEM_NONE)
-			{
-				mScaleValue = *mScale->getItemDataAt<double>(index);
-				updateScale();
-			}
-		}
-	}
-
-	void SkinTextureControl::fillScale()
-	{
-		mScale->removeAllItems();
-
-		mScale->addItem("50 %", (double)0.5);
-		mScale->addItem("100 %", (double)1);
-		mScale->addItem("200 %", (double)2);
-		mScale->addItem("400 %", (double)4);
-		mScale->addItem("800 %", (double)8);
-		mScale->addItem("1600 %", (double)16);
-
-		size_t index = 1;
-
-		mScale->setIndexSelected(index);
-		mScaleValue = *mScale->getItemDataAt<double>(index);
-
-		updateScale();
-	}
-
-	void SkinTextureControl::updateScale()
-	{
-		double width = (double)mTextureSize.width * mScaleValue;
-		double height = (double)mTextureSize.height * mScaleValue;
-
-		mView->setCanvasSize(MyGUI::IntSize((int)width, (int)height));
-		mRegionSelectorControl->setScale(mScaleValue);
 	}
 
 	void SkinTextureControl::updateRegionCoord()
