77ff56f0d8aaccf47959d509e9fba8819b4bf845
my_name <mynameco@users.noreply.github.com>
2010-08-08 21:55:26 +0000

    SE: fix scale selectors

--- Tools/SkinEditor/SeparatorTextureControl.h
+++ Tools/SkinEditor/SeparatorTextureControl.h
@@ -8,6 +8,8 @@
 
 #include "TextureControl.h"
 #include "PropertyAdvisor.h"
+#include "HorizontalSelectorControl.h"
+#include "VerticalSelectorControl.h"
 
 namespace tools
 {
@@ -20,24 +22,35 @@ namespace tools
 		SeparatorTextureControl(MyGUI::Widget* _parent);
 		virtual ~SeparatorTextureControl();
 
-	private:
+	protected:
 		virtual void updateSkinProperties();
 		virtual void updateStateProperties();
+		virtual void updateSeparatorProperties();
 
 		virtual void updateSkinProperty(Property* _sender, const MyGUI::UString& _owner);
 		virtual void updateStateProperty(Property* _sender, const MyGUI::UString& _owner);
+		virtual void updateSeparatorProperty(Property* _sender, const MyGUI::UString& _owner);
 
 	private:
+		void notifyChangePosition();
+
 		void updateTextureControl();
 		void updateTextureName();
 		void updateRegionSize();
 		void updateRegionPosition();
 		void updateTextureVisible();
 
+		void updatePosition();
+		void updateVisible();
+
 	private:
 		MyGUI::UString mTextureName;
 		MyGUI::IntCoord mTextureRegion;
 		bool mTextureVisible;
+		MyGUI::UString mTypeName;
+
+		HorizontalSelectorControl* mHorizontalSelectorControl;
+		VerticalSelectorControl* mVerticalSelectorControl;
 	};
 
 } // namespace tools
