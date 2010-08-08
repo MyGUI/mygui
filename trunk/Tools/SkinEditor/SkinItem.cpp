d5da663c2f251ff65b3f62df1815e5f1e6129086
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-08 14:44:36 +0000

    SE: add separator property

--- trunk/Tools/SkinEditor/SkinItem.cpp
+++ trunk/Tools/SkinEditor/SkinItem.cpp
@@ -16,24 +16,33 @@ namespace tools
 		mPropertySet->createChild("Texture", "Texture");
 		mPropertySet->createChild("Coord", "IntCoord", "0 0 32 32");
 
-		StateItem* item = nullptr;
+		StateItem* skin = nullptr;
+		skin = mStates.createChild();
+		skin->setName("disabled");
+		skin = mStates.createChild();
+		skin->setName("normal");
+		skin = mStates.createChild();
+		skin->setName("over");
+		skin = mStates.createChild();
+		skin->setName("pressed");
+		skin = mStates.createChild();
+		skin->setName("selected disabled");
+		skin = mStates.createChild();
+		skin->setName("selected normal");
+		skin = mStates.createChild();
+		skin->setName("selected over");
+		skin = mStates.createChild();
+		skin->setName("selected pressed");
 
-		item = mStates.createChild();
-		item->setName("disabled");
-		item = mStates.createChild();
-		item->setName("normal");
-		item = mStates.createChild();
-		item->setName("over");
-		item = mStates.createChild();
-		item->setName("pressed");
-		item = mStates.createChild();
-		item->setName("selected disabled");
-		item = mStates.createChild();
-		item->setName("selected normal");
-		item = mStates.createChild();
-		item->setName("selected over");
-		item = mStates.createChild();
-		item->setName("selected pressed");
+		SeparatorItem* separator = nullptr;
+		separator = mSeparators.createChild();
+		separator->setName("left horizontal");
+		separator = mSeparators.createChild();
+		separator->setName("right horizontal");
+		separator = mSeparators.createChild();
+		separator->setName("top vertical");
+		separator = mSeparators.createChild();
+		separator->setName("bottom vertical");
 	}
 
 	SkinItem::~SkinItem()
