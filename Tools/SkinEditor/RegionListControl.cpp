5aa532e6aa776bb858d22444c869063d86b9b24b
my_name <my.name.post@gmail.com>
2010-08-12 13:11:16 +0000

    SE: update region position

--- Tools/SkinEditor/RegionListControl.cpp
+++ Tools/SkinEditor/RegionListControl.cpp
@@ -84,12 +84,42 @@ namespace tools
 	void RegionListControl::updateSeparatorProperties()
 	{
 		updateRegionEnabled();
+		updateRegionPosition();
 	}
 
 	void RegionListControl::updateSeparatorProperty(Property* _sender, const MyGUI::UString& _owner)
 	{
-		if (_sender->getName() == "Visible")
+		if (_sender->getName() == "Visible" || _sender->getName() == "Position" )
+		{
 			updateRegionEnabled();
+			updateRegionPosition();
+		}
+	}
+
+	void RegionListControl::updateSkinProperties()
+	{
+		updateCoord();
+	}
+
+	void RegionListControl::updateSkinProperty(Property* _sender, const MyGUI::UString& _owner)
+	{
+		if (_sender->getName() == "Coord")
+			updateCoord();
+	}
+
+	void RegionListControl::updateCoord()
+	{
+		MyGUI::UString value;
+
+		if (getCurrentSkin() != nullptr)
+			value = getCurrentSkin()->getPropertySet()->getPropertyValue("Coord");
+
+		MyGUI::IntCoord coord;
+		if (MyGUI::utility::parseComplex(value, coord.left, coord.top, coord.width, coord.height))
+		{
+			mCoordValue = coord;
+			updateRegionPosition();
+		}
 	}
 
 	void RegionListControl::updateRegionEnabled()
@@ -101,8 +131,8 @@ namespace tools
 			{
 				bool enabled = true;
 				RegionItem* item = regions.current();
-				MyGUI::Align separator = item->getSeparator();
 
+				MyGUI::Align separator = item->getSeparator();
 				if (separator.isLeft() && !isSeparatorVisible(MyGUI::Align::Left))
 					enabled = false;
 				if (separator.isRight() && !isSeparatorVisible(MyGUI::Align::Right))
@@ -121,6 +151,87 @@ namespace tools
 		}
 	}
 
+	void RegionListControl::updateRegionPosition()
+	{
+		if (getCurrentSkin() == nullptr)
+			return;
+		
+		bool leftVisible = isSeparatorVisible(MyGUI::Align::Left);
+		bool rightVisible = isSeparatorVisible(MyGUI::Align::Right);
+		bool topVisible = isSeparatorVisible(MyGUI::Align::Top);
+		bool bottomVisible = isSeparatorVisible(MyGUI::Align::Bottom);
+
+		int leftPosition = leftVisible ? getSeparatorPosition(MyGUI::Align::Left) : 0;
+		int rightPosition = rightVisible ? getSeparatorPosition(MyGUI::Align::Right) : mCoordValue.width;
+		int topPosition = topVisible ? getSeparatorPosition(MyGUI::Align::Top) : 0;
+		int bottomPosition = bottomVisible ? getSeparatorPosition(MyGUI::Align::Bottom) : mCoordValue.height;
+
+		RegionItem* item = nullptr;
+
+		item = getRegion(MyGUI::Align::Left | MyGUI::Align::Top);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(0, 0, leftPosition, topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Right | MyGUI::Align::Top);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(rightPosition, 0, mCoordValue.width - rightPosition, topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Right | MyGUI::Align::Bottom);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(rightPosition, bottomPosition, mCoordValue.width - rightPosition, mCoordValue.height - bottomPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Left | MyGUI::Align::Bottom);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(0, bottomPosition, leftPosition, mCoordValue.height - bottomPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Left);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(0, topPosition, leftPosition, bottomPosition - topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Top);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(leftPosition, 0, rightPosition - leftPosition, topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Right);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(rightPosition, topPosition, mCoordValue.width - rightPosition, bottomPosition - topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Bottom);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(leftPosition, bottomPosition, rightPosition - leftPosition, mCoordValue.height - bottomPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+
+		item = getRegion(MyGUI::Align::Center);
+		if (item != nullptr)
+		{
+			MyGUI::IntCoord coord(leftPosition, topPosition, rightPosition - leftPosition, bottomPosition - topPosition);
+			item->getPropertySet()->setPropertyValue("Position", coord.print(), mTypeName);
+		}
+	}
+
 	bool RegionListControl::isSeparatorVisible(MyGUI::Align _value)
 	{
 		ItemHolder<SeparatorItem>::EnumeratorItem separators = getCurrentSkin()->getSeparators().getChildsEnumerator();
@@ -133,6 +244,32 @@ namespace tools
 		}
 
 		return true;
+	}
+
+	int RegionListControl::getSeparatorPosition(MyGUI::Align _value)
+	{
+		ItemHolder<SeparatorItem>::EnumeratorItem separators = getCurrentSkin()->getSeparators().getChildsEnumerator();
+		while (separators.next())
+		{
+			SeparatorItem* item = separators.current();
+
+			if (item->getCorner() == _value)
+				return MyGUI::utility::parseInt(item->getPropertySet()->getPropertyValue("Position"));
+		}
+
+		return 0;
+	}
+
+	RegionItem* RegionListControl::getRegion(MyGUI::Align _align)
+	{
+		ItemHolder<RegionItem>::EnumeratorItem regions = getCurrentSkin()->getRegions().getChildsEnumerator();
+		while (regions.next())
+		{
+			RegionItem* item = regions.current();
+			if (item->getSeparator() == _align)
+				return item;
+		}
+		return nullptr;
 	}
 
 } // namespace tools
