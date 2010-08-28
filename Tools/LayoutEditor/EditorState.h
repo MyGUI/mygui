e4a3590bb5d3e4993235015392f15282361431cc
my_name <my.name.post@gmail.com>
2010-08-28 22:31:22 +0000

    LE: update save behavior

--- Tools/LayoutEditor/EditorState.h
+++ Tools/LayoutEditor/EditorState.h
@@ -30,14 +30,6 @@ public:
 	virtual void onFileDrop(const std::wstring& _filename);
 	virtual bool onWinodwClose(size_t _handle);
 
-	// main panel
-	//void notifyLoad();
-	//bool notifySave();
-	//void notifySettings();
-	//void notifyEndTest();
-	//void notifyClear();
-	//void notifyQuit();
-
 	typedef std::vector<std::wstring> VectorWString;
 	const VectorWString& getParams() { return mParams; }
 
@@ -49,26 +41,12 @@ private:
 	void notifyMessageBoxResultQuit(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
 	void notifyMessageBoxResultLoadDropFile(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
 
-	//void notifyConfirmLoadMessage(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
-
-	//void notifyClearMessage(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
-	void clear(/*bool _clearName = true*/);
-	//void notifyConfirmQuitMessage(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
-	void save(/*const MyGUI::UString& _file*/);
-	void load(/*const MyGUI::UString& _file*/);
-	//void loadFile(const std::wstring& _file);
+	void clear();
+	bool save();
+	void load();
 	void loadDropFile();
 
-
-	//void notifyOpenSaveEndDialog(tools::Dialog* _dialog, bool _result);
-	//void notifySettingsWindowEndDialog(tools::Dialog* _dialog, bool _result);
-
 	void notifyRecreate();
-
-	//void clearWidgetWindow();
-
-	//void setModeSaveLoadDialog(bool _save, const MyGUI::UString& _filename);
-
 	void notifyFrameStarted(float _time);
 
 	int toGrid(int _x);
@@ -105,9 +83,6 @@ private:
 	// drop select after skin change
 	bool mRecreate;
 
-	// last loaded/saved file name
-	//MyGUI::UString mFileName;
-
 	//bool mTestMode;
 	MyGUI::VectorWidgetPtr mInterfaceWidgets;
 	MyGUI::xml::Document * mTestLayout;
@@ -118,7 +93,7 @@ private:
 	//CodeGenerator * mCodeGenerator;
 	tools::OpenSaveFileDialog* mOpenSaveFileDialog;
 
-	std::vector<std::wstring> mParams;
+	VectorWString mParams;
 	std::string mLocale;
 
 	tools::MainMenuControl* mMainMenuControl;
