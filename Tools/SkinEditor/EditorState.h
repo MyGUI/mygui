22244fb6c286d1cc32d07241967159842c5fa16f
my_name <mynameco@users.noreply.github.com>
2010-08-28 11:37:03 +0000

    SE: refactoring states

--- Tools/SkinEditor/EditorState.h
+++ Tools/SkinEditor/EditorState.h
@@ -8,6 +8,9 @@
 
 #include "StateController.h"
 #include "MainPane.h"
+#include "OpenSaveFileDialog.h"
+#include "TestWindow.h"
+#include "MessageBoxFadeControl.h"
 
 namespace tools
 {
@@ -25,7 +28,42 @@ namespace tools
 		virtual void resumeState();
 
 	private:
+		void notifyMessageBoxResultLoad(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
+		void notifyMessageBoxResultClear(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
+		void notifyMessageBoxResultQuit(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
+		void notifyMessageBoxResultLoadDropFile(MyGUI::Message* _sender, MyGUI::MessageBoxStyle _result);
+
+		void notifyEndDialog(Dialog* _sender, bool _result);
+		void notifyEndDialogTest(Dialog* _sender, bool _result);
+		void notifyChanges(bool _changes);
+
+		void commandLoad(const MyGUI::UString& _commandName);
+		void commandSave(const MyGUI::UString& _commandName);
+		void commandSaveAs(const MyGUI::UString& _commandName);
+		void commandClear(const MyGUI::UString& _commandName);
+		void commandTest(const MyGUI::UString& _commandName);
+		void commandQuit(const MyGUI::UString& _commandName);
+		void commandFileDrop(const MyGUI::UString& _commandName);
+
+		void clear();
+		void save();
+		void load();
+		void loadDropFile();
+
+		void showLoadWindow();
+		void showSaveAsWindow();
+
+		void updateCaption();
+
+	private:
+		MyGUI::UString mFileName;
+		MyGUI::UString mDefaultFileName;
+		MyGUI::UString mDropFileName;
+
 		MainPane* mMainPane;
+		OpenSaveFileDialog* mOpenSaveFileDialog;
+		TestWindow* mTestWindow;
+		MessageBoxFadeControl* mMessageBoxFadeControl;
 	};
 
 } // namespace tools
