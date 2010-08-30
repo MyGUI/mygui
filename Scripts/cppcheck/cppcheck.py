b6d460000670289aa2968588758869566d91172c
George Evmenov <altren@gmail.com>
2010-08-30 02:00:01 +0000

    update cppcheck script to ignore some warnings

--- Scripts/cppcheck/cppcheck.py
+++ Scripts/cppcheck/cppcheck.py
@@ -1,12 +1,37 @@
 # run from root sources directory: python Scripts/cppcheck/cppcheck.py
 import os
 
+ignoredEndings = ["is never used", "It is safe to deallocate a NULL pointer", "Throwing exception in destructor"]
+ignoredContent = ["MyGUI_UString"]
+
+def isIgnoredWarning(warning):
+	for ignore in ignoredEndings:
+		if warning.endswith(ignore):
+			return True
+	for ignore in ignoredContent:
+		if warning.find(ignore) != -1:
+			return True
+	return False
+
+def parseOutput():
+	file = open("temp.cppcheck", 'r')
+	
+	line = file.readline()
+	while line != "":
+		line = line[0:len(line)-1]
+		if (not isIgnoredWarning(line)):
+			print line
+		line = file.readline()
+	file.close ()
+
 def checkFolderSources(folder) :
-	os.system("cppcheck --enable=all -I Scripts/cppcheck " + folder)
+	os.system("cppcheck --enable=all -I Scripts/cppcheck " + folder + " 2>temp.cppcheck")
+	parseOutput()
 
 
 #checkFolderSources('MyGUIEngine')
-os.system("cppcheck --enable=all -I Scripts/cppcheck -I MyGUIEngine/include MyGUIEngine/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I MyGUIEngine/include MyGUIEngine/src 2>temp.cppcheck")
+parseOutput()
 
 checkFolderSources('Demos')
 checkFolderSources('Tools')
@@ -14,13 +39,17 @@ checkFolderSources('UnitTests')
 checkFolderSources('Common')
 #checkFolderSources('Platforms/OpenGL')
 # temporary disabled due to cppcheck bug
-#os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/OpenGL/OpenGLPlatform/include Platforms/OpenGL/OpenGLPlatform/src")
+#os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/OpenGL/OpenGLPlatform/include Platforms/OpenGL/OpenGLPlatform/src 2>temp.cppcheck")
+parseOutput()
 #checkFolderSources('Platforms/Ogre')
-os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/Ogre/OgrePlatform/include Platforms/Ogre/OgrePlatform/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/Ogre/OgrePlatform/include Platforms/Ogre/OgrePlatform/src 2>temp.cppcheck")
+parseOutput()
 #checkFolderSources('Platforms/DirectX')
-os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/DirectX/DirectXPlatform/include Platforms/DirectX/DirectXPlatform/src")
+os.system("cppcheck --enable=all -I Scripts/cppcheck -I Platforms/DirectX/DirectXPlatform/include Platforms/DirectX/DirectXPlatform/src 2>temp.cppcheck")
+parseOutput()
 checkFolderSources('Platforms/OpenGL')
 checkFolderSources('Platforms/OGRE')
 checkFolderSources('Platforms/DirectX')
 checkFolderSources('Plugins')
 checkFolderSources('Wrapper')
+
