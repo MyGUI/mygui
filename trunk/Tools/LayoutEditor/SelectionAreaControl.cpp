e0ae621c7ae2f159347e855d82c3fa1e1f9a091e
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-08-31 20:33:28 +0000

    LE: refactoring

--- trunk/Tools/LayoutEditor/SelectionAreaControl.cpp
+++ trunk/Tools/LayoutEditor/SelectionAreaControl.cpp
@@ -1,29 +1,33 @@
 #include "Common.h"
 #include "SelectionAreaControl.h"
+#include "PropertiesPanelView.h"
 #include "SettingsManager.h"
-#include "EditorWidgets.h"
-#include "UndoManager.h"
 #include "WidgetSelectorManager.h"
+#include "UndoManager.h"
+
+//FIXME
+template <> tools::SelectionAreaControl* MyGUI::Singleton<tools::SelectionAreaControl>::msInstance = nullptr;
+template <> const char* MyGUI::Singleton<tools::SelectionAreaControl>::mClassTypeName("SelectionAreaControl");
 
 namespace tools
 {
 	SelectionAreaControl::SelectionAreaControl(MyGUI::Widget* _parent) :
 		wraps::BaseLayout("SelectionAreaControl.layout", _parent),
-		mWindow(nullptr),
-		mGridStep(0),
+		mCurrentWidgetRectangle(nullptr),
 		mCurrentWidget(nullptr),
+		mGridStep(0),
 		mArrowMove(false)
 	{
-		assignWidget(mWindow, "_Main");
+		assignWidget(mCurrentWidgetRectangle, "_Main");
 
-		mWindow->eventWindowChangeCoord += newDelegate(this, &SelectionAreaControl::notifyRectangleResize);
-		//mWindow->eventMouseButtonDoubleClick += newDelegate(mPanelItems, &PanelItems::notifyRectangleDoubleClick);
-		//mWindow->eventKeyButtonPressed += newDelegate(this, &SelectionAreaControl::notifyRectangleKeyPressed);
-		mWindow->eventMouseButtonClick += MyGUI::newDelegate(this, &SelectionAreaControl::notifyMouseButtonClick);
+		mCurrentWidgetRectangle->eventWindowChangeCoord += newDelegate(this, &SelectionAreaControl::notifyRectangleResize);
+		mCurrentWidgetRectangle->eventKeyButtonPressed += newDelegate(this, &SelectionAreaControl::notifyRectangleKeyPressed);
 
 		mGridStep = SettingsManager::getInstance().getSector("SettingsWindow")->getPropertyValue<int>("Grid");
+
 		notifyChangeSelectedWidget(nullptr);
 
+		PropertiesPanelView::getInstance().eventChangeCoord += MyGUI::newDelegate(this, &SelectionAreaControl::notifyChangeCoord);
 		SettingsManager::getInstance().eventSettingsChanged += MyGUI::newDelegate(this, &SelectionAreaControl::notifySettingsChanged);
 		WidgetSelectorManager::getInstance().eventChangeSelectedWidget += MyGUI::newDelegate(this, &SelectionAreaControl::notifyChangeSelectedWidget);
 	}
@@ -31,15 +35,41 @@ namespace tools
 	SelectionAreaControl::~SelectionAreaControl()
 	{
 		WidgetSelectorManager::getInstance().eventChangeSelectedWidget -= MyGUI::newDelegate(this, &SelectionAreaControl::notifyChangeSelectedWidget);
+		PropertiesPanelView::getInstance().eventChangeCoord -= MyGUI::newDelegate(this, &SelectionAreaControl::notifyChangeCoord);
 		SettingsManager::getInstance().eventSettingsChanged -= MyGUI::newDelegate(this, &SelectionAreaControl::notifySettingsChanged);
+	}
+
+	void SelectionAreaControl::notifyChangeCoord(const MyGUI::IntCoord& _coord)
+	{
+		mCurrentWidgetRectangle->setCoord(_coord);
+	}
+
+	int SelectionAreaControl::toGrid(int _x)
+	{
+		if (mGridStep < 1)
+			return _x;
+		return _x / mGridStep * mGridStep;
+	}
+
+	void SelectionAreaControl::notifySettingsChanged(const MyGUI::UString& _sectorName, const MyGUI::UString& _propertyName)
+	{
+		if (_sectorName == "SettingsWindow")
+		{
+			if (_propertyName == "Grid")
+				mGridStep = SettingsManager::getInstance().getSector("SettingsWindow")->getPropertyValue<int>("Grid");
+		}
+	}
+
+	MyGUI::Window* SelectionAreaControl::getWidgetRectangle()
+	{
+		return mCurrentWidgetRectangle;
 	}
 
 	void SelectionAreaControl::notifyRectangleResize(MyGUI::Window* _sender)
 	{
-		/*if (!_sender->getVisible())
-			return;*/
+		if (!_sender->getVisible())
+			return;
 
-		// найдем соответствующий контейнер виджета и переместим/растянем
 		if (WidgetTypes::getInstance().findWidgetStyle(mCurrentWidget->getTypeName())->resizeable)
 		{
 			MyGUI::IntCoord coord = utility::convertCoordToParentCoord(_sender->getCoord(), mCurrentWidget);
@@ -81,56 +111,70 @@ namespace tools
 			// update coord because of mCurrentWidget can have MinMax size
 			coord = mCurrentWidget->getCoord();
 
-			WidgetContainer* widgetContainer = EditorWidgets::getInstance().find(mCurrentWidget);
-			SettingsManager::getInstance().getSector("Widget")->setPropertyValue("Position", widgetContainer->position());
-			//setPositionText(widgetContainer->position());
+			PropertiesPanelView::getInstance().setCoord(coord);
 
 			UndoManager::getInstance().addValue(PR_POSITION);
 		}
-
-		mWindow->setCoord(mCurrentWidget->getAbsoluteCoord());
+		mCurrentWidgetRectangle->setCoord(mCurrentWidget->getAbsoluteCoord());
 	}
 
-	int SelectionAreaControl::toGrid(int _x)
+	void SelectionAreaControl::notifyRectangleKeyPressed(MyGUI::Widget* _sender, MyGUI::KeyCode _key, MyGUI::Char _char)
 	{
-		if (mGridStep < 1)
-			return _x;
-		return _x / mGridStep * mGridStep;
-	}
-
-	void SelectionAreaControl::notifySettingsChanged(const MyGUI::UString& _sectorName, const MyGUI::UString& _propertyName)
-	{
-		if (_sectorName == "SettingsWindow")
+		MyGUI::IntPoint delta;
+		int k = MyGUI::InputManager::getInstance().isShiftPressed() ? 1 : mGridStep;
+		if (MyGUI::KeyCode::Tab == _key)
 		{
-			if (_propertyName == "Grid")
-				mGridStep = SettingsManager::getInstance().getSector("SettingsWindow")->getPropertyValue<int>("Grid");
-		}
-		else if (_sectorName == "Widget")
-		{
-			if (_propertyName == "Position")
+			if ((nullptr != mCurrentWidget) && (nullptr != mCurrentWidget->getParent()) && (mCurrentWidget->getParent()->getTypeName() == "Tab"))
+				notifyChangeSelectedWidget(mCurrentWidget->getParent());
+			if (mCurrentWidget && mCurrentWidget->getTypeName() == "Tab")
 			{
-				MyGUI::IntCoord coord = MyGUI::IntCoord::parse(SettingsManager::getInstance().getSector("Widget")->getPropertyValue("Position"));
-				if (mMainWidget->getCoord() != coord)
-					mMainWidget->setCoord(coord);
+				MyGUI::Tab* tab = mCurrentWidget->castType<MyGUI::Tab>();
+				size_t sheet = tab->getIndexSelected();
+				sheet++;
+				if (sheet >= tab->getItemCount()) sheet = 0;
+				if (tab->getItemCount()) tab->setIndexSelected(sheet);
 			}
 		}
-	}
-
-	void SelectionAreaControl::notifyChangeSelectedWidget(MyGUI::Widget* _widget)
-	{
-		mCurrentWidget = _widget;
-
-		mMainWidget->setVisible(mCurrentWidget != nullptr);
-
-		if (mCurrentWidget != nullptr)
+		else if (MyGUI::KeyCode::Delete == _key)
 		{
-			mWindow->setCoord(mCurrentWidget->getAbsoluteCoord());
+			if (mCurrentWidget)
+			{
+				EditorWidgets::getInstance().remove(mCurrentWidget);
+				WidgetSelectorManager::getInstance().setSelectedWidget(nullptr);
+				UndoManager::getInstance().addValue();
+			}
+		}
+		else if (MyGUI::KeyCode::ArrowLeft == _key)
+		{
+			delta = MyGUI::IntPoint(-k, 0);
+		}
+		else if (MyGUI::KeyCode::ArrowRight == _key)
+		{
+			delta = MyGUI::IntPoint(k, 0);
+		}
+		else if (MyGUI::KeyCode::ArrowUp == _key)
+		{
+			delta = MyGUI::IntPoint(0, -k);
+		}
+		else if (MyGUI::KeyCode::ArrowDown == _key)
+		{
+			delta = MyGUI::IntPoint(0, k);
+		}
+
+		if (delta != MyGUI::IntPoint())
+		{
+			mArrowMove = true;
+			mCurrentWidgetRectangle->setPosition(mCurrentWidgetRectangle->getPosition() + delta);
+			notifyRectangleResize(mCurrentWidgetRectangle);
+			UndoManager::getInstance().addValue(PR_KEY_POSITION);
 		}
 	}
 
-	void SelectionAreaControl::notifyMouseButtonClick(MyGUI::Widget* _sender)
+	void SelectionAreaControl::notifyChangeSelectedWidget(MyGUI::Widget* _currentWidget)
 	{
-		int test = 0;
+		mCurrentWidget = _currentWidget;
+
+		mCurrentWidgetRectangle->setVisible(mCurrentWidget != nullptr);
 	}
 
 } // namespace tools
