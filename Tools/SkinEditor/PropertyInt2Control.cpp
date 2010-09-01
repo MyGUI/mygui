d06f57b1f4912aee070a4fa86d5e26d261d3a86f
my_name <my.name.post@gmail.com>
2010-09-01 09:24:55 +0000

    SE: fix colour tags

--- Tools/SkinEditor/PropertyInt2Control.cpp
+++ Tools/SkinEditor/PropertyInt2Control.cpp
@@ -5,6 +5,7 @@
 */
 #include "precompiled.h"
 #include "PropertyInt2Control.h"
+#include "Localise.h"
 
 namespace tools
 {
@@ -82,7 +83,7 @@ namespace tools
 	{
 		MyGUI::UString value = mEdit->getOnlyText();
 		if (!_validate)
-			value = MyGUI::LanguageManager::getInstance().replaceTags("ColourError") + value;
+			value = replaceTags("ColourError") + value;
 
 		size_t index = mEdit->getTextCursor();
 		mEdit->setCaption(value);
--- Tools/SkinEditor/PropertyIntControl.cpp
+++ Tools/SkinEditor/PropertyIntControl.cpp
@@ -5,6 +5,7 @@
 */
 #include "precompiled.h"
 #include "PropertyIntControl.h"
+#include "Localise.h"
 
 namespace tools
 {
@@ -80,7 +81,7 @@ namespace tools
 	{
 		MyGUI::UString value = mEdit->getOnlyText();
 		if (!_validate)
-			value = MyGUI::LanguageManager::getInstance().replaceTags("ColourError") + value;
+			value = replaceTags("ColourError") + value;
 
 		size_t index = mEdit->getTextCursor();
 		mEdit->setCaption(value);
