d2ffa0c8638964edb0e1ee7ac3e2e8bb1f502b94
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-09-02 19:55:00 +0000

    add prerequest to MyGUI_LastHeader.h

--- trunk/Platforms/Ogre/OgrePlatform/include/MyGUI_LastHeader.h
+++ trunk/Platforms/Ogre/OgrePlatform/include/MyGUI_LastHeader.h
@@ -22,6 +22,8 @@
 #ifndef __MYGUI_LAST_HEADER_H__
 #define __MYGUI_LAST_HEADER_H__
 
+#include "MyGUI_Prerequest.h"
+
 // used to enable deprecated warnings, disabled by Ogre
 #if MYGUI_COMPILER == MYGUI_COMPILER_MSVC
 
