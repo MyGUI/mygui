731c7c8bfd26491d0a91e151b506e16e0ae072e0
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-09-04 17:30:58 +0000

    update test apps

--- trunk/UnitTests/TestApp/DemoKeeper.cpp
+++ trunk/UnitTests/TestApp/DemoKeeper.cpp
@@ -5,7 +5,8 @@
 namespace demo
 {
 
-	DemoKeeper::DemoKeeper()
+	DemoKeeper::DemoKeeper() :
+		mRenderSystem(nullptr)
 	{
 	}
 
@@ -17,16 +18,116 @@ namespace demo
 
 	void DemoKeeper::createScene()
 	{
-		MyGUI::ResourceManager::getInstance().load("TestApp2.xml");
-		//MyGUI::VectorWidgetPtr layout = MyGUI::LayoutManager::getInstance().loadLayout("TestApp2.layout");
-		//MyGUI::LayoutManager::getInstance().unloadLayout(layout);
+		getSceneManager()->addRenderQueueListener(this);
 
-		MyGUI::Button* button = MyGUI::Gui::getInstance().createWidget<MyGUI::Button>("TestApp2", MyGUI::IntCoord(10, 10, 100, 100), MyGUI::Align::Default, "Overlapped");
-		button->setCaption("Test");
+		mRenderSystem = Ogre::Root::getSingleton().getRenderSystem();
+
+		// Initialise blending modes to be used.
+		mColourBlendMode.blendType = Ogre::LBT_COLOUR;
+		mColourBlendMode.source1 = Ogre::LBS_TEXTURE;
+		mColourBlendMode.source2 = Ogre::LBS_DIFFUSE;
+		mColourBlendMode.operation = Ogre::LBX_MODULATE;
+
+		mAlphaBlendMode.blendType = Ogre::LBT_ALPHA;
+		mAlphaBlendMode.source1 = Ogre::LBS_TEXTURE;
+		mAlphaBlendMode.source2 = Ogre::LBS_DIFFUSE;
+		mAlphaBlendMode.operation = Ogre::LBX_MODULATE;
+
+		mUvwAddressMode.u = Ogre::TextureUnitState::TAM_CLAMP;
+		mUvwAddressMode.v = Ogre::TextureUnitState::TAM_CLAMP;
+		mUvwAddressMode.w = Ogre::TextureUnitState::TAM_CLAMP;
+
+		VertexBuffer* buffer = createVertexBuffer();
+		buffer->setTextureName("smile1.png");
+		buffer->setCoord(MyGUI::IntCoord(10, 10, 400, 400));
+
+		buffer = createVertexBuffer();
+		buffer->setTextureName("smile2.png");
+		buffer->setCoord(MyGUI::IntCoord(300, 300, 400, 400));
 	}
 
 	void DemoKeeper::destroyScene()
 	{
+		destroyAllBuffers();
+		getSceneManager()->removeRenderQueueListener(this);
+	}
+
+	void DemoKeeper::renderQueueStarted(Ogre::uint8 queueGroupId, const Ogre::String& invocation, bool& skipThisInvocation)
+	{
+		if (Ogre::RENDER_QUEUE_OVERLAY != queueGroupId)
+			return;
+
+		doRender();
+	}
+
+	void DemoKeeper::renderQueueEnded(Ogre::uint8 queueGroupId, const Ogre::String& invocation, bool& repeatThisInvocation)
+	{
+	}
+
+	void DemoKeeper::initState()
+	{
+		// set-up matrices
+		mRenderSystem->_setWorldMatrix(Ogre::Matrix4::IDENTITY);
+		mRenderSystem->_setViewMatrix(Ogre::Matrix4::IDENTITY);
+		mRenderSystem->_setProjectionMatrix(Ogre::Matrix4::IDENTITY);
+
+		// initialise render settings
+		mRenderSystem->setLightingEnabled(false);
+		mRenderSystem->_setDepthBufferParams(false, false);
+		mRenderSystem->_setDepthBias(0, 0);
+		mRenderSystem->_setCullingMode(Ogre::CULL_NONE);
+		mRenderSystem->_setFog(Ogre::FOG_NONE);
+		mRenderSystem->_setColourBufferWriteEnabled(true, true, true, true);
+		mRenderSystem->unbindGpuProgram(Ogre::GPT_FRAGMENT_PROGRAM);
+		mRenderSystem->unbindGpuProgram(Ogre::GPT_VERTEX_PROGRAM);
+		mRenderSystem->setShadingType(Ogre::SO_GOURAUD);
+		mRenderSystem->_setPolygonMode(Ogre::PM_SOLID);
+
+		// initialise texture settings
+		mRenderSystem->_setTextureCoordCalculation(0, Ogre::TEXCALC_NONE);
+		mRenderSystem->_setTextureCoordSet(0, 0);
+		mRenderSystem->_setTextureUnitFiltering(0, Ogre::FO_LINEAR, Ogre::FO_LINEAR, Ogre::FO_POINT);
+		mRenderSystem->_setTextureAddressingMode(0, mUvwAddressMode);
+		mRenderSystem->_setTextureMatrix(0, Ogre::Matrix4::IDENTITY);
+		mRenderSystem->_setAlphaRejectSettings(Ogre::CMPF_ALWAYS_PASS, 0, false);
+		mRenderSystem->_setTextureBlendMode(0, mColourBlendMode);
+		mRenderSystem->_setTextureBlendMode(0, mAlphaBlendMode);
+		mRenderSystem->_disableTextureUnitsFrom(1);
+
+		// enable alpha blending
+		mRenderSystem->_setSceneBlending(Ogre::SBF_SOURCE_ALPHA, Ogre::SBF_ONE_MINUS_SOURCE_ALPHA);
+	}
+
+	void DemoKeeper::doRender()
+	{
+		bool first = true;
+		for (VectorVertexBuffer::iterator item = mBuffers.begin(); item != mBuffers.end(); ++ item)
+		{
+			if ((*item)->getEmpty())
+				continue;
+
+			mRenderSystem->_setTexture(0, true, (*item)->getTextureName());
+			if (first)
+			{
+				initState();
+				first = false;
+			}
+			mRenderSystem->_render((*item)->getRenderOperation());
+		}
+	}
+
+	VertexBuffer* DemoKeeper::createVertexBuffer()
+	{
+		VertexBuffer* buffer = new VertexBuffer();
+		mBuffers.push_back(buffer);
+		return buffer;
+	}
+
+	void DemoKeeper::destroyAllBuffers()
+	{
+		for (VectorVertexBuffer::iterator item = mBuffers.begin(); item != mBuffers.end(); ++ item)
+			delete (*item);
+		mBuffers.clear();
 	}
 
 } // namespace demo
---
+++ trunk/UnitTests/TestApp/VertexBuffer.cpp
@@ -0,0 +1,117 @@
+#include "precompiled.h"
+#include "VertexBuffer.h"
+#include "MyGUI_OgreRenderManager.h"
+
+namespace demo
+{
+
+	VertexBuffer::VertexBuffer() :
+		mVertexCount(6),
+		mCurrentTexture(0, 0, 1, 1),
+		mCurrentColour(0xFFFFFFFF)
+	{
+		createVertexBuffer();
+	}
+
+	VertexBuffer::~VertexBuffer()
+	{
+		destroyVertexBuffer();
+	}
+
+	const std::string& VertexBuffer::getTextureName()
+	{
+		return mTextureName;
+	}
+
+	void VertexBuffer::setTextureName(const std::string& _value)
+	{
+		mTextureName = _value;
+
+		if (!Ogre::TextureManager::getSingletonPtr()->resourceExists(mTextureName))
+			Ogre::TextureManager::getSingletonPtr()->load(mTextureName, "General", Ogre::TEX_TYPE_2D, 0);
+	}
+
+	Ogre::RenderOperation& VertexBuffer::getRenderOperation()
+	{
+		return mRenderOperation;
+	}
+
+	bool VertexBuffer::getEmpty()
+	{
+		return mVertexCount == 0;
+	}
+
+	void VertexBuffer::createVertexBuffer()
+	{
+		mRenderOperation.vertexData = new Ogre::VertexData();
+		mRenderOperation.vertexData->vertexStart = 0;
+
+		Ogre::VertexDeclaration* vd = mRenderOperation.vertexData->vertexDeclaration;
+		vd->addElement( 0, 0, Ogre::VET_FLOAT3, Ogre::VES_POSITION );
+		vd->addElement( 0, Ogre::VertexElement::getTypeSize( Ogre::VET_FLOAT3 ), Ogre::VET_COLOUR, Ogre::VES_DIFFUSE );
+		vd->addElement( 0, Ogre::VertexElement::getTypeSize( Ogre::VET_FLOAT3 ) +
+						   Ogre::VertexElement::getTypeSize( Ogre::VET_COLOUR ),
+						   Ogre::VET_FLOAT2, Ogre::VES_TEXTURE_COORDINATES );
+
+		// Create the Vertex Buffer, using the Vertex Structure we previously declared in _declareVertexStructure.
+		mVertexBuffer = Ogre::HardwareBufferManager::getSingleton( ).createVertexBuffer(
+			mRenderOperation.vertexData->vertexDeclaration->getVertexSize(0), // declared Vertex used
+			mVertexCount,
+			Ogre::HardwareBuffer::HBU_DYNAMIC_WRITE_ONLY_DISCARDABLE,
+			false );
+
+		// Bind the created buffer to the renderOperation object.  Now we can manipulate the buffer, and the RenderOp keeps the changes.
+		mRenderOperation.vertexData->vertexBufferBinding->setBinding( 0, mVertexBuffer );
+		mRenderOperation.operationType = Ogre::RenderOperation::OT_TRIANGLE_LIST;
+		mRenderOperation.useIndexes = false;
+	}
+
+	void VertexBuffer::destroyVertexBuffer()
+	{
+		delete mRenderOperation.vertexData;
+		mRenderOperation.vertexData = 0;
+		mVertexBuffer.setNull();
+	}
+
+	const MyGUI::IntCoord& VertexBuffer::getCoord()
+	{
+		return mCoord;
+	}
+
+	void VertexBuffer::setCoord(const MyGUI::IntCoord& _value)
+	{
+		mCoord = _value;
+		update();
+	}
+
+	void VertexBuffer::update()
+	{
+		MyGUI::VertexQuad* quad = (MyGUI::VertexQuad*)mVertexBuffer->lock(Ogre::HardwareVertexBuffer::HBL_DISCARD);
+
+		const MyGUI::RenderTargetInfo& info = MyGUI::OgreRenderManager::getInstance().getInfo();
+
+		float vertex_z = info.maximumDepth;
+
+		float vertex_left = ((info.pixScaleX * (float)(mCoord.left - info.leftOffset) + info.hOffset) * 2) - 1;
+		float vertex_right = vertex_left + (info.pixScaleX * (float)mCoord.width * 2);
+		float vertex_top = -(((info.pixScaleY * (float)(mCoord.top - info.topOffset) + info.vOffset) * 2) - 1);
+		float vertex_bottom = vertex_top - (info.pixScaleY * (float)mCoord.height * 2);
+
+		quad->set(
+			vertex_left,
+			vertex_top,
+			vertex_right,
+			vertex_bottom,
+			vertex_z,
+			mCurrentTexture.left,
+			mCurrentTexture.top,
+			mCurrentTexture.right,
+			mCurrentTexture.bottom,
+			mCurrentColour
+			);
+
+		mVertexBuffer->unlock();
+		mRenderOperation.vertexData->vertexCount = 6;
+	}
+
+} // namespace demo
