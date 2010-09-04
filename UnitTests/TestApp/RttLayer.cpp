090120877f5ecafa79c38f89609d8ac759d6c580
my_name <my.name.post@gmail.com>
2010-09-04 23:38:05 +0000

    update TestApp

--- UnitTests/TestApp/RttLayer.cpp
+++ UnitTests/TestApp/RttLayer.cpp
@@ -5,14 +5,18 @@ namespace demo
 {
 	RttLayer::RttLayer() :
 		mBuffer(nullptr),
-		mViewport(nullptr)
+		mViewport(nullptr),
+		mProjectMatrix(Ogre::Matrix4::IDENTITY),
+		mTextureFilterNone(false)
 	{
+		std::string textureName = MyGUI::utility::toString("RttLayerTexture", (int)this);
+
 		mTexture = Ogre::TextureManager::getSingleton().createManual(
-			"RttLayerTexture",
+			textureName,
 			"General",
 			Ogre::TEX_TYPE_2D,
-			64,
-			64,
+			256,
+			256,
 			0,
 			Ogre::PF_A8R8G8B8,
 			Ogre::TU_RENDERTARGET,
@@ -23,10 +27,19 @@ namespace demo
 		mViewport->setClearEveryFrame(false);
 		mViewport->setOverlaysEnabled(false);
 
+		if (!rtt->requiresTextureFlipping())
+		{
+			mProjectMatrix[1][0] = -mProjectMatrix[1][0];
+			mProjectMatrix[1][1] = -mProjectMatrix[1][1];
+			mProjectMatrix[1][2] = -mProjectMatrix[1][2];
+			mProjectMatrix[1][3] = -mProjectMatrix[1][3];
+		}
+
 		mBuffer = new VertexBuffer();
-		mBuffer->setTextureName("RttLayerTexture");
-		mBuffer->setCoord(MyGUI::IntCoord(10, 400, 800, 500));
-		mBuffer->setRenderMode(VertexBuffer::RenderModeFilterNone);
+		mBuffer->setTextureName(textureName);
+		mBuffer->setCoord(MyGUI::IntCoord(0, 0, 1024, 768));
+		if (mTextureFilterNone)
+			mBuffer->setRenderMode(VertexBuffer::RenderModeFilterNone);
 	}
 
 	RttLayer::~RttLayer()
@@ -41,12 +54,24 @@ namespace demo
 		Ogre::Viewport* saveViewport = system->_getViewport();
 		system->_setViewport(mViewport);
 		system->clearFrameBuffer(Ogre::FBT_COLOUR, Ogre::ColourValue::ZERO);
+		system->_setProjectionMatrix(mProjectMatrix);
 
 		Layer::doRender(_renderManager);
 
 		system->_setViewport(saveViewport);
+		system->_setProjectionMatrix(Ogre::Matrix4::IDENTITY);
 
 		mBuffer->doRender(_renderManager);
+	}
+
+	void RttLayer::setTextureFilterNone(bool _value)
+	{
+		mTextureFilterNone = _value;
+
+		if (mTextureFilterNone)
+			mBuffer->setRenderMode(VertexBuffer::RenderModeFilterNone);
+		else
+			mBuffer->setRenderMode(VertexBuffer::RenderModeNormal);
 	}
 
 } // namespace demo
