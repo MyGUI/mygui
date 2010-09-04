29eb942bd93aef27b1edef4c20e97fbf4c5f3ac3
George Evmenov <Altren@users.noreply.github.com>
2010-09-04 16:05:38 +0000

    add vera++ check

---
+++ Scripts/vera++/scripts/rules/PointerNearType.tcl
@@ -0,0 +1,15 @@
+# Don't use space characters before '*' in pointers declaration
+#if [regexp {^[^=\*]*\S+\w+\s\*\s\w+.*$} $line] {
+
+foreach f [getSourceFileNames] {
+    set lineNumber 1
+    foreach line [getAllLines $f] {
+
+        if [regexp {^[^=\*]*\S+\w+\s\*\s\w+[\)\s][;=].*$} $line] {
+            report $f $lineNumber "space character before '*' in pointer declaration"
+            report $f $lineNumber $line
+        }
+
+        incr lineNumber
+    }
+}
