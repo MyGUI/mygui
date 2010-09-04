b8bbe666a8cf7b5113dc53f1c5b8f236ebf934c7
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-09-04 16:14:30 +0000

    update vera++ rule

--- trunk/Scripts/vera++/scripts/rules/PointerNearType.tcl
+++ trunk/Scripts/vera++/scripts/rules/PointerNearType.tcl
@@ -5,7 +5,7 @@ foreach f [getSourceFileNames] {
     set lineNumber 1
     foreach line [getAllLines $f] {
 
-        if [regexp {^[^=\*]*\S+\w+\s\*\s\w+[\)\s][;=].*$} $line] {
+        if [regexp {^[^=\*]*\S+\w+\s\*\s\w+(\);|\s=\s|,\s).*$} $line] {
             report $f $lineNumber "space character before '*' in pointer declaration"
             report $f $lineNumber $line
         }
