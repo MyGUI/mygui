12cacd14a577b315e0953315b2c98f80670ec0c7
George Evmenov <altren@gmail.com>
2010-09-07 05:36:48 +0000

    add Gray theme

--- Demos/Demo_Themes/DemoKeeper.cpp
+++ Demos/Demo_Themes/DemoKeeper.cpp
@@ -35,19 +35,39 @@ namespace demo
 			MyGUI::LanguageManager::getInstance().loadUserTags("core_theme_black_orange_tag.xml");
 			MyGUI::ResourceManager::getInstance().load("core_skin.xml");
 		}
+		else if (_index == 3)
+		{
+			MyGUI::LanguageManager::getInstance().loadUserTags("core_theme_grayscale_tag.xml");
+			MyGUI::ResourceManager::getInstance().load("core_skin.xml");
+		}
 
 		MyGUI::VectorWidgetPtr windows = MyGUI::LayoutManager::getInstance().loadLayout("Themes.layout");
 		MYGUI_ASSERT(windows.size() == 1, "Error load layout");
 		mDemoView = windows[0];
+		if (_index == 3)
+		{
+			colourWidgets(mDemoView, MyGUI::Colour::Green);
+		}
 
 		mComboSkins = getGUI()->findWidget<MyGUI::ComboBox>("Combo");
 		mComboSkins->setComboModeDrop(true);
 		mComboSkins->addItem("core");
 		mComboSkins->addItem("black & blue");
 		mComboSkins->addItem("black & orange");
+		mComboSkins->addItem("gray");
 
 		mComboSkins->setIndexSelected(_index);
 		mComboSkins->eventComboAccept += MyGUI::newDelegate(this, &DemoKeeper::notifyComboAccept);
+	}
+
+	void DemoKeeper::colourWidgets(MyGUI::Widget* _widget, const MyGUI::Colour& _colour)
+	{
+		_widget->setColour(_colour);
+		MyGUI::EnumeratorWidgetPtr enumerator = _widget->getEnumerator();
+		while (enumerator.next())
+		{
+			colourWidgets(enumerator.current(), _colour);
+		}
 	}
 
 	void DemoKeeper::notifyComboAccept(MyGUI::ComboBox* _sender, size_t _index)
