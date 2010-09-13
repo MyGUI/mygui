404481f045dde85ade857a8818d41c7cc2f67146
my_name <my.name.post@gmail.com>
2010-09-13 17:51:35 +0000

    update window

--- MyGUIEngine/include/MyGUI_Window.h
+++ MyGUIEngine/include/MyGUI_Window.h
@@ -119,15 +119,14 @@ namespace MyGUI
 		virtual void initialiseOverride();
 		virtual void shutdownOverride();
 
-		// переопределяем для присвоению клиенту
-		//virtual Widget* baseCreateWidget(WidgetStyle _style, const std::string& _type, const std::string& _skin, const IntCoord& _coord, Align _align, const std::string& _layer, const std::string& _name);
-
 		void onMouseChangeRootFocus(bool _focus);
 		void onKeyChangeRootFocus(bool _focus);
 		void onMouseDrag(int _left, int _top);
 		void onMouseButtonPressed(int _left, int _top, MouseButton _id);
+		void onMouseButtonReleased(int _left, int _top, MouseButton _id);
 
 		void notifyMousePressed(MyGUI::Widget* _sender, int _left, int _top, MouseButton _id);
+		void notifyMouseReleased(MyGUI::Widget* _sender, int _left, int _top, MouseButton _id);
 		void notifyPressedButtonEvent(MyGUI::Widget* _sender);
 		void notifyMouseDrag(MyGUI::Widget* _sender, int _left, int _top);
 
