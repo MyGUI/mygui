f1972029a9f187418ef3a7af45cf1724228b103a
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-09-21 22:16:22 +0000

    SE: update hot keys

--- trunk/Tools/SkinEditor/SeparatorTextureControl.h
+++ trunk/Tools/SkinEditor/SeparatorTextureControl.h
@@ -32,14 +32,18 @@ namespace tools
 		virtual void updateStateProperty(Property* _sender, const MyGUI::UString& _owner);
 		virtual void updateSeparatorProperty(Property* _sender, const MyGUI::UString& _owner);
 
+		void onMouseButtonClick(const MyGUI::IntPoint& _point);
+
 	private:
 		void notifyChangePosition();
+		void notifySettingsChanged(const MyGUI::UString& _sectorName, const MyGUI::UString& _propertyName);
 
 		void updateTextureControl();
 		void updateTextureName();
 		void updateRegionSize();
 		void updateRegionPosition();
 		void updateTextureVisible();
+		void updateFromPointValue();
 
 		void updatePosition();
 		void updateVisible();
@@ -47,6 +51,20 @@ namespace tools
 		void updateUnselectedStates();
 		void addCoord(std::vector<int>& _coordsHor, std::vector<int>& _coordsVert, MyGUI::Align _corner, const MyGUI::UString& _position);
 		void drawUnselectedStates(std::vector<int>& _coordsHor, std::vector<int>& _coordsVert);
+
+		bool checkCommand();
+		int toGrid(int _value);
+
+		void CommandMoveLeft(const MyGUI::UString& _commandName, bool& _result);
+		void CommandMoveRight(const MyGUI::UString& _commandName, bool& _result);
+		void CommandMoveTop(const MyGUI::UString& _commandName, bool& _result);
+		void CommandMoveBottom(const MyGUI::UString& _commandName, bool& _result);
+		void CommandGridMoveLeft(const MyGUI::UString& _commandName, bool& _result);
+		void CommandGridMoveRight(const MyGUI::UString& _commandName, bool& _result);
+		void CommandGridMoveTop(const MyGUI::UString& _commandName, bool& _result);
+		void CommandGridMoveBottom(const MyGUI::UString& _commandName, bool& _result);
+
+		MyGUI::Align getCorner();
 
 	private:
 		MyGUI::UString mTextureName;
@@ -60,6 +78,9 @@ namespace tools
 
 		std::vector<HorizontalSelectorBlackControl*> mHorizontalBlackSelectors;
 		std::vector<VerticalSelectorBlackControl*> mVerticalBlackSelectors;
+
+		int mGridStep;
+		int mValue;
 	};
 
 } // namespace tools
