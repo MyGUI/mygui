9a7b18231292e4e90c04489a7991a540225d7982
my_name <mynameco@users.noreply.github.com>
2010-10-21 10:21:49 +0000

    fix pointer for OIS

--- Tools/LayoutEditor/Application.cpp
+++ Tools/LayoutEditor/Application.cpp
@@ -55,6 +55,7 @@ namespace tools
 		MyGUI::FactoryManager::getInstance().registerFactory<MyGUI::FilterNone>("BasisSkin");
 
 		MyGUI::ResourceManager::getInstance().load("EditorLayer.xml");
+		MyGUI::ResourceManager::getInstance().load("EditorSettings.xml");
 
 		getStatisticInfo()->setVisible(false);
 
