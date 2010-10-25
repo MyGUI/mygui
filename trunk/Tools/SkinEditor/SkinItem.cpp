8601c929f87943e597c6e1b70fec1807d9d04233
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-10-25 12:57:20 +0000

    SE: change default parameters

--- trunk/Tools/SkinEditor/SkinItem.cpp
+++ trunk/Tools/SkinEditor/SkinItem.cpp
@@ -56,22 +56,22 @@ namespace tools
 		separator = mSeparators.createChild();
 		separator->setName("Top");
 		separator->setCorner(MyGUI::Align::Top);
-		separator->getPropertySet()->setPropertyValue("Offset", "6", "");
+		separator->getPropertySet()->setPropertyValue("Offset", "1", "");
 
 		separator = mSeparators.createChild();
 		separator->setName("Bottom");
 		separator->setCorner(MyGUI::Align::Bottom);
-		separator->getPropertySet()->setPropertyValue("Offset", "6", "");
+		separator->getPropertySet()->setPropertyValue("Offset", "1", "");
 
 		separator = mSeparators.createChild();
 		separator->setName("Left");
 		separator->setCorner(MyGUI::Align::Left);
-		separator->getPropertySet()->setPropertyValue("Offset", "6", "");
+		separator->getPropertySet()->setPropertyValue("Offset", "1", "");
 
 		separator = mSeparators.createChild();
 		separator->setName("Right");
 		separator->setCorner(MyGUI::Align::Right);
-		separator->getPropertySet()->setPropertyValue("Offset", "6", "");
+		separator->getPropertySet()->setPropertyValue("Offset", "1", "");
 
 
 		RegionItem* region = nullptr;
