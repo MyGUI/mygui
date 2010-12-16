7d241183ae8bbdece38c987c0bbd074e7b96479b
my_name <my.name.post@gmail.com>
2010-12-16 08:15:47 +0000

    LE: refactoring property fields

--- Tools/LayoutEditor/PropertiesPanelView.h
+++ Tools/LayoutEditor/PropertiesPanelView.h
@@ -7,6 +7,7 @@
 #define __PROPERTIES_PANEL_VIEW_H__
 
 #include "BaseLayout/BaseLayout.h"
+#include "PropertyField.h"
 #include "PanelView.h"
 #include "PanelMainProperties.h"
 #include "PanelProperties.h"
@@ -17,35 +18,6 @@
 
 namespace tools
 {
-	class Entry
-	{
-	public:
-		Entry();
-
-		void createPropertiesWidgetsPair(MyGUI::Widget* _window, const std::string& _property, const std::string& _value, const std::string& _type, int y, MyGUI::TextBox*& _field, int _height, MyGUI::Widget* _currentWidget, EditorToolTip* _toolTip);
-		void destroy();
-
-	private:
-		void notifyApplyProperties(MyGUI::Widget* _sender, bool _force);
-		void notifyTryApplyProperties(MyGUI::EditBox* _sender);
-		void notifyForceApplyProperties(MyGUI::EditBox* _widget);
-		void notifyForceApplyProperties2(MyGUI::ComboBox* _widget, size_t _index);
-
-		void notifyToolTip(MyGUI::Widget* _sender, const MyGUI::ToolTipInfo& _info);
-		SkinInfo getCellData(MyGUI::Widget* _sender, size_t _index);
-
-		bool checkType(MyGUI::EditBox* _edit, const std::string& _type);
-		bool isSkinExist(const std::string& _skinName);
-		std::string splitString(std::string& str, char separator);
-		bool checkTemplate(const std::string& _skinName);
-
-	private:
-		MyGUI::TextBox* mText;
-		MyGUI::Widget* mField;
-		MyGUI::Widget* mCurrentWidget;
-		EditorToolTip* mToolTip;
-	};
-
 	class PropertiesPanelView :
 		public wraps::BaseLayout
 	{
@@ -66,9 +38,8 @@ namespace tools
 		MyGUI::IntSize mOldSize;
 		PanelView* mPanelView;
 
-		typedef std::vector<Entry> VectorEntry;
-
-		typedef std::map<MyGUI::Widget*, VectorEntry> MapInfo;
+		typedef std::vector<PropertyField> VectorPropertyField;
+		typedef std::map<MyGUI::Widget*, VectorPropertyField> MapInfo;
 		MapInfo mPropertyInfo;
 
 		PanelMainProperties* mPanelMainProperties;
