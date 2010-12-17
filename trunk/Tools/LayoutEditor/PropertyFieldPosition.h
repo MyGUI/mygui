c7c040e6197821255d372f44db44a646256144ad
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2010-12-17 11:51:12 +0000

    LE: fix erase iterator

--- trunk/Tools/LayoutEditor/PropertyFieldPosition.h
+++ trunk/Tools/LayoutEditor/PropertyFieldPosition.h
@@ -7,20 +7,43 @@
 #define __PROPERTY_FIELD_POSITION_H__
 
 #include "EditorToolTip.h"
-#include "PropertyFieldEditBox.h"
+#include "BaseLayout/BaseLayout.h"
+#include "IPropertyField.h"
 
 namespace tools
 {
 	class PropertyFieldPosition :
-		public PropertyFieldEditBox
+		public wraps::BaseLayout,
+		public IPropertyField
 	{
 	public:
 		PropertyFieldPosition(MyGUI::Widget* _parent);
 		virtual ~PropertyFieldPosition();
 
+		virtual void initialise(const std::string& _type, MyGUI::Widget* _currentWidget);
+
+		virtual void setValue(const std::string& _value);
+		virtual void setName(const std::string& _value);
+
+		virtual MyGUI::IntSize getContentSize();
+		virtual void setCoord(const MyGUI::IntCoord& _coord);
+
+	private:
+		void notifyApplyProperties(MyGUI::Widget* _sender, bool _force);
+		void notifyTryApplyProperties(MyGUI::EditBox* _sender);
+		void notifyForceApplyProperties(MyGUI::EditBox* _widget);
+
 	protected:
 		virtual bool onCheckValue();
 		virtual void onAction(const std::string& _value, bool _force);
+
+	protected:
+		MyGUI::TextBox* mText;
+		MyGUI::EditBox* mField;
+		MyGUI::Button* mButton;
+		MyGUI::Widget* mCurrentWidget;
+		std::string mType;
+		std::string mName;
 	};
 
 } // namespace tools
