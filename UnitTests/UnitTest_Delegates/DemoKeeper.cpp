4f5c8bedb12ae4bc20150de0962784350ca6527c
George Evmenov <altren@gmail.com>
2011-01-24 21:18:11 +0000

    add UnitTest_Delegates

---
+++ UnitTests/UnitTest_Delegates/DemoKeeper.cpp
@@ -0,0 +1,95 @@
+/*!
+	@file
+	@author		George Evmenov
+	@date		01/2011
+*/
+#include "Precompiled.h"
+#include "DemoKeeper.h"
+#include "Base/Main.h"
+
+#include "FunctorDelegate.h"
+
+#define BOOST
+
+#ifdef BOOST
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/bind.hpp>
+#endif
+
+namespace demo
+{
+
+#ifdef BOOST
+	typedef boost::function<void (MyGUI::WidgetPtr _sender)> Delegate_W_Type;
+
+	class SomeClass
+	{
+	public:
+		SomeClass(int _value) :
+			mValue(_value)
+		{
+		}
+
+		int getValue() { return mValue; }
+	private:
+		int mValue;
+	};
+
+	typedef boost::shared_ptr<SomeClass> SomeClassPtr;
+
+	static void Delegate_W(SomeClassPtr _foo, MyGUI::WidgetPtr _sender)
+	{
+		_sender->castType<MyGUI::Button>()->setCaption("Functor call. " + MyGUI::utility::toString(_foo->getValue()));
+	}
+#endif
+
+	void handleClick_GlobalFunction(MyGUI::Widget* _sender)
+	{
+		_sender->castType<MyGUI::Button>()->setCaption("Function call");
+	}
+
+	void DemoKeeper::createScene()
+	{
+		MyGUI::Gui* gui = MyGUI::Gui::getInstancePtr();
+		MyGUI::Button* button1 = gui->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(10, 10, 200, 30), MyGUI::Align::Default, "Main");
+		button1->setCaption("Function");
+		button1->eventMouseButtonClick += MyGUI::newDelegate(handleClick_GlobalFunction);
+
+		MyGUI::Button* button2 = gui->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(10, 50, 200, 30), MyGUI::Align::Default, "Main");
+		button2->setCaption("Class method");
+		button2->eventMouseButtonClick += MyGUI::newDelegate(this, &DemoKeeper::handleClick_MemberFunction);
+
+		MyGUI::Button* button3 = gui->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(10, 90, 200, 30), MyGUI::Align::Default, "Main");
+		button3->setCaption("Static class method");
+		button3->eventMouseButtonClick += MyGUI::newDelegate(handleClick_StaticMemberFunction);
+		// or
+		//button3->eventMouseButtonClick += MyGUI::newDelegate(DemoKeeper::handleClick_StaticMemberFunction);
+
+#ifdef BOOST
+		MyGUI::Button* button4 = gui->createWidget<MyGUI::Button>("Button", MyGUI::IntCoord(10, 130, 200, 30), MyGUI::Align::Default, "Main");
+		button4->setCaption("Boost functor");
+		button4->eventMouseButtonClick += MyGUI::newDelegate(handleClick_StaticMemberFunction);
+		SomeClassPtr classInstance(new SomeClass(4));
+		Delegate_W_Type f = boost::bind(Delegate_W, classInstance, _1);
+		button4->eventMouseButtonClick += MyGUI::newFunctorDelegate<Delegate_W_Type, MyGUI::WidgetPtr>(f);
+#endif
+	}
+
+	void DemoKeeper::destroyScene()
+	{
+	}
+
+	void DemoKeeper::handleClick_MemberFunction(MyGUI::Widget* _sender)
+	{
+		_sender->castType<MyGUI::Button>()->setCaption("Class method call");
+	}
+
+	void DemoKeeper::handleClick_StaticMemberFunction(MyGUI::Widget* _sender)
+	{
+		_sender->castType<MyGUI::Button>()->setCaption("Static class method call");
+	}
+
+} // namespace demo
+
+MYGUI_APP(demo::DemoKeeper)
