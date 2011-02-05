88d6325bc5f7aa5e04e71cb266ac76ba3f761d54
George Evmenov <altren@gmail.com>
2011-02-05 19:03:25 +0000

    update BerkeliumWidget plugin (works with Berkelium 8.0.552.23)

--- Plugins/Plugin_BerkeliumWidget/BerkeliumWidget.cpp
+++ Plugins/Plugin_BerkeliumWidget/BerkeliumWidget.cpp
@@ -4,8 +4,9 @@
 	@date		02/2010
 */
 #include "BerkeliumWidget.h"
-#include "MyGUI.h"
 #include "../../Common/Input/InputConverter.h"
+#include <berkelium/Berkelium.hpp>
+#include <berkelium/Context.hpp>
 
 namespace MyGUI
 {
@@ -21,12 +22,16 @@ namespace MyGUI
 	{
 		Base::initialiseOverride();
 
+		// FIXME нам нужен фокус клавы
+		setNeedKeyFocus(true);
+
 		updateSize();
 
 		createTexture(mOldWidth, mOldHeight, TRM_PT_VIEW_REQUESTED);
 		requestUpdateCanvas = newDelegate(this, &BerkeliumWidget::notifyUpdateCanvas);
 
-		mWindow = Berkelium::Window::create();
+		Berkelium::Context *context = Berkelium::Context::create();
+		mWindow = Berkelium::Window::create(context);
 
 		updateSize();
 
@@ -41,9 +46,11 @@ namespace MyGUI
 	{
 		MyGUI::Gui::getInstance().eventFrameStart -= MyGUI::newDelegate(this, &BerkeliumWidget::notifyFrameStart);
 
+		destroyTexture();
+
 		if (mWindow != nullptr)
 		{
-			delete mWindow;
+			mWindow->destroy();
 			mWindow = nullptr;
 		}
 		requestUpdateCanvas = nullptr;
@@ -56,6 +63,91 @@ namespace MyGUI
 		mOldWidth = std::max(1, getWidth());
 		mOldHeight = std::max(1, getHeight());
 	}
+
+	////////////////////
+
+	void BerkeliumWidget::onAddressBarChanged(Berkelium::Window *win, Berkelium::URLString newURL) {}
+	void BerkeliumWidget::onStartLoading(Berkelium::Window *win, Berkelium::URLString newURL) {}
+	void BerkeliumWidget::onLoad(Berkelium::Window *win) {}
+	void BerkeliumWidget::onCrashedWorker(Berkelium::Window *win) {}
+	void BerkeliumWidget::onCrashedPlugin(Berkelium::Window *win, Berkelium::WideString pluginName) {}
+	void BerkeliumWidget::onProvisionalLoadError(Berkelium::Window *win,Berkelium:: URLString url,
+												 int errorCode, bool isMainFrame) {}
+	void BerkeliumWidget::onConsoleMessage(Berkelium::Window *win, Berkelium::WideString message,
+										   Berkelium::WideString sourceId, int line_no) {}
+	void BerkeliumWidget::onScriptAlert(Berkelium::Window *win, Berkelium::WideString message,
+										Berkelium::WideString defaultValue, Berkelium::URLString url,
+										int flags, bool &success, Berkelium::WideString &value) {}
+	void BerkeliumWidget::freeLastScriptAlert(Berkelium::WideString lastValue) {}
+	void BerkeliumWidget::onNavigationRequested(Berkelium::Window *win, Berkelium::URLString newUrl,
+												Berkelium::URLString referrer, bool isNewWindow,
+												bool &cancelDefaultAction) {}
+	void BerkeliumWidget::onLoadingStateChanged(Berkelium::Window *win, bool isLoading) {}
+	void BerkeliumWidget::onTitleChanged(Berkelium::Window *win, Berkelium::WideString title) {}
+	void BerkeliumWidget::onTooltipChanged(Berkelium::Window *win, Berkelium::WideString text) {}
+	void BerkeliumWidget::onCrashed(Berkelium::Window *win) {}
+	void BerkeliumWidget::onUnresponsive(Berkelium::Window *win) {}
+	void BerkeliumWidget::onResponsive(Berkelium::Window *win) {}
+	void BerkeliumWidget::onExternalHost(
+										 Berkelium::Window *win,
+										 Berkelium::WideString message,
+										 Berkelium::URLString origin,
+										 Berkelium::URLString target) {}
+	void BerkeliumWidget::onCreatedWindow(Berkelium::Window *win, Berkelium::Window *newWindow,
+										  const Berkelium::Rect &initialRect) {}
+	void BerkeliumWidget::onPaint(
+								  Berkelium::Window *win,
+								  const unsigned char *sourceBuffer,
+								  const Berkelium::Rect &sourceBufferRect,
+								  size_t numCopyRects,
+								  const Berkelium::Rect *copyRects,
+								  int dx, int dy,
+								  const Berkelium::Rect &scrollRect)
+	{
+		if (mWindow == nullptr)	return;
+
+		mBuffer.scroll(dx, dy, scrollRect.left(), scrollRect.top(), scrollRect.width(), scrollRect.height());
+
+		for(size_t i = 0; i < numCopyRects; i++)
+		{
+
+			const Berkelium::Rect &updateRect = copyRects[i];
+
+			mBuffer.update((void*)sourceBuffer, updateRect.left(), updateRect.top(), updateRect.width(), updateRect.height(),
+				updateRect.left() - sourceBufferRect.left(), updateRect.top() - sourceBufferRect.top(), sourceBufferRect.width());
+		}	
+	}
+
+	void BerkeliumWidget::onWidgetCreated(Berkelium::Window *win, Berkelium::Widget *newWidget, int zIndex) {}
+	void BerkeliumWidget::onWidgetDestroyed(Berkelium::Window *win, Berkelium::Widget *wid) {}
+	void BerkeliumWidget::onWidgetResize(
+										 Berkelium::Window *win,
+										 Berkelium::Widget *wid,
+										 int newWidth,
+										 int newHeight) {}
+	void BerkeliumWidget::onWidgetMove(
+									   Berkelium:: Window *win,
+									   Berkelium::Widget *wid,
+									   int newX,
+									   int newY) {}
+	void BerkeliumWidget::onWidgetPaint(
+										Berkelium::Window *win,
+										Berkelium::Widget *wid,
+										const unsigned char *sourceBuffer,
+										const Berkelium::Rect &sourceBufferRect,
+										size_t numCopyRects,
+										const Berkelium::Rect *copyRects,
+										int dx, int dy,
+										const Berkelium::Rect &scrollRect) {}
+	void BerkeliumWidget::onCursorUpdated(Berkelium::Window *win, const Berkelium::Cursor& newCursor)
+	{
+		int a = 0;
+	}
+	void BerkeliumWidget::onShowContextMenu(Berkelium::Window *win,
+											const Berkelium::ContextMenuEventArgs& args) {}
+
+
+	///////////////////////
 
 	void BerkeliumWidget::notifyUpdateCanvas(Canvas* _canvas, Canvas::Event _event)
 	{
@@ -88,15 +180,6 @@ namespace MyGUI
 	{
 		if (mWindow != nullptr)
 			mWindow->navigateTo(_url.data(), _url.length());
-	}
-
-	void BerkeliumWidget::onPaint(Berkelium::Window* win, const unsigned char* sourceBuffer, const Berkelium::Rect& rect, int dx, int dy, const Berkelium::Rect& scrollRect)
-	{
-		if (mWindow == nullptr)
-			return;
-
-		mBuffer.scroll(dx, dy, scrollRect.left(), scrollRect.top(), scrollRect.width(), scrollRect.height());
-		mBuffer.update((void*)sourceBuffer, rect.left(), rect.top(), rect.width(), rect.height());
 	}
 
 	void BerkeliumWidget::notifyFrameStart(float _time)
@@ -221,7 +304,7 @@ namespace MyGUI
 			loadURL(_value);
 		else
 		{
-			Base::setProperty(_key, _value);
+			Base::setPropertyOverride(_key, _value);
 			return;
 		}
 		eventChangeProperty(this, _key, _value);
