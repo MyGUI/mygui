52843bc5b77b7098832e89a568ba3482d2cd728e
George Evmenov <altren@gmail.com>
2011-02-10 15:08:53 +0000

    Demo_PluginBerkeliumWidget: update layout abd generated code

--- Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.cpp
+++ Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.cpp
@@ -1,9 +1,19 @@
+/*!
+	@file
+	@author		George Evmenov
+	@date		02/2011
+*/
+#include "Precompiled.h"
 #include "BerkeliumBrowser.h"
 
 namespace demo
 {
 
-	BerkeliumBrowser::BerkeliumBrowser(MyGUI::Widget* _parent)
+	BerkeliumBrowser::BerkeliumBrowser(MyGUI::Widget* _parent) :
+		mWebPageBerkeliumWidget(nullptr),
+		mUrlEditBox(nullptr),
+		mBackButton(nullptr),
+		mForwardButton(nullptr)
 	{
 		initialiseByAttributes(this, _parent);
 	}
