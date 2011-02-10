98b0da524181b07fcb0b40134d346bc80522f0ed
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2011-02-10 20:33:33 +0000

    improve Demo_PluginBerkeliumWidget

--- trunk/Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.cpp
+++ trunk/Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.cpp
@@ -16,10 +16,44 @@ namespace demo
 		mForwardButton(nullptr)
 	{
 		initialiseByAttributes(this, _parent);
+
+		mWebPageBerkeliumWidget->getBerkeliumWindow()->setTransparent(true);
+		mUrlEditBox->eventEditSelectAccept += MyGUI::newDelegate(this, &BerkeliumBrowser::notifyEditSelectAccept);
+		mBackButton->eventMouseButtonClick += MyGUI::newDelegate(this, &BerkeliumBrowser::notifyBackButtonClick);
+		mForwardButton->eventMouseButtonClick += MyGUI::newDelegate(this, &BerkeliumBrowser::notifyForwardButtonClick);
+
+		mWebPageBerkeliumWidget->setWindowDelegate(this);
 	}
 
 	BerkeliumBrowser::~BerkeliumBrowser()
 	{
+	}
+
+	void BerkeliumBrowser::notifyEditSelectAccept(MyGUI::EditBox* _sender)
+	{
+		mWebPageBerkeliumWidget->loadURL(_sender->getOnlyText());
+	}
+
+	void BerkeliumBrowser::notifyBackButtonClick(MyGUI::Widget* _sender)
+	{
+		mWebPageBerkeliumWidget->getBerkeliumWindow()->goBack();
+	}
+
+	void BerkeliumBrowser::notifyForwardButtonClick(MyGUI::Widget* _sender)
+	{
+		mWebPageBerkeliumWidget->getBerkeliumWindow()->goForward();
+	}
+
+	void BerkeliumBrowser::onAddressBarChanged(Berkelium::Window *win, Berkelium::URLString newURL)
+	{
+		mUrlEditBox->setCaption(newURL.data());
+		mBackButton->setEnabled(mWebPageBerkeliumWidget->getBerkeliumWindow()->canGoBack());
+		mForwardButton->setEnabled(mWebPageBerkeliumWidget->getBerkeliumWindow()->canGoForward());
+	}
+
+	void BerkeliumBrowser::onLoadingStateChanged(Berkelium::Window *win, bool isLoading)
+	{
+		mLoadingProgressBar->setVisible(isLoading);
 	}
 
 } // demo
--- trunk/Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.h
+++ trunk/Demos/Demo_PluginBerkeliumWidget/BerkeliumBrowser.h
@@ -8,18 +8,28 @@
 
 #include "BaseLayout/BaseLayout.h"
 #include "BerkeliumWidget.h"
+#include "BerkeliumDelegate.h"
 
 namespace demo
 {
 
 	ATTRIBUTE_CLASS_LAYOUT(BerkeliumBrowser, "BerkeliumBrowser.layout");
 	class BerkeliumBrowser :
-		public wraps::BaseLayout
+		public wraps::BaseLayout,
+		public MyGUI::BerkeliumDelegate
 	{
 	public:
 		BerkeliumBrowser(MyGUI::Widget* _parent = nullptr);
 		virtual ~BerkeliumBrowser();
 
+		void notifyEditSelectAccept(MyGUI::EditBox* _sender);
+		void notifyBackButtonClick(MyGUI::Widget* _sender);
+		void notifyForwardButtonClick(MyGUI::Widget* _sender);
+
+	protected:
+		// override Berkelium::WindowDelegate functions
+		virtual void onAddressBarChanged(Berkelium::Window *win, Berkelium::URLString newURL);
+		virtual void onLoadingStateChanged(Berkelium::Window *win, bool isLoading);
 	private:
 	//%LE Widget_Declaration list start
 		ATTRIBUTE_FIELD_WIDGET_NAME(BerkeliumBrowser, mWebPageBerkeliumWidget, "WebPage");
@@ -30,6 +40,8 @@ namespace demo
 		MyGUI::Button* mBackButton;
 		ATTRIBUTE_FIELD_WIDGET_NAME(BerkeliumBrowser, mForwardButton, "Forward");
 		MyGUI::Button* mForwardButton;
+		ATTRIBUTE_FIELD_WIDGET_NAME(BerkeliumBrowser, mLoadingProgressBar, "Loading");
+		MyGUI::ProgressBar* mLoadingProgressBar;
 	//%LE Widget_Declaration list end
 	};
 
