83f4f55f2bdff3fcdd13b8c991536981cde3b134
my_name <my.name.post@gmail.com>
2011-02-13 15:46:30 +0000

    update test app

--- UnitTests/TestApp/DemoKeeper.cpp
+++ UnitTests/TestApp/DemoKeeper.cpp
@@ -1,6 +1,7 @@
 #include "Precompiled.h"
 #include "DemoKeeper.h"
 #include "Base/Main.h"
+#include "WordWrapPanel.h"
 
 namespace demo
 {
@@ -12,14 +13,55 @@ namespace demo
 	{
 		base::BaseManager::setupResources();
 		addResourceLocation(getRootMedia() + "/UnitTests/TestApp");
+		addResourceLocation(getRootMedia() + "/Common/Tools");
 	}
 
 	void DemoKeeper::createScene()
 	{
+		MyGUI::ResourceManager::getInstance().load("FrameworkFonts.xml");
+		MyGUI::FactoryManager::getInstance().registerFactory<MyGUI::WordWrapPanel>("Widget");
+
+		MyGUI::Window* window = MyGUI::Gui::getInstance().createWidget<MyGUI::Window>("WindowCSX", MyGUI::IntCoord(100, 100, 100, 100), MyGUI::Align::Default, "Main");
+		MyGUI::IntCoord coord = window->getClientCoord();
+		MyGUI::WordWrapPanel* panel = window->createWidget<MyGUI::WordWrapPanel>("PanelEmpty", MyGUI::IntCoord(0, 0, coord.width, coord.height), MyGUI::Align::Stretch);
+
+		panel->setFreezeContent(true);
+
+		srand(GetTickCount());
+
+		for (size_t index = 0; index < 60; ++ index)
+		{
+			if (rand() % 2)
+				addText(panel);
+			else
+				addImage(panel);
+		}
+
+		panel->setFreezeContent(false);
 	}
 
 	void DemoKeeper::destroyScene()
 	{
+		MyGUI::FactoryManager::getInstance().unregisterFactory<MyGUI::WordWrapPanel>("Widget");
+	}
+
+	void DemoKeeper::addText(MyGUI::Widget* _parent)
+	{
+		MyGUI::TextBox* text = _parent->createWidget<MyGUI::TextBox>("Button", MyGUI::IntCoord(0, 0, 10, 10), MyGUI::Align::Default);
+		const char* names[8] = { "ArrowPointerImage", "BeamPointerImage", "SizeLeftPointerImage", "SizeRightPointerImage", "SizeHorzPointerImage", "SizeVertPointerImage", "HandPointerImage", "LinkPointerImage" };
+		const char* fonts[3] = { "DejaVuSansFont.17", "DejaVuSansFont.14", "MicroFont.11" };
+		text->setCaption(names[rand() % 8]);
+		text->setTextAlign(MyGUI::Align::Left | MyGUI::Align::Bottom);
+		text->setFontHeight(rand() % 50 + 10);
+		text->setFontName(fonts[rand() %3 ]);
+	}
+
+	void DemoKeeper::addImage(MyGUI::Widget* _parent)
+	{
+		int sizeImage = rand() % 32 + 16;
+		MyGUI::ImageBox* image = _parent->createWidget<MyGUI::ImageBox>("ImageBox", MyGUI::IntCoord(0, 0, sizeImage, sizeImage), MyGUI::Align::Default);
+		const char* names[8] = { "ArrowPointerImage", "BeamPointerImage", "SizeLeftPointerImage", "SizeRightPointerImage", "SizeHorzPointerImage", "SizeVertPointerImage", "HandPointerImage", "LinkPointerImage" };
+		image->setItemResource(names[rand() % 8]);
 	}
 
 } // namespace demo
---
+++ UnitTests/TestApp/WordWrapPanel.cpp
@@ -0,0 +1,145 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		02/2011
+*/
+
+#include "WordWrapPanel.h"
+#include "MyGUI_TextBox.h"
+
+namespace MyGUI
+{
+
+	WordWrapPanel::WordWrapPanel() :
+		mOldWidth(0),
+		mFreezeContent(false),
+		mContentChanged(false)
+	{
+	}
+
+	void WordWrapPanel::setPosition(const IntPoint& _value)
+	{
+		Base::setPosition(_value);
+	}
+
+	void WordWrapPanel::setSize(const IntSize& _value)
+	{
+		Base::setSize(_value);
+
+		onSizeChanged(_value);
+	}
+
+	void WordWrapPanel::setCoord(const IntCoord& _value)
+	{
+		Base::setCoord(_value);
+
+		onSizeChanged(_value.size());
+	}
+
+	void WordWrapPanel::onSizeChanged(const IntSize& _size)
+	{
+		if (mOldWidth != _size.width)
+		{
+			mOldWidth = _size.width;
+			updateContent();
+		}
+	}
+
+	void WordWrapPanel::updateContent()
+	{
+		if (mFreezeContent)
+		{
+			mContentChanged = true;
+			return;
+		}
+
+		int currentWidth = 0;
+		int currentHeight = 0;
+		int maxWidth = getWidth();
+		int maxLineHeight = 0;
+		bool hasWidget = false;
+		size_t startLineIndex = 0;
+
+		size_t count = getChildCount();
+		for (size_t index = 0; index < count; ++ index)
+		{
+			Widget* child = getChildAt(index);
+			IntSize size = getWidgetSize(child);
+
+			if (((currentWidth + size.width) > maxWidth) && hasWidget)
+			{
+				alignChildLine(startLineIndex, index, currentHeight, maxLineHeight);
+
+				currentHeight += maxLineHeight;
+				currentWidth = size.width;
+				maxLineHeight = size.height;
+
+				startLineIndex = index;
+			}
+			else
+			{
+				currentWidth += size.width;
+				if (size.height > maxLineHeight)
+					maxLineHeight = size.height;
+			}
+
+			hasWidget = true;
+		}
+
+		if (hasWidget)
+			alignChildLine(startLineIndex, count, currentHeight, maxLineHeight);
+	}
+
+	void WordWrapPanel::onWidgetCreated(Widget* _widget)
+	{
+		Base::onWidgetCreated(_widget);
+
+		updateContent();
+	}
+
+	bool WordWrapPanel::getFreezeContent()
+	{
+		return mFreezeContent;
+	}
+
+	void WordWrapPanel::setFreezeContent(bool _value)
+	{
+		if (mFreezeContent != _value)
+		{
+			mFreezeContent = _value;
+
+			if (mFreezeContent)
+			{
+				mContentChanged = false;
+			}
+			else
+			{
+				if (mContentChanged)
+					updateContent();
+			}
+		}
+	}
+
+	void WordWrapPanel::alignChildLine(size_t _startIndex, size_t _stopIndex, int _top, int _height)
+	{
+		int currentWidth = 0;
+		for (size_t index = _startIndex; index < _stopIndex; ++ index)
+		{
+			Widget* child = getChildAt(index);
+			IntSize size = getWidgetSize(child);
+
+			child->setCoord(currentWidth, _top + _height - size.height, size.width, size.height);
+			currentWidth += size.width;
+		}
+	}
+
+	IntSize WordWrapPanel::getWidgetSize(Widget* _widget)
+	{
+		TextBox* text = _widget->castType<TextBox>(false);
+		if (text != nullptr)
+			return text->getSize() - text->getTextRegion().size() + text->getTextSize();
+
+		return _widget->getSize();
+	}
+
+} // namespace MyGUI
