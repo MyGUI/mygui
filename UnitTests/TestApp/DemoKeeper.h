7a49037f95d81a1cd0f57d36aee0812710120f43
my_name <my.name.post@gmail.com>
2011-04-12 21:21:12 +0000

    update hyper text

--- UnitTests/TestApp/DemoKeeper.h
+++ UnitTests/TestApp/DemoKeeper.h
@@ -24,6 +24,10 @@ namespace demo
 		void parseText(MyGUI::Widget* _parent, const std::string& _value);
 		void parseTag(MyGUI::Widget* _parent, const std::string& _value);
 
+		void OnTextLostFocus(MyGUI::Widget* _sender, MyGUI::Widget* _new);
+		void OnTextSetFocus(MyGUI::Widget* _sender, MyGUI::Widget* _old);
+		void OnTextClick(MyGUI::Widget* _sender);
+
 	private:
 		bool mBold;
 		bool mItalic;
@@ -33,10 +37,14 @@ namespace demo
 		bool mHeader1;
 		bool mHeader2;
 		bool mHeader3;
+		bool mColour;
+		bool mUrl;
+		MyGUI::Colour mColourValue;
 		MyGUI::IntSize mImageSize;
 		MyGUI::IntSize mSpacer;
 		std::string mDefaultTextSkin;
 		std::string mDefaultParagraphSkin;
+		std::string mUrlValue;
 	};
 
 } // namespace demo
