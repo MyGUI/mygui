MyGUI/mygui

commit b7bd8d634c1c7bfa34b9961129b64cf916f4fd47

Removed some unnecessary member variable initializations from CharInfo in a way that doesn't trigger Cppcheck warnings.
git-svn-id: https://my-gui.svn.sourceforge.net/svnroot/my-gui/trunk@4350 10a74664-c72d-0410-8fa9-ef67a5ae4784

MyGUIEngine/include/MyGUI_TextViewData.h
@@ -20,8 +20,7 @@ namespace MyGUI
 			mHeight(0.0f),
 			mAdvance(0.0f),
 			mBearingX(0.0f),
-			mBearingY(0.0f),
-			mColour(0)
+			mBearingY(0.0f)
 		{ }
 
 		CharInfo(
@@ -37,17 +36,11 @@ namespace MyGUI
 			mHeight(_height),
 			mAdvance(_advance),
 			mBearingX(_bearingX),
-			mBearingY(_bearingY),
-			mColour(0)
+			mBearingY(_bearingY)
 		{ }
 
 		CharInfo(uint32 _colour) :
 			mIsColour(true),
-			mWidth(0.0f),
-			mHeight(0.0f),
-			mAdvance(0.0f),
-			mBearingX(0.0f),
-			mBearingY(0.0f),
 			mColour(_colour)
 		{ }
 
@@ -93,13 +86,21 @@ namespace MyGUI
 
 	private:
 		bool mIsColour;
-		FloatRect mUVRect;
-		float mWidth;
-		float mHeight;
-		float mAdvance;
-		float mBearingX;
-		float mBearingY;
-		uint32 mColour;
+
+		union
+		{
+			struct
+			{
+				FloatRect mUVRect;
+				float mWidth;
+				float mHeight;
+				float mAdvance;
+				float mBearingX;
+				float mBearingY;
+			};
+
+			uint32 mColour;
+		};
 	};
 
 	typedef std::vector<CharInfo> VectorCharInfo;

commit 2dd244ee3459e37a425dad3f333b28bce1d6be58

Fixed some GCC compile errors in CharInfo that were introduced in r4350.

MyGUIEngine/include/MyGUI_TextViewData.h
@@ -15,13 +15,14 @@ namespace MyGUI
 	{
 	public:
 		CharInfo() :
-			mIsColour(false),
-			mWidth(0.0f),
-			mHeight(0.0f),
-			mAdvance(0.0f),
-			mBearingX(0.0f),
-			mBearingY(0.0f)
-		{ }
+			mIsColour(false)
+		{
+			mMetrics.mWidth = 0.0f;
+			mMetrics.mHeight = 0.0f;
+			mMetrics.mAdvance = 0.0f;
+			mMetrics.mBearingX = 0.0f;
+			mMetrics.mBearingY = 0.0f;
+		}
 
 		CharInfo(
 			const FloatRect& _rect,
@@ -31,13 +32,14 @@ namespace MyGUI
 			float _bearingX,
 			float _bearingY) :
 			mIsColour(false),
-			mUVRect(_rect),
-			mWidth(_width),
-			mHeight(_height),
-			mAdvance(_advance),
-			mBearingX(_bearingX),
-			mBearingY(_bearingY)
-		{ }
+			mUVRect(_rect)
+		{
+			mMetrics.mWidth = _width;
+			mMetrics.mHeight = _height;
+			mMetrics.mAdvance = _advance;
+			mMetrics.mBearingX = _bearingX;
+			mMetrics.mBearingY = _bearingY;
+		}
 
 		CharInfo(uint32 _colour) :
 			mIsColour(true),
@@ -51,27 +53,27 @@ namespace MyGUI
 
 		float getWidth() const
 		{
-			return mWidth;
+			return mMetrics.mWidth;
 		}
 
 		float getHeight() const
 		{
-			return mHeight;
+			return mMetrics.mHeight;
 		}
 
 		float getAdvance() const
 		{
-			return mAdvance;
+			return mMetrics.mAdvance;
 		}
 
 		float getBearingX() const
 		{
-			return mBearingX;
+			return mMetrics.mBearingX;
 		}
 
 		float getBearingY() const
 		{
-			return mBearingY;
+			return mMetrics.mBearingY;
 		}
 
 		const FloatRect& getUVRect() const
@@ -85,22 +87,25 @@ namespace MyGUI
 		}
 
 	private:
+
 		bool mIsColour;
+		FloatRect mUVRect;
+
+		struct Metrics
+		{
+			float mWidth;
+			float mHeight;
+			float mAdvance;
+			float mBearingX;
+			float mBearingY;
+		};
 
 		union
 		{
-			struct
-			{
-				FloatRect mUVRect;
-				float mWidth;
-				float mHeight;
-				float mAdvance;
-				float mBearingX;
-				float mBearingY;
-			};
-
+			Metrics mMetrics;
 			uint32 mColour;
 		};
+
 	};
 
 	typedef std::vector<CharInfo> VectorCharInfo;