865f16b31a476f47b5dc2402be92f3f3ebedbc9b
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-03-05 21:07:05 +0000

    fix bug with not determining ".." as folder on some file systems

--- trunk/Common/FileSystemInfo/FileSystemInfo.h
+++ trunk/Common/FileSystemInfo/FileSystemInfo.h
@@ -14,6 +14,9 @@
 #else
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #endif
 
 #include <string>
@@ -85,7 +88,7 @@ namespace common
 		while (lHandle != -1 && res != -1)
 		{
 			if (( !ms_IgnoreHidden || (tagData.attrib & _A_HIDDEN) == 0 ) &&
-				(!isReservedDir (tagData.name)))
+				!isReservedDir(tagData.name))
 			{
 				_result.push_back(FileInfo(concatenatePath(directory, tagData.name), (tagData.attrib & _A_SUBDIR) != 0));
 			}
@@ -107,8 +110,14 @@ namespace common
 
 		while ((dp = readdir (dir)) != NULL)
 		{
-			if (!isReservedDir (MyGUI::UString(dp->d_name).asWStr_c_str()))
-				_result.push_back(FileInfo(MyGUI::UString(dp->d_name).asWStr(), (dp->d_type == DT_DIR)));
+			if (!isReservedDir(MyGUI::UString(dp->d_name).asWStr_c_str()))
+			{
+				struct stat fInfo;
+				char path[NAME_MAX];
+				//snprintf(path, NAME_MAX, "%s/%s", MyGUI::UString(_folder).asUTF8_c_str(), dp->d_name);
+				if(stat(path, &fInfo) == -1)perror("stat");
+				_result.push_back(FileInfo(MyGUI::UString(dp->d_name).asWStr(), (S_ISDIR(fInfo.st_mode))));
+			}
 		}
 
 		closedir(dir);
