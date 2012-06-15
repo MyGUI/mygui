0a76814e8b9659fed65e85f5baa2debbe63b7d3c
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-06-15 09:39:46 +0000

    refactoring

--- trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Program.cs
+++ trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Program.cs
@@ -9,13 +9,9 @@ namespace DoxygenWrapper
 	{
 		private static void Main(string[] _args)
 		{
-			//XmlDocument doc = new XmlDocument();
-			//doc.LoadXml("<Document><Node1><InnerNode2> test2 </InnerNode2> test3 </Node1></Document>");
-			//doc.Load(@"D:\MyGUI\MyGUI_Trunk\Wrappers\WrapperGenerator\doxygen\xml\classMyGUI_1_1WidgetInput.xml");
 			CompoundManager manager = new CompoundManager();
 			manager.SetIgnoredNameList(new string[] { "MyGUI::Allocator", "MyGUI::Allocator::rebind" });
-			manager.SetIgnoredTypeList(new string[] { "define", "friend", "enumvalue", "enum", "file", "example", "dir" });
-			manager.Initialise(@"D:\MyGUI\MyGUI_Trunk\Wrappers\WrapperGenerator\doxygen\xml\index.xml");
+			manager.Initialise(@"..\..\..\..\WrapperGenerator\doxygen\xml");
 		}
 	}
 }
--- trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Wrappers/Compounds/CompoundNamespace.cs
+++ trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Wrappers/Compounds/CompoundNamespace.cs
@@ -1,10 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace DoxygenWrapper.Wrappers.Compounds
 {
 	public class CompoundNamespace :
 		Compound
 	{
+		protected override void OnParse(XmlNode _node)
+		{
+			base.OnParse(_node);
+
+			AddChildItems(_node, "innerclass");
+			AddChildItems(_node, "innernamespace");
+
+			ParseChildItems(_node);
+		}
+
+		private void ParseChildItems(XmlNode _node)
+		{
+			foreach (XmlNode node in _node.SelectNodes("sectiondef/memberdef"))
+			{
+				string id = node.Attributes["id"].Value;
+				if (CompoundManager.Instance.IsIgnoredCompound(id))
+					continue;
+
+				string kind = node.Attributes["kind"].Value;
+				if (CompoundManager.Instance.IsIgnoredType(kind))
+					continue;
+
+				Compound compound = CompoundManager.Instance.GetCompound(id);
+				if (compound == null)
+				{
+					ConsoleUtility.WriteErrorLine("Item {0} not found (for this {1})", id, RefID);
+				}
+				else
+				{
+					if (compound.Parent != this)
+					{
+						ConsoleUtility.WriteErrorLine("Item {0} not added for {1}", compound.RefID, RefID);
+					}
+					else
+					{
+						compound.Parse(node);
+					}
+				}
+			}
+		}
+
+		private void AddChildItems(XmlNode _node, string _name)
+		{
+			foreach (XmlNode node in _node.SelectNodes(_name))
+			{
+				string refid = node.Attributes["refid"].Value;
+				if (CompoundManager.Instance.IsIgnoredCompound(refid))
+					continue;
+
+				Compound compound = CompoundManager.Instance.GetCompound(refid);
+				if (compound == null)
+				{
+					ConsoleUtility.WriteErrorLine("Item {0} not found", refid);
+				}
+				else
+				{
+					AddChild(compound);
+				}
+			}
+		}
 	}
 }
