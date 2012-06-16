ac7ef120d6d7fbd2bde7acfae18c838f55d0c689
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-06-16 21:20:30 +0000

    update

--- trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Program.cs
+++ trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Program.cs
@@ -12,10 +12,10 @@ namespace DoxygenWrapper
 			CompoundManager compoundManager = new CompoundManager();
 			compoundManager.SetIgnoredNameList(new string[] { "MyGUI::Allocator", "MyGUI::Allocator::rebind" });
 			compoundManager.Initialise(@"..\..\..\..\WrapperGenerator\doxygen\xml");
-			compoundManager.DumpCompoundTree("CompoundTree.xml");
+			//compoundManager.DumpCompoundTree("CompoundTree.xml");
 
 			WrapperManager wrapperManager = new WrapperManager();
-			wrapperManager.Initialise(@"..\..\..\..\WrapperGenerator\Data\SharpData.xml");
+			wrapperManager.Initialise(@"..\..\..\Data\SharpData.xml");
 
 			ConsoleUtility.WriteLine("Complite");
 		}
