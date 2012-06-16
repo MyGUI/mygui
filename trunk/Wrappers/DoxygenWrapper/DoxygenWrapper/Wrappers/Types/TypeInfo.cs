dbaf000260f9436e9611010d6b2363699b64e78c
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-06-16 16:21:00 +0000

    update types

---
+++ trunk/Wrappers/DoxygenWrapper/DoxygenWrapper/Wrappers/Types/TypeInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DoxygenWrapper.Wrappers.Types
+{
+	public class TypeInfo
+	{
+		public TypeInfo(XmlNode _node)
+		{
+			mNode = _node;
+
+			mName = _node.Attributes["name"].Value;
+
+			foreach (XmlNode node in _node)
+				mReplace[node.Name] = node.InnerText;
+		}
+
+		public string Name
+		{
+			get { return mName; }
+		}
+
+		private XmlNode mNode;
+		private string mName;
+		private Dictionary<string, string> mReplace = new Dictionary<string, string>();
+	}
+}
