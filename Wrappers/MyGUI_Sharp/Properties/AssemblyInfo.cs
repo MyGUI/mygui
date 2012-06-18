e2ae00ca3487520931b2c5fa773385851346a581
my_name <my.name.post@gmail.com>
2012-06-18 11:00:29 +0000

    update

--- Wrappers/MyGUI_Sharp/Properties/AssemblyInfo.cs
+++ Wrappers/MyGUI_Sharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyGUI.Sharp")]
+[assembly: AssemblyTitle("MyGUI_Sharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyGUI.Sharp")]
+[assembly: AssemblyProduct("MyGUI_Sharp")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
