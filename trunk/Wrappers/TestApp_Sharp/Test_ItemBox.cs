ed5220d71c515a4269473ce1732ec5d9589447ea
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-06-19 17:09:15 +0000

    update test

--- trunk/Wrappers/TestApp_Sharp/Test_ItemBox.cs
+++ trunk/Wrappers/TestApp_Sharp/Test_ItemBox.cs
@@ -22,7 +22,10 @@ namespace TestApp.Sharp
 
             box.ResetDrag();
             Widget cell = box.GetWidgetByIndex(0);
-            uint index = box.GetIndexByWidget(cell);
+			if (cell != null)
+			{
+				uint index = box.GetIndexByWidget(cell);
+			}
             Widget drag = box.WidgetDrag;
             box.VerticalAlignment = !box.VerticalAlignment;
             box.ClearItemDataAt(0);
