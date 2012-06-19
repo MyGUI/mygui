48603b57a6d48e1afd7b0fe985341e3582e2c813
my_name <my.name.post@gmail.com>
2012-06-19 17:01:26 +0000

    update export platform

---
+++ Wrappers/TestApp_Sharp/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.Sharp
+{
+	public class Test
+	{
+		public static void TestWidgets()
+		{
+			Test_Button.Test();
+			Test_ComboBox.Test();
+			Test_DDContainer.Test();
+			Test_EditBox.Test();
+			Test_ItemBox.Test();
+			Test_ListBox.Test();
+			Test_MenuBar.Test();
+			Test_MenuControl.Test();
+			Test_MultiListBox.Test();
+			Test_PopupMenu.Test();
+			Test_ProgressBar.Test();
+			Test_ScrollView.Test();
+			Test_ImageBox.Test();
+			Test_TextBox.Test();
+			Test_TabControl.Test();
+			Test_ScrollBar.Test();
+			Test_Widget.Test();
+			Test_Window.Test();
+
+			Test_Gui.Test();
+		}
+	}
+}
