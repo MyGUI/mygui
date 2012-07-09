8157ef6071e53ac0eaf3375e7a8d621f60f5b8c7
my_name <my.name.post@gmail.com>
2012-07-09 11:07:27 +0000

    update list box control

---
+++ Tools/SkinEditor/ListBoxItemData.h
@@ -0,0 +1,20 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2012
+*/
+#ifndef __LIST_BOX_ITEM_DATA_H__
+#define __LIST_BOX_ITEM_DATA_H__
+
+namespace tools
+{
+	class ListBoxItemData
+	{
+	public:
+		ListBoxItemData()
+		{
+		}
+	};
+}
+
+#endif
