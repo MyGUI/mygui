10e5485820f974e3bdc8df322b3d97faa0fd0977
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-12 18:02:24 +0000

    update

--- trunk/Tools/ImageEditor/SeparatorControl.cpp
+++ trunk/Tools/ImageEditor/SeparatorControl.cpp
@@ -60,7 +60,7 @@ namespace tools
 		for (VectorWidget::const_iterator child = mChilds.begin(); child != mChilds.end(); child ++)
 		{
 			std::string separator = (*child)->getUserString("Separator");
-			if (separator.empty())
+			if (!separator.empty())
 			{
 				(*child)->eventMouseButtonPressed += MyGUI::newDelegate(this, &SeparatorControl::notifyMouseButtonPressed);
 				(*child)->eventMouseDrag += MyGUI::newDelegate(this, &SeparatorControl::notifyMouseDrag);
@@ -75,6 +75,8 @@ namespace tools
 				data->FirstPart = lastPart;
 				data->FirstSeparator = lastSeparator;
 				(*child)->setUserData(data);
+
+				lastSeparator = (*child);
 			}
 			else
 			{
@@ -90,14 +92,92 @@ namespace tools
 
 	void SeparatorControl::notifyMouseButtonPressed(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id)
 	{
-		if (_id == MyGUI::MouseButton::Left)
-		{
-			SeparatorData* data = *_sender->getUserData<SeparatorData*>();
-			data->PressedPoint = MyGUI::IntPoint(_left, _top);
-		}
+		if (_id != MyGUI::MouseButton::Left)
+			return;
+
+		SeparatorData* data = *_sender->getUserData<SeparatorData*>();
+		//data->PressedPoint = MyGUI::IntPoint(_left, _top);
+		data->OffsetPoint = MyGUI::IntPoint(_left, _top) - _sender->getAbsolutePosition();
 	}
 
 	void SeparatorControl::notifyMouseDrag(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id)
 	{
+		if (_id != MyGUI::MouseButton::Left)
+			return;
+
+		SeparatorData* data = *_sender->getUserData<SeparatorData*>();
+		MyGUI::IntPoint parentAbsolutePosition = _sender->getParent()->getAbsolutePosition();
+		MyGUI::IntPoint offset = MyGUI::IntPoint(_left, _top) - parentAbsolutePosition - data->OffsetPoint;
+
+		MoveSeparator(_sender, offset);
+	}
+
+	void SeparatorControl::MoveSeparator(MyGUI::Widget* _separator, const MyGUI::IntPoint& _point)
+	{
+		SeparatorData* data = *_separator->getUserData<SeparatorData*>();
+
+		if (data->FirstPart == nullptr || data->SecondPart == nullptr)
+			return;
+
+		MyGUI::IntCoord firstCoord = data->FirstPart->getCoord();
+		MyGUI::IntCoord secondCoord = data->SecondPart->getCoord();
+		MyGUI::IntCoord separatorCoord = _separator->getCoord();
+
+		firstCoord.width = _point.left - firstCoord.left;
+		separatorCoord.left = _point.left;
+		secondCoord.width = secondCoord.right() - (_point.left + separatorCoord.width);
+		secondCoord.left = _point.left + separatorCoord.width;
+
+		data->FirstPart->setCoord(firstCoord);
+		data->SecondPart->setCoord(secondCoord);
+		_separator->setCoord(separatorCoord);
+
+		int size = MyGUI::utility::parseValue<int>(data->FirstPart->getUserString("MinSize"));
+		if (size > firstCoord.width)
+		{
+			if (data->FirstSeparator != nullptr)
+			{
+				MoveSeparator(data->FirstSeparator, _point - MyGUI::IntPoint(size + data->FirstSeparator->getWidth(), data->FirstSeparator->getHeight()));
+			}
+
+			firstCoord = data->FirstPart->getCoord();
+			secondCoord = data->SecondPart->getCoord();
+			separatorCoord = _separator->getCoord();
+
+			int offset = size - firstCoord.width;
+
+			firstCoord.width += offset;
+			separatorCoord.left += offset;
+			secondCoord.width -= offset;
+			secondCoord.left += offset;
+
+			data->FirstPart->setCoord(firstCoord);
+			data->SecondPart->setCoord(secondCoord);
+			_separator->setCoord(separatorCoord);
+		}
+
+		/*size = MyGUI::utility::parseValue<int>(data->SecondPart->getUserString("MinSize"));
+		if (size > secondCoord.width)
+		{
+			if (data->SecondSeparator != nullptr)
+			{
+				MoveSeparator(data->SecondSeparator, _point + MyGUI::IntPoint(size + _separator->getWidth(), _separator->getHeight()));
+			}
+
+			firstCoord = data->FirstPart->getCoord();
+			secondCoord = data->SecondPart->getCoord();
+			separatorCoord = _separator->getCoord();
+
+			int offset = size - secondCoord.width;
+
+			firstCoord.width -= offset;
+			separatorCoord.left -= offset;
+			secondCoord.width += offset;
+			secondCoord.left -= offset;
+
+			data->FirstPart->setCoord(firstCoord);
+			data->SecondPart->setCoord(secondCoord);
+			_separator->setCoord(separatorCoord);
+		}*/
 	}
 }
