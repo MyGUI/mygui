4da9c540cea554a4f53746a4140cbbed16301b37
my_name <my.name.post@gmail.com>
2012-07-12 15:19:07 +0000

    update separators

--- Tools/ImageEditor/SeparatorControl.cpp
+++ Tools/ImageEditor/SeparatorControl.cpp
@@ -1,32 +1,103 @@
 /*!
 	@file
 	@author		Albert Semenov
-	@date		08/2010
+	@date		07/2012
 */
 #include "Precompiled.h"
 #include "SeparatorControl.h"
+#include "FactoryItemAttribute.h"
+#include "FactoryManager.h"
+#include "SeparatorUtility.h"
 
 namespace tools
 {
+	FACTORY_ITEM_ATTRIBUTE(SeparatorControl);
 
-	/*SeparatorControl::SeparatorControl(MyGUI::Widget* _parent) :
-		wraps::BaseLayout("SeparatorControl.layout", _parent),
-		mSeparatorListControl(nullptr),
-		mSeparatorPropertyControl(nullptr),
-		mSeparatorTextureControl(nullptr)
+	SeparatorControl::SeparatorControl() :
+		mHorizontal(false)
 	{
-		assignBase(mSeparatorListControl, "SeparatorListControl");
-		assignBase(mSeparatorPropertyControl, "SeparatorPropertyControl");
-		assignBase(mSeparatorTextureControl, "SeparatorTextureControl");
 	}
 
 	SeparatorControl::~SeparatorControl()
 	{
+		for (VectorWidget::const_iterator child = mChilds.begin(); child != mChilds.end(); child ++)
+		{
+			SeparatorData** data = (*child)->getUserData<SeparatorData*>(false);
+			if (data != nullptr)
+			{
+				delete (*data);
+				(*child)->setUserData(nullptr);
+			}
+		}
 	}
 
-	void SeparatorControl::setActivate(bool _value)
+	void SeparatorControl::Initialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName)
 	{
-		mSeparatorTextureControl->setActivate(_value);
-	}*/
+		Control::Initialise(_parent, _place, _layoutName);
 
-} // namespace tools
+		CreateSeparators();
+	}
+
+	void SeparatorControl::CreateSeparators()
+	{
+		for (size_t index = 0; index < mMainWidget->getChildCount(); index ++)
+		{
+			MyGUI::Widget* widget = mMainWidget->getChildAt(index);
+			mChilds.push_back(widget);
+
+			std::string separator = widget->getUserString("Separator");
+			if (separator == "Horizontal")
+				mHorizontal = true;
+		}
+
+		if (mHorizontal)
+			std::sort(mChilds.begin(), mChilds.end(), sortHorizontal);
+		else
+			std::sort(mChilds.begin(), mChilds.end(), sortVertical);
+
+		MyGUI::Widget* lastPart = nullptr;
+		MyGUI::Widget* lastSeparator = nullptr;
+		for (VectorWidget::const_iterator child = mChilds.begin(); child != mChilds.end(); child ++)
+		{
+			std::string separator = (*child)->getUserString("Separator");
+			if (separator.empty())
+			{
+				(*child)->eventMouseButtonPressed += MyGUI::newDelegate(this, &SeparatorControl::notifyMouseButtonPressed);
+				(*child)->eventMouseDrag += MyGUI::newDelegate(this, &SeparatorControl::notifyMouseDrag);
+
+				if (lastSeparator != nullptr)
+				{
+					SeparatorData* data = *lastSeparator->getUserData<SeparatorData*>();
+					data->SecondSeparator = (*child);
+				}
+
+				SeparatorData* data = new SeparatorData();
+				data->FirstPart = lastPart;
+				data->FirstSeparator = lastSeparator;
+				(*child)->setUserData(data);
+			}
+			else
+			{
+				lastPart = (*child);
+				if (lastSeparator != nullptr)
+				{
+					SeparatorData* data = *lastSeparator->getUserData<SeparatorData*>();
+					data->SecondPart = lastPart;
+				}
+			}
+		}
+	}
+
+	void SeparatorControl::notifyMouseButtonPressed(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id)
+	{
+		if (_id == MyGUI::MouseButton::Left)
+		{
+			SeparatorData* data = *_sender->getUserData<SeparatorData*>();
+			data->PressedPoint = MyGUI::IntPoint(_left, _top);
+		}
+	}
+
+	void SeparatorControl::notifyMouseDrag(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id)
+	{
+	}
+}
