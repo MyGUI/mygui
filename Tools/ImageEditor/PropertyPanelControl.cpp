0254862e7e5648b81dfef7c41bd4f8473c6cba59
my_name <my.name.post@gmail.com>
2012-07-15 08:40:58 +0000

    add property panel

---
+++ Tools/ImageEditor/PropertyPanelControl.cpp
@@ -0,0 +1,26 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2012
+*/
+#include "Precompiled.h"
+#include "PropertyPanelControl.h"
+#include "FactoryManager.h"
+
+namespace tools
+{
+	FACTORY_ITEM_ATTRIBUTE(PropertyPanelControl);
+
+	PropertyPanelControl::PropertyPanelControl()
+	{
+	}
+
+	PropertyPanelControl::~PropertyPanelControl()
+	{
+	}
+
+	void PropertyPanelControl::OnInitialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName)
+	{
+		Control::OnInitialise(_parent, _place, _layoutName);
+	}
+}
