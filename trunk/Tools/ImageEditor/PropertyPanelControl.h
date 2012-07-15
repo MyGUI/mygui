cadd6646ed3cf30e1203830b14ebafaf89d13ec2
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-15 08:40:58 +0000

    add property panel

---
+++ trunk/Tools/ImageEditor/PropertyPanelControl.h
@@ -0,0 +1,26 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2012
+*/
+
+#ifndef _a90345b7_27c4_4613_9148_281c8f996ded_
+#define _a90345b7_27c4_4613_9148_281c8f996ded_
+
+#include "Control.h"
+
+namespace tools
+{
+	class PropertyPanelControl :
+		public Control
+	{
+	public:
+		PropertyPanelControl();
+		virtual ~PropertyPanelControl();
+
+	protected:
+		virtual void OnInitialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName);
+	};
+}
+
+#endif
