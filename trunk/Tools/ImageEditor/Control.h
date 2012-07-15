075bf53f618667a3675d48a6f2f2fac71df5431f
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-15 07:25:41 +0000

    update color panel

--- trunk/Tools/ImageEditor/Control.h
+++ trunk/Tools/ImageEditor/Control.h
@@ -21,12 +21,37 @@ namespace tools
 
 		MyGUI::Widget* getRoot();
 
-		void Initialise(const std::string& _layoutName);
+		void Initialise(const std::string& _layoutName = "");
 
 		typedef std::vector<Control*> VectorControl;
 		const VectorControl& getChilds() const;
 
 		void SendCommand(const std::string& _command);
+
+		template <typename Type>
+		std::string GetLayoutName(Type* _owner)
+		{
+			return attribute::AttributeLayout<Type>::getData();
+		}
+
+		template <typename Type>
+		void InitialiseByAttributes(Type* _owner, MyGUI::Widget* _parent = nullptr, bool _throw = true, bool _createFakeWidgets = true)
+		{
+			typename attribute::AttributeFieldWidgetName<Type>::VectorBindPair& data = attribute::AttributeFieldWidgetName<Type>::getData();
+			for (typename attribute::AttributeFieldWidgetName<Type>::VectorBindPair::iterator item = data.begin(); item != data.end(); ++item)
+			{
+				MyGUI::Widget* value = nullptr;
+				assignWidget(value, item->second, _throw, false);
+
+				bool result = item->first->set(_owner, value);
+
+				if (!result && _createFakeWidgets)
+				{
+					value = CreateFakeWidgetT(item->first->getFieldTypeName(), mMainWidget);
+					item->first->set(_owner, value);
+				}
+			}
+		}
 
 	protected:
 		virtual void OnInitialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName);
@@ -40,6 +65,8 @@ namespace tools
 		void notifyTabChangeSelect(MyGUI::TabControl* _sender, size_t _index);
 		void notifyWindowButtonPressed(MyGUI::Window* _sender, const std::string& _name);
 		void notifyEditSelectAccept(MyGUI::EditBox* _sender);
+
+		MyGUI::Widget* CreateFakeWidgetT(const std::string& _typeName, MyGUI::Widget* _parent);
 
 	private:
 		VectorControl mChilds;
