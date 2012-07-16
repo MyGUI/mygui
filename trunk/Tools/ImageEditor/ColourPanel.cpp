7605b48eb56073417656a51dc87cf011773831f5
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-16 17:33:07 +0000

    refactoring

--- trunk/Tools/ImageEditor/ColourPanel.cpp
+++ trunk/Tools/ImageEditor/ColourPanel.cpp
@@ -11,7 +11,6 @@
 
 namespace tools
 {
-
 	FACTORY_ITEM_ATTRIBUTE(ColourPanel)
 
 	ColourPanel::ColourPanel() :
--- trunk/Tools/ImageEditor/FactoryItemAttribute.h
+++ trunk/Tools/ImageEditor/FactoryItemAttribute.h
@@ -7,38 +7,8 @@
 #ifndef _974be190_ed90_49c5_a20f_700745d10220_
 #define _974be190_ed90_49c5_a20f_700745d10220_
 
-#define FACTORY_ITEM_ATTRIBUTE(name) \
-class name; \
-namespace factories \
-{ \
-	namespace \
-	{ \
-		class Registrator \
-		{ \
-		public: \
-			Registrator() : \
-				mFactory(0) \
-			{ \
-				if (!::components::FactoryManager::GetInstancePtr()->ExistFactory(#name)) \
-				{ \
-					mFactory = new ::components::FactoryTemplate<name>(); \
-					::components::FactoryManager::GetInstancePtr()->RegisterFactory(mFactory, #name); \
-				} \
-			} \
-			~Registrator() \
-			{ \
-				if (mFactory != 0) \
-				{ \
-					::components::FactoryManager::GetInstancePtr()->UnregisterFactory(mFactory, #name); \
-					delete mFactory; \
-					mFactory = 0; \
-				} \
-			} \
-		private: \
-			::components::IFactory* mFactory; \
-		}; \
-		static Registrator instance; \
-	} \
-}
+#include "FactoryItemRegistrator.h"
+
+#define FACTORY_ITEM_ATTRIBUTE(type) namespace { static ::factories::FactoryItemRegistrator<type> instance(#type); }
 
 #endif
