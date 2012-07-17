76dc7912262c007670f66b9caf7ee8438f689bce
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-17 09:10:30 +0000

    add property

---
+++ trunk/Tools/ImageEditor/PropertyStringControl.cpp
@@ -0,0 +1,73 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		08/2010
+*/
+
+#include "Precompiled.h"
+#include "PropertyStringControl.h"
+#include "Localise.h"
+#include "FactoryManager.h"
+
+namespace tools
+{
+
+	FACTORY_ITEM_ATTRIBUTE(PropertyStringControl)
+
+	PropertyStringControl::PropertyStringControl() :
+		mName(nullptr),
+		mEdit(nullptr)
+	{
+	}
+
+	PropertyStringControl::~PropertyStringControl()
+	{
+		mEdit->eventEditTextChange -= MyGUI::newDelegate(this, &PropertyStringControl::notifyEditTextChange);
+	}
+
+	void PropertyStringControl::OnInitialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName)
+	{
+		Control::OnInitialise(_parent, _place, "PropertyEditControl.layout");
+
+		assignWidget(mName, "Name");
+		assignWidget(mEdit, "Edit");
+
+		mEdit->eventEditTextChange += MyGUI::newDelegate(this, &PropertyStringControl::notifyEditTextChange);
+	}
+
+	void PropertyStringControl::updateCaption()
+	{
+		Property* proper = getProperty();
+		if (proper != nullptr)
+			mName->setCaption(proper->getType()->getName());
+	}
+
+	void PropertyStringControl::updateProperty()
+	{
+		Property* proper = getProperty();
+		if (proper != nullptr)
+		{
+			mEdit->setEnabled(!proper->getType()->getReadOnly());
+			mEdit->setCaption(proper->getValue());
+		}
+		else
+		{
+			mEdit->setCaption("");
+			mEdit->setEnabled(false);
+		}
+	}
+
+	void PropertyStringControl::notifyEditTextChange(MyGUI::EditBox* _sender)
+	{
+		Property* proper = getProperty();
+		if (proper != nullptr)
+			proper->setValue(getClearValue());
+	}
+
+	MyGUI::UString PropertyStringControl::getClearValue()
+	{
+		MyGUI::UString value = mEdit->getOnlyText();
+		return value;
+	}
+
+}
