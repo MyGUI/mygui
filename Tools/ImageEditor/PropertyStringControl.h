a968b2d65532e0cb9781cfe5a1d990dc85f0e0c6
my_name <my.name.post@gmail.com>
2012-07-17 09:10:30 +0000

    add property

---
+++ Tools/ImageEditor/PropertyStringControl.h
@@ -0,0 +1,40 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		08/2010
+*/
+
+#ifndef _ac3769fd_b132_461a_b607_737e76f02a6e_
+#define _ac3769fd_b132_461a_b607_737e76f02a6e_
+
+#include "BaseLayout/BaseLayout.h"
+#include "PropertyControl.h"
+
+namespace tools
+{
+
+	class PropertyStringControl :
+		public PropertyControl
+	{
+	public:
+		PropertyStringControl();
+		virtual ~PropertyStringControl();
+
+	protected:
+		virtual void updateCaption();
+		virtual void updateProperty();
+		virtual void OnInitialise(Control* _parent, MyGUI::Widget* _place, const std::string& _layoutName);
+
+	private:
+		void notifyEditTextChange(MyGUI::EditBox* _sender);
+
+		MyGUI::UString getClearValue();
+
+	private:
+		MyGUI::TextBox* mName;
+		MyGUI::EditBox* mEdit;
+	};
+
+}
+
+#endif
