016bbf62fd78da91342dacb3e7380d3a4479df89
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-21 21:12:17 +0000

    update test

---
+++ trunk/Tools/ImageEditor/SkinTextureController.h
@@ -0,0 +1,47 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2012
+*/
+
+#ifndef _80163c7b_2ec7_42d3_b7e7_cd1a29d88865_
+#define _80163c7b_2ec7_42d3_b7e7_cd1a29d88865_
+
+#include "IControlController.h"
+#include "ScopeTextureControl.h"
+
+namespace tools
+{
+
+	class SkinTextureController :
+		public IControlController,
+		public sigslot::has_slots<>
+	{
+	public:
+		SkinTextureController();
+		virtual ~SkinTextureController();
+
+		virtual void setTarget(Control* _control);
+
+		virtual void activate();
+		virtual void deactivate();
+
+	private:
+		void notifyChangeDataSelector(Data* _data, bool _changeOnlySelection);
+		void notifyChangeProperty(Property* _sender);
+		void notifyChangeValue(const std::string& _value);
+		void notifyChangeScope(const std::string& _scope);
+
+		void updateCoords(const std::string& _value);
+
+	private:
+		ScopeTextureControl* mControl;
+		std::string mParentTypeName;
+		std::string mScopeName;
+		Data* mParentData;
+		bool mActivated;
+	};
+
+}
+
+#endif
