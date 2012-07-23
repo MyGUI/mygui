bff0f2f4d999e914f7784487d0176d91705a06a4
my_name <my.name.post@gmail.com>
2012-07-23 19:02:04 +0000

    upadte

--- Tools/ImageEditor/SkinExportSerializer.cpp
+++ Tools/ImageEditor/SkinExportSerializer.cpp
@@ -53,7 +53,6 @@ namespace tools
 		for (pugi::xpath_node_set::const_iterator node = nodes.begin(); node != nodes.end(); node ++)
 			parseSkin((*node).node());
 
-		//updateImageProperty(DataManager::getInstance().getRoot());
 		return true;
 	}
 
@@ -74,59 +73,7 @@ namespace tools
 		MyGUI::IntSize size = MyGUI::IntSize::parse(_node.attribute("size").value());
 
 		data->setPropertyValue("Size", MyGUI::IntCoord(point.left, point.top, size.width, size.height).print());
-
-		/*pugi::xpath_node_set nodes = _node.select_nodes("Group");
-		for (pugi::xpath_node_set::const_iterator node = nodes.begin(); node != nodes.end(); node ++)
-			parseGroup((*node).node(), data);*/
 	}
-
-	/*void SkinExportSerializer::parseGroup(pugi::xml_node _node, Data* _parent)
-	{
-		Data* data = new Data();
-		data->setType(DataTypeManager::getInstance().getType("Group"));
-		std::string value = _node.attribute("name").value();
-		if (value.empty())
-			value = "unnamed";
-		data->setPropertyValue("Name", value);
-		data->setPropertyValue("Texture", _node.attribute("texture").value());
-		data->setPropertyValue("Size", _node.attribute("size").value());
-
-		_parent->addChild(data);
-
-		pugi::xpath_node_set nodes = _node.select_nodes("Index");
-		for (pugi::xpath_node_set::const_iterator node = nodes.begin(); node != nodes.end(); node ++)
-			parseIndex((*node).node(), data);
-	}
-
-	void SkinExportSerializer::parseIndex(pugi::xml_node _node, Data* _parent)
-	{
-		Data* data = new Data();
-		data->setType(DataTypeManager::getInstance().getType("Index"));
-		std::string value = _node.attribute("name").value();
-		if (value.empty())
-			value = "unnamed";
-		data->setPropertyValue("Name", value);
-		data->setPropertyValue("Rate", _node.attribute("rate").value());
-
-		_parent->addChild(data);
-
-		pugi::xpath_node_set nodes = _node.select_nodes("Frame");
-		for (pugi::xpath_node_set::const_iterator node = nodes.begin(); node != nodes.end(); node ++)
-			parseFrame((*node).node(), data);
-	}
-
-	void SkinExportSerializer::parseFrame(pugi::xml_node _node, Data* _parent)
-	{
-		Data* data = new Data();
-		data->setType(DataTypeManager::getInstance().getType("Frame"));
-		data->setPropertyValue("Point", _node.attribute("point").value());
-		std::string value = _node.attribute("count").value();
-		if (value.empty())
-			value = "1";
-		data->setPropertyValue("Count", value);
-
-		_parent->addChild(data);
-	}*/
 
 	void SkinExportSerializer::writeSkin(pugi::xml_node _parent, Data* _data)
 	{
@@ -135,99 +82,7 @@ namespace tools
 		node.append_attribute("name").set_value(_data->getPropertyValue("Name").c_str());
 		node.append_attribute("texture").set_value(_data->getPropertyValue("Texture").c_str());
 		node.append_attribute("size").set_value(MyGUI::IntCoord::parse(_data->getPropertyValue("Size")).size().print().c_str());
-
-		/*for (Data::VectorData::const_iterator child = _data->getChilds().begin(); child != _data->getChilds().end(); child ++)
-			writeGroup(node, (*child));*/
 	}
-
-	/*void SkinExportSerializer::writeGroup(pugi::xml_node _parent, Data* _data)
-	{
-		pugi::xml_node node = _parent.append_child("Group");
-		node.append_attribute("name").set_value(_data->getPropertyValue("Name").c_str());
-		node.append_attribute("texture").set_value(_data->getPropertyValue("Texture").c_str());
-		node.append_attribute("size").set_value(MyGUI::IntCoord::parse(_data->getPropertyValue("Size")).size().print().c_str());
-
-		for (Data::VectorData::const_iterator child = _data->getChilds().begin(); child != _data->getChilds().end(); child ++)
-			writeIndex(node, (*child));
-	}
-
-	void SkinExportSerializer::writeIndex(pugi::xml_node _parent, Data* _data)
-	{
-		pugi::xml_node node = _parent.append_child("Index");
-		node.append_attribute("name").set_value(_data->getPropertyValue("Name").c_str());
-
-		std::string value = _data->getPropertyValue("Rate");
-		if (!value.empty())
-			node.append_attribute("rate").set_value(value.c_str());
-
-		for (Data::VectorData::const_iterator child = _data->getChilds().begin(); child != _data->getChilds().end(); child ++)
-			writeFrame(node, (*child));
-	}
-	
-	void SkinExportSerializer::writeFrame(pugi::xml_node _parent, Data* _data)
-	{
-		pugi::xml_node node = _parent.append_child("Frame");
-		node.append_attribute("point").set_value(_data->getPropertyValue("Point").c_str());
-
-		size_t count = MyGUI::utility::parseValue<size_t>(_data->getPropertyValue("Count"));
-		if (count > 1)
-			node.append_attribute("count").set_value(MyGUI::utility::toString(count).c_str());
-	}
-
-	void SkinExportSerializer::updateImageProperty(Data* _data)
-	{
-		const Data::VectorData& childs = _data->getChilds();
-		for (Data::VectorData::const_iterator child = childs.begin(); child != childs.end(); child++)
-		{
-			bool unique = PropertyUtility::isUniqueName((*child), "Name");
-			(*child)->setPropertyValue("UniqueName", unique);
-			updateGroupProperty(*child);
-		}
-	}
-
-	void SkinExportSerializer::updateGroupProperty(Data* _data)
-	{
-		const Data::VectorData& childs = _data->getChilds();
-		for (Data::VectorData::const_iterator child = childs.begin(); child != childs.end(); child++)
-		{
-			bool unique = PropertyUtility::isUniqueName((*child), "Name");
-			(*child)->setPropertyValue("UniqueName", unique);
-			updateIndexProperty(*child);
-		}
-	}
-
-	void SkinExportSerializer::updateIndexProperty(Data* _data)
-	{
-		const Data::VectorData& childs = _data->getChilds();
-		for (Data::VectorData::const_iterator child = childs.begin(); child != childs.end(); child++)
-		{
-			bool unique = PropertyUtility::isUniqueName((*child), "Name");
-			(*child)->setPropertyValue("UniqueName", unique);
-		}
-
-		MyGUI::IntPoint point = getFirstFramePoint(_data);
-		MyGUI::IntSize size = _data->getPropertyValue<MyGUI::IntSize>("Size");
-		MyGUI::IntCoord coord(point, size);
-		_data->setPropertyValue("Size", coord);
-	}
-
-	MyGUI::IntPoint SkinExportSerializer::getFirstFramePoint(Data* _data)
-	{
-		if (_data->getType()->getName() != "Group")
-			return MyGUI::IntPoint();
-
-		if (_data->getChilds().size() != 0)
-		{
-			Data* child = _data->getChildByIndex(0);
-			if (child->getChilds().size() != 0)
-			{
-				return child->getChildByIndex(0)->getPropertyValue<MyGUI::IntPoint>("Point");
-			}
-		}
-
-
-		return MyGUI::IntPoint();
-	}*/
 
 	void SkinExportSerializer::FillStateData(Data* _data, pugi::xml_node _node)
 	{
