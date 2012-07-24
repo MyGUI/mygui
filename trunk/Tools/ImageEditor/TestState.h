df2ba342b920d04ae31355a1989ab7018c96a2ae
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-07-24 19:11:18 +0000

    add test state

---
+++ trunk/Tools/ImageEditor/TestState.h
@@ -0,0 +1,38 @@
+/*!
+	@file
+	@author		Albert Semenov
+	@date		07/2012
+*/
+
+#ifndef _41f706df_2a78_46f6_a92a_c9e12a910964_
+#define _41f706df_2a78_46f6_a92a_c9e12a910964_
+
+#include "StateController.h"
+#include "sigslot.h"
+
+namespace tools
+{
+
+	class TestState :
+		public StateController,
+		public sigslot::has_slots<>
+	{
+	public:
+		TestState();
+		virtual ~TestState();
+
+		virtual void initState();
+		virtual void cleanupState();
+
+		virtual void pauseState();
+		virtual void resumeState();
+
+	private:
+		bool checkCommand();
+		void commandTest(const MyGUI::UString& _commandName, bool& _result);
+		void commandQuit(const MyGUI::UString& _commandName, bool& _result);
+	};
+
+}
+
+#endif
