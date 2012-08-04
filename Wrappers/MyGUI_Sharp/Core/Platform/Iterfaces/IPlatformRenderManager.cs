f4ebb7336b4271bc3830912503dd3364762da7f5
my_name <my.name.post@gmail.com>
2012-08-04 23:54:59 +0000

    add

---
+++ Wrappers/MyGUI_Sharp/Core/Platform/Iterfaces/IPlatformRenderManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGUI.Sharp
+{
+	public interface IPlatformRenderManager
+	{
+		void GetTextureSize(string _name, out int _width, out int _height);
+	}
+}
