54a799aa8b100d43e670bec7d4f2cc668def3b98
my_name <my.name.post@gmail.com>
2012-08-04 13:28:16 +0000

    update export

--- Wrappers/MyGUI_Export/MyGUI_ExportDataManager.cpp
+++ Wrappers/MyGUI_Export/MyGUI_ExportDataManager.cpp
@@ -7,6 +7,89 @@
 #include "MyGUI_ExportDataManager.h"
 #include "MyGUI_ExportDiagnostic.h"
 #include "MyGUI_DataFileStream.h"
+#include "ExportDefine.h"
+#include "ExportMarshaling.h"
+#include "MyGUI_DataMemoryStream.h"
+
+namespace Export
+{
+	namespace ScopeDataManager_IsDataExist
+	{
+		typedef Convert< bool >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< const std::string& >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateIsDataExist( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetDataPath
+	{
+		typedef Convert< const std::string& >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< const std::string& >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetDataPath( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetDataSize
+	{
+		typedef Convert< size_t >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< const std::string& >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetDataSize( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetData
+	{
+		typedef Convert< unsigned char* >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< const std::string& >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetData( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetDataListSize
+	{
+		typedef Convert< size_t >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< const std::string& >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetDataListSize( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetDataListItem
+	{
+		typedef Convert< const std::string& >::Type (MYGUICALLBACK *ExportHandle)(
+			Convert< size_t >::Type );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetDataListItem( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+	namespace ScopeDataManager_GetDataListComplete
+	{
+		typedef void (MYGUICALLBACK *ExportHandle)( );
+		ExportHandle mExportHandle = nullptr;
+		
+		MYGUIEXPORT void MYGUICALL ExportDataManager_DelegateGetDataListComplete( ExportHandle _delegate )
+		{
+			mExportHandle = _delegate;
+		}
+	}
+}
 
 namespace MyGUI
 {
@@ -31,28 +114,68 @@ namespace MyGUI
 
 	IDataStream* ExportDataManager::getData(const std::string& _name)
 	{
-		return nullptr;
+		size_t size = 0;
+		if (Export::ScopeDataManager_GetDataSize::mExportHandle != nullptr)
+			size = Export::Convert< size_t >::From ( Export::ScopeDataManager_GetDataSize::mExportHandle(Export::Convert< const std::string& >::To(_name)) );
+
+		if (size == 0)
+			return nullptr;
+
+		unsigned char* data = nullptr;
+		if (Export::ScopeDataManager_GetData::mExportHandle != nullptr)
+			data = Export::Convert< unsigned char* >::From ( Export::ScopeDataManager_GetData::mExportHandle(Export::Convert< const std::string& >::To(_name)) );
+
+		if (data == nullptr)
+			return nullptr;
+
+		MyGUI::DataMemoryStream* stream = new MyGUI::DataMemoryStream(data, size);
+		return stream;
 	}
 
 	void ExportDataManager::destroyData(IDataStream* _data)
 	{
-		delete _data;
+		if (_data == nullptr)
+			return;
+
+		MyGUI::DataMemoryStream* stream = static_cast<MyGUI::DataMemoryStream*>(_data);
+		delete stream;
 	}
 
 	bool ExportDataManager::isDataExist(const std::string& _name)
 	{
+		if (Export::ScopeDataManager_IsDataExist::mExportHandle != nullptr)
+			return Export::Convert< bool >::From ( Export::ScopeDataManager_IsDataExist::mExportHandle(Export::Convert< const std::string& >::To(_name)) );
 		return false;
 	}
 
 	const VectorString& ExportDataManager::getDataListNames(const std::string& _pattern)
 	{
 		static VectorString result;
+		result.clear();
+
+		size_t count = 0;
+		if (Export::ScopeDataManager_GetDataListSize::mExportHandle != nullptr)
+			count =  Export::Convert< size_t >::From ( Export::ScopeDataManager_GetDataListSize::mExportHandle(Export::Convert< const std::string& >::To(_pattern)) );
+
+		if (Export::ScopeDataManager_GetDataListItem::mExportHandle != nullptr)
+		{
+			for (size_t index = 0; index < count; index ++)
+				result.push_back(Export::Convert< const std::string& >::From ( Export::ScopeDataManager_GetDataListItem::mExportHandle(Export::Convert< size_t >::To(index)) ));
+		}
+
+		if (Export::ScopeDataManager_GetDataListComplete::mExportHandle != nullptr)
+			Export::ScopeDataManager_GetDataListComplete::mExportHandle();
+
 		return result;
 	}
 
 	const std::string& ExportDataManager::getDataPath(const std::string& _name)
 	{
 		static std::string result;
+
+		if (Export::ScopeDataManager_GetDataPath::mExportHandle != nullptr)
+			result = Export::Convert< const std::string& >::From ( Export::ScopeDataManager_GetDataPath::mExportHandle(Export::Convert< const std::string& >::To(_name)) );
+
 		return result;
 	}
 
