6e052e5e6717e1a8b1ff350a362e73c4593df390
my_name <my_name@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2012-08-05 08:53:22 +0000

    update

--- trunk/Wrappers/MyGUI_Sharp/Core/Platform/Iterfaces/IPlatformRenderManager.cs
+++ trunk/Wrappers/MyGUI_Sharp/Core/Platform/Iterfaces/IPlatformRenderManager.cs
@@ -7,5 +7,10 @@ namespace MyGUI.Sharp
 	public interface IPlatformRenderManager
 	{
 		void GetTextureSize(string _name, out int _width, out int _height);
+
+		void Lock(ref IntPtr _vertextes, uint _size);
+		void Unlock(uint _id);
+
+		void DoRender(uint _bufferId, string _texture, uint _count);
 	}
 }
