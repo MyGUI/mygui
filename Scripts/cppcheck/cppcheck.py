00b54a445a8a642302228ed8d6bc270ceab18002
George Evmenov <altren@gmail.com>
2012-11-04 13:02:26 +0000

    update cppcheck script

--- Scripts/cppcheck/cppcheck.py
+++ Scripts/cppcheck/cppcheck.py
@@ -6,7 +6,6 @@ ignoredContent = [
 	"Exception thrown in destructor.", # ignore, fine for us
 #	"::initialise' can be const.", # ignore, because some are not const and we keep all non-const for similarity
 #	"::shutdown' can be const.", # ignore, because some are not const and we keep all non-const for similarity
-#	"hides typedef with same name", # false positive
 	"(style) The function '", # ignore "Function is never used"
 	"MyGUI_UString", # ignore warnings from UString, because it wasn't written by the MyGUI developers
 	"pugixml", # ignore warnings from pugixml, because it wasn't written by the MyGUI developers
@@ -17,7 +16,8 @@ ignoredContent = [
 	"is assigned in constructor body. Consider performing initialization in initialization list.", # ignore, fine for us
 	"(performance) Prefer prefix ++/-- operators for non-primitive types", # ignore, fine for us - leave this optimisation to compiler
 	"MyGUI_DelegateImplement.h:195]: (warning) 'operator=' should check for assignment to self to avoid problems with dynamic memory.", # false positive
-	"(error) Reference to temporary returned." # only false positives (revert later)
+#	"(error) Reference to temporary returned." # only false positives (revert later)
+	"does not have a copy constructor which is recommended since the class contains a pointer to allocated memory." # ignore, not important
 ]
 
 def isIgnoredWarning(warning):
@@ -47,7 +47,7 @@ checkFolderSources('MyGUIEngine', '')
 checkFolderSources('Demos', '-I Common -I Common/Base/Ogre -I Common/Input/OIS -I Platforms/Ogre/OgrePlatform/include -I Plugins/Plugin_BerkeliumWidget')
 checkFolderSources('Tools', '-I Common -I Common/Base/Ogre -I Common/Input/OIS -I Platforms/Ogre/OgrePlatform/include -I Tools/EditorFramework')
 checkFolderSources('UnitTests/UnitTest_*', '-I Common -I Common/Base/Ogre -I Common/Input/OIS -I Platforms/Ogre/OgrePlatform/include')
-checkFolderSources('Common', '-I Common')
+checkFolderSources('Common', '-I Common -I Common/Input/OIS')
 checkFolderSources('Platforms/OpenGL/OpenGLPlatform/src', '-I Platforms/OpenGL/OpenGLPlatform/include -I Common')
 checkFolderSources('Platforms/Ogre/OgrePlatform/src', '-I Platforms/Ogre/OgrePlatform/include')
 checkFolderSources('Platforms/DirectX/DirectXPlatform/src', '-I Platforms/DirectX/DirectXPlatform/include -I Common')
