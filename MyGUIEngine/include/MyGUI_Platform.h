764568010f06d9945ef427b3436d1ff52300d929
George Evmenov <altren@gmail.com>
2013-03-03 17:35:06 +0000

    update license header (switching from LGPL to MIT license) update license docs page

--- MyGUIEngine/include/MyGUI_Platform.h
+++ MyGUIEngine/include/MyGUI_Platform.h
@@ -1,26 +1,8 @@
-/*!
-	@file
-	@author		Denis Koronchik
-	@author		Georgiy Evmenov
-	@author		Albert Semenov
-	@date		09/2007
-*/
 /*
-	This file is part of MyGUI.
-
-	MyGUI is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	MyGUI is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with MyGUI.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This source file is part of MyGUI. For the latest info, see http://mygui.info/
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
 
 #ifndef __MYGUI_PLATFORM_H__
 #define __MYGUI_PLATFORM_H__
--- MyGUIEngine/src/MyGUI_Precompiled.cpp
+++ MyGUIEngine/src/MyGUI_Precompiled.cpp
@@ -1,1 +1,7 @@
+/*
+ * This source file is part of MyGUI. For the latest info, see http://mygui.info/
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
 #include "MyGUI_Precompiled.h"
