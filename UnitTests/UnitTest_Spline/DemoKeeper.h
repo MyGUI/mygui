c08d453e9e8a19e7794290c46510efb1affb81b4
George Evmenov <altren@gmail.com>
2013-05-15 09:51:39 +0000

    make PolygonalSkin::setStroke value used as step (instead of step - 1) add stroke scroll into UnitTest_Spline

--- UnitTests/UnitTest_Spline/DemoKeeper.h
+++ UnitTests/UnitTest_Spline/DemoKeeper.h
@@ -28,12 +28,14 @@ namespace demo
 		void notifyPointMove(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id);
 		void notifyPointPressed(MyGUI::Widget* _sender, int _left, int _top, MyGUI::MouseButton _id);
 		void notifyChangeQuality(MyGUI::ScrollBar* _sender, size_t _position);
+		void notifyChangeStroke(MyGUI::ScrollBar* _sender, size_t _position);
 
 	private:
 		MyGUI::Widget* mClient;
 		std::vector<MyGUI::FloatPoint> mLinePoints;
 		MyGUI::PolygonalSkin* mPolygonalSkin;
 		MyGUI::TextBox* mQualityText;
+		MyGUI::TextBox* mStrokeText;
 		MyGUI::IntPoint mOffset;
 	};
 
