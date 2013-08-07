20a92ba8b5a917bf41447792fce357d7ffb5e18a
my_name <my.name.post@gmail.com>
2013-08-07 00:29:05 +0000

    refactoeing

--- Wrappers/MyGUI_Export/Generate/MyGUI_Export_MarshalingWidget.h
+++ Wrappers/MyGUI_Export/Generate/MyGUI_Export_MarshalingWidget.h
@@ -17,380 +17,254 @@ namespace Export
 
 	template <> struct Convert< MyGUI::MultiListItem* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::MultiListItem* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::MultiListItem* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::MultiListItem* From(Interface _value)
+		inline static MyGUI::MultiListItem* From(Type _value)
 		{
-			return static_cast<MyGUI::MultiListItem*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::MultiListItem*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::TabItem* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::TabItem* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::TabItem* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::TabItem* From(Interface _value)
+		inline static MyGUI::TabItem* From(Type _value)
 		{
-			return static_cast<MyGUI::TabItem*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::TabItem*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::MenuItem* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::MenuItem* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::MenuItem* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::MenuItem* From(Interface _value)
+		inline static MyGUI::MenuItem* From(Type _value)
 		{
-			return static_cast<MyGUI::MenuItem*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::MenuItem*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::Window* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::Window* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::Window* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::Window* From(Interface _value)
+		inline static MyGUI::Window* From(Type _value)
 		{
-			return static_cast<MyGUI::Window*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::Window*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ScrollBar* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ScrollBar* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ScrollBar* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ScrollBar* From(Interface _value)
+		inline static MyGUI::ScrollBar* From(Type _value)
 		{
-			return static_cast<MyGUI::ScrollBar*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ScrollBar*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::TabControl* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::TabControl* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::TabControl* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::TabControl* From(Interface _value)
+		inline static MyGUI::TabControl* From(Type _value)
 		{
-			return static_cast<MyGUI::TabControl*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::TabControl*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::TextBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::TextBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::TextBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::TextBox* From(Interface _value)
+		inline static MyGUI::TextBox* From(Type _value)
 		{
-			return static_cast<MyGUI::TextBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::TextBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ImageBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ImageBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ImageBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ImageBox* From(Interface _value)
+		inline static MyGUI::ImageBox* From(Type _value)
 		{
-			return static_cast<MyGUI::ImageBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ImageBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ScrollView* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ScrollView* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ScrollView* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ScrollView* From(Interface _value)
+		inline static MyGUI::ScrollView* From(Type _value)
 		{
-			return static_cast<MyGUI::ScrollView*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ScrollView*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ProgressBar* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ProgressBar* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ProgressBar* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ProgressBar* From(Interface _value)
+		inline static MyGUI::ProgressBar* From(Type _value)
 		{
-			return static_cast<MyGUI::ProgressBar*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ProgressBar*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::PopupMenu* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::PopupMenu* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::PopupMenu* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::PopupMenu* From(Interface _value)
+		inline static MyGUI::PopupMenu* From(Type _value)
 		{
-			return static_cast<MyGUI::PopupMenu*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::PopupMenu*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::MultiListBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::MultiListBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::MultiListBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::MultiListBox* From(Interface _value)
+		inline static MyGUI::MultiListBox* From(Type _value)
 		{
-			return static_cast<MyGUI::MultiListBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::MultiListBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::MenuControl* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::MenuControl* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::MenuControl* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::MenuControl* From(Interface _value)
+		inline static MyGUI::MenuControl* From(Type _value)
 		{
-			return static_cast<MyGUI::MenuControl*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::MenuControl*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::MenuBar* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::MenuBar* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::MenuBar* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::MenuBar* From(Interface _value)
+		inline static MyGUI::MenuBar* From(Type _value)
 		{
-			return static_cast<MyGUI::MenuBar*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::MenuBar*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ListBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ListBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ListBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ListBox* From(Interface _value)
+		inline static MyGUI::ListBox* From(Type _value)
 		{
-			return static_cast<MyGUI::ListBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ListBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ItemBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ItemBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ItemBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ItemBox* From(Interface _value)
+		inline static MyGUI::ItemBox* From(Type _value)
 		{
-			return static_cast<MyGUI::ItemBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ItemBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::EditBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::EditBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::EditBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::EditBox* From(Interface _value)
+		inline static MyGUI::EditBox* From(Type _value)
 		{
-			return static_cast<MyGUI::EditBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::EditBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::DDContainer* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::DDContainer* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::DDContainer* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::DDContainer* From(Interface _value)
+		inline static MyGUI::DDContainer* From(Type _value)
 		{
-			return static_cast<MyGUI::DDContainer*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::DDContainer*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::ComboBox* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::ComboBox* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::ComboBox* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::ComboBox* From(Interface _value)
+		inline static MyGUI::ComboBox* From(Type _value)
 		{
-			return static_cast<MyGUI::ComboBox*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::ComboBox*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::Button* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::Button* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::Button* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::Button* From(Interface _value)
+		inline static MyGUI::Button* From(Type _value)
 		{
-			return static_cast<MyGUI::Button*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::Button*>( _value );
 		}
 	};
 	template <> struct Convert< MyGUI::Widget* >
 	{
-		typedef Interface Type;
-		inline static Interface To(MyGUI::Widget* _value)
+		typedef MyGUI::Widget* Type;
+		inline static Type To(MyGUI::Widget* _value)
 		{
-			if (_value == nullptr) return nullptr;
-			Interface* wrapper = _value->getUserData<Interface>(false);
-			if (wrapper == nullptr)
-			{
-				return CreateWrapper(_value);
-			}
-			return *wrapper;
+			return _value;
 		}
-		inline static MyGUI::Widget* From(Interface _value)
+		inline static MyGUI::Widget* From(Type _value)
 		{
-			return static_cast<MyGUI::Widget*>( GetNativeByWrapper(_value) );
+			return static_cast<MyGUI::Widget*>( _value );
 		}
 	};
 } // namespace Export
