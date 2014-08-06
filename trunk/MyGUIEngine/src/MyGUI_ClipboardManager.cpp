61dbd8a939e4fb7ff2421f96ebe820d871e72dd4
altren <altren@10a74664-c72d-0410-8fa9-ef67a5ae4784>
2014-08-06 16:17:52 +0000

    clipboard changer and requested events make default windows clipboard handler use this events and be optional

--- trunk/MyGUIEngine/src/MyGUI_ClipboardManager.cpp
+++ trunk/MyGUIEngine/src/MyGUI_ClipboardManager.cpp
@@ -8,60 +8,20 @@
 #include "MyGUI_ClipboardManager.h"
 #include "MyGUI_Gui.h"
 #include "MyGUI_TextIterator.h"
-
 #if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-#include <windows.h>
+#include "MyGUI_WindowsClipboardHandler.h"
 #endif
 
 namespace MyGUI
 {
 
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-
-	HWND g_hWnd = NULL;
-
-	BOOL CALLBACK EnumWindowProc(HWND hWnd, LPARAM lParam)
-	{
-		DWORD dwProcessID = 0;
-		GetWindowThreadProcessId(hWnd, &dwProcessID);
-
-		if (dwProcessID != (DWORD)lParam)
-			return TRUE;
-
-		if (GetParent(hWnd) == NULL)
-		{
-			// Нашли. hWnd - то что надо
-			g_hWnd = hWnd;
-			return FALSE;
-		}
-
-		return TRUE;
-	}
-
-	BOOL CALLBACK EnumChildWindowProc(HWND hWnd, LPARAM lParam)
-	{
-		DWORD dwProcessID = 0;
-		GetWindowThreadProcessId(hWnd, &dwProcessID);
-
-		if (dwProcessID != GetCurrentProcessId())
-			return TRUE;
-
-		if (GetWindowLongPtr(hWnd, GWLP_HINSTANCE) == lParam)
-		{
-			// Нашли. hWnd - то что надо
-			g_hWnd = hWnd;
-			return FALSE;
-		}
-
-		return TRUE;
-	}
-
-#endif
-
 	template <> ClipboardManager* Singleton<ClipboardManager>::msInstance = nullptr;
 	template <> const char* Singleton<ClipboardManager>::mClassTypeName = "ClipboardManager";
 
 	ClipboardManager::ClipboardManager() :
+#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
+		mWindowsClipboardHandler(nullptr),
+#endif
 		mIsInitialise(false)
 	{
 	}
@@ -72,15 +32,8 @@ namespace MyGUI
 		MYGUI_LOG(Info, "* Initialise: " << getClassTypeName());
 
 #if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-		// берем имя нашего экзешника
-		char buf[MAX_PATH];
-		GetModuleFileName(0, (LPCH)&buf, MAX_PATH);
-		// берем инстанс нашего модуля
-		HINSTANCE instance = GetModuleHandle(buf);
-
-		EnumChildWindows(GetDesktopWindow(), (WNDENUMPROC)EnumWindowProc, (LPARAM)instance);
-		mHwnd = (size_t)g_hWnd;
-
+		mWindowsClipboardHandler = new WindowsClipboardHandler();
+		mWindowsClipboardHandler->initialise();
 #endif
 
 		MYGUI_LOG(Info, getClassTypeName() << " successfully initialized");
@@ -92,6 +45,12 @@ namespace MyGUI
 		MYGUI_ASSERT(mIsInitialise, getClassTypeName() << " is not initialised");
 		MYGUI_LOG(Info, "* Shutdown: " << getClassTypeName());
 
+#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
+		mWindowsClipboardHandler->shutdown();
+		delete mWindowsClipboardHandler;
+		mWindowsClipboardHandler = nullptr;
+#endif
+
 		MYGUI_LOG(Info, getClassTypeName() << " successfully shutdown");
 		mIsInitialise = false;
 	}
@@ -100,27 +59,7 @@ namespace MyGUI
 	{
 		mClipboardData[_type] = _data;
 
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-		if (_type == "Text")
-		{
-			mPutTextInClipboard = TextIterator::getOnlyText(UString(_data));
-			size_t size = (mPutTextInClipboard.size() + 1) * 2;
-			//открываем буфер обмена
-			if (OpenClipboard((HWND)mHwnd))
-			{
-				EmptyClipboard(); //очищаем буфер
-				HGLOBAL hgBuffer = GlobalAlloc(GMEM_DDESHARE, size);//выделяем память
-				wchar_t* chBuffer = hgBuffer ? (wchar_t*)GlobalLock(hgBuffer) : NULL;
-				if (chBuffer)
-				{
-					memcpy(chBuffer, mPutTextInClipboard.asWStr_c_str(), size);
-					GlobalUnlock(hgBuffer);//разблокируем память
-					SetClipboardData(CF_UNICODETEXT, hgBuffer);//помещаем текст в буфер обмена
-				}
-				CloseClipboard(); //закрываем буфер обмена
-			}
-		}
-#endif
+		eventClipboardChanged(_type, _data);
 	}
 
 	void ClipboardManager::clearClipboardData(const std::string& _type)
@@ -131,39 +70,14 @@ namespace MyGUI
 
 	std::string ClipboardManager::getClipboardData(const std::string& _type)
 	{
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-		if (_type == "Text")
-		{
-			UString buff;
-			//открываем буфер обмена
-			if (OpenClipboard((HWND)mHwnd))
-			{
-				HANDLE hData = GetClipboardData(CF_UNICODETEXT);//извлекаем текст из буфера обмена
-				wchar_t* chBuffer = hData ? (wchar_t*)GlobalLock(hData) : NULL;
-				if (chBuffer)
-				{
-					buff = chBuffer;
-					GlobalUnlock(hData);//разблокируем память
-				}
-				CloseClipboard();//закрываем буфер обмена
-			}
-			// если в буфере не то что мы ложили, то берем из буфера
-			if (mPutTextInClipboard != buff)
-			{
-				// вставляем теги, если нуно
-				const UString& text = TextIterator::toTagsString(buff);
-				return text.asUTF8();
-			}
-
-			MapString::iterator iter = mClipboardData.find(_type);
-			if (iter != mClipboardData.end()) return (*iter).second;
-			return "";
-		}
-#endif
-
+		std::string ret;
 		MapString::iterator iter = mClipboardData.find(_type);
-		if (iter != mClipboardData.end()) return (*iter).second;
-		return "";
+		if (iter != mClipboardData.end())
+			ret = (*iter).second;
+
+		// Give delegates a chance to fill the clipboard with data
+		eventClipboardRequested(_type, ret);
+		return ret;
 	}
 
 } // namespace MyGUI
