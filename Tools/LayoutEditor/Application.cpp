a3abaf2d77d2decba6d070b6e216692cf9b24306
George Evmenov <altren@gmail.com>
2014-09-25 03:13:45 +0400

    load additional resources after plugins
    (makes it possible to use types from plugins in resources)

--- Tools/LayoutEditor/Application.cpp
+++ Tools/LayoutEditor/Application.cpp
@@ -80,8 +80,6 @@ namespace tools
 		new HotKeyManager();
 		HotKeyManager::getInstance().initialise();
 
-		LoadGuiSettings();
-
 		std::string language = SettingsManager::getInstance().getValue("Settings/InterfaceLanguage");
 		if (language.empty() || language == "Auto")
 		{
@@ -142,6 +140,8 @@ namespace tools
 		GridManager::getInstance().initialise();
 
 		MyGUI::ResourceManager::getInstance().load("Initialise.xml");
+
+		LoadGuiSettings();
 
 		bool maximized = SettingsManager::getInstance().getValue<bool>("Controls/Main/Maximized");
 		setWindowMaximized(maximized);
