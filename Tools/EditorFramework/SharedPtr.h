f4f1017ad15713675ba4b9c7f969530083a38179
scrawl <scrawl@baseoftrash.de>
2015-06-18 19:25:07 +0200

    Rename nil() method to fix compile issue on mac, added shared_ptr operator=(T*) to fix nullptr issue

--- Tools/EditorFramework/SharedPtr.h
+++ Tools/EditorFramework/SharedPtr.h
@@ -18,12 +18,22 @@ namespace tools
 		friend class weak_ptr<T>;
 
 	public:
-		shared_ptr() : ptr(0), count(nil()) { incref(); }
+		shared_ptr() : ptr(0), count(create_nil_counter()) { incref(); }
 		~shared_ptr() { decref(); }
 
 		shared_ptr(const shared_ptr<T>& o) : ptr(o.ptr), count(o.count) { incref(); }
 		shared_ptr(T* p) : ptr(p), count(new unsigned(1)) { }
 		explicit shared_ptr(const weak_ptr<T>& w) : ptr(w.ptr), count(w.count) { incref(); }
+
+		shared_ptr<T>& operator=(T* o)
+		{
+			if (ptr == o)
+				return *this;
+			decref();
+			ptr = o;
+			incref();
+			return *this;
+		}
 
 		shared_ptr<T>& operator=(const shared_ptr<T>& o)
 		{
@@ -44,6 +54,7 @@ namespace tools
 		const T* operator->() const { return ptr; }
 		const T& operator*() const { return *ptr; }
 
+		// in C++11, we can't implicitly convert nullptr -> shared_ptr, so need to define pointer comparison operators
 		bool operator==(const T* o) const { return ptr == o; }
 		bool operator!=(const T* o) const { return ptr != o; }
 		bool operator<(const T* o) const { return ptr < o; }
@@ -56,7 +67,7 @@ namespace tools
 
 	private:
 		/* special case, null pointer (nil-code) */
-		static unsigned* nil() { static unsigned nil_counter(1); return &nil_counter; }
+		static unsigned* create_nil_counter() { static unsigned nil_counter(1); return &nil_counter; }
 
 		void decref() { if (--(*count) == 0) { delete ptr; delete count; } }
 		void incref() { ++(*count); }
@@ -72,7 +83,7 @@ namespace tools
 		friend class shared_ptr<T>;
 
 	public:
-		weak_ptr() : ptr(0), count(shared_ptr<T>::nil()) { }
+		weak_ptr() : ptr(0), count(shared_ptr<T>::create_nil_counter()) { }
 		explicit weak_ptr( const shared_ptr<T>& s) : ptr(s.ptr), count(s.count) { }
 
 		shared_ptr<T> lock() const { return shared_ptr<T>(*this); }
