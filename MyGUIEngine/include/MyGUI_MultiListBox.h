8c1dc6ad5a4d529f5d115582c5cd8329db0e0a85
acme_pjz <acme_pjz@hotmail.com>
2017-10-06 01:11:49 +0800

    added another slightly advanced compare function for MultiListBox

--- MyGUIEngine/include/MyGUI_MultiListBox.h
+++ MyGUIEngine/include/MyGUI_MultiListBox.h
@@ -24,6 +24,7 @@ namespace MyGUI
 	typedef delegates::CMultiDelegate5<Widget*, size_t, const UString&, const UString&, bool&> EventHandle_WidgetIntUTFStringUTFStringBool;
 
 	typedef delegates::CDelegate5<MultiListBox*, size_t, const UString&, const UString&, bool&> EventHandle_MultiListPtrSizeTCUTFStringRefCUTFStringRefBoolRef;
+	typedef delegates::CDelegate5<MultiListBox*, size_t, size_t, size_t, bool&> EventHandle_MultiListPtrSizeTSizeTSizeTBoolRef;
 	typedef delegates::CMultiDelegate2<MultiListBox*, size_t> EventHandle_MultiListPtrSizeT;
 
 	/** \brief @wpage{MultiListBox}
@@ -286,6 +287,16 @@ namespace MyGUI
 			@param _less Comparsion result (write your value here)
 		*/
 		EventHandle_MultiListPtrSizeTCUTFStringRefCUTFStringRefBoolRef requestOperatorLess;
+
+		/** Event : Less than operator for sort multilist by columns. This has higher priority than \ref requestOperatorLess.\n
+		signature : void method(MyGUI::MultiListBox* _sender, size_t _column, size_t _index1, size_t _index2, bool& _less)\n
+		@param _sender widget that called this event
+		@param _column Index of column
+		@param _index1 Index of row for compare
+		@param _index2 Index of row for compare
+		@param _less Comparsion result (write your value here)
+		*/
+		EventHandle_MultiListPtrSizeTSizeTSizeTBoolRef requestOperatorLess2;
 
 		/*internal:*/
 		// IItemContainer impl
