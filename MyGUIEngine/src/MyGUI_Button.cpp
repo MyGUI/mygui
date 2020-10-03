46467a58c95c6399589f5131545d59ddfd379aee
Kapil_Kumar <58102926+codewithkapil@users.noreply.github.com>
2020-10-03 14:01:57 +0530

    Update MyGUI_Button.cpp

--- MyGUIEngine/src/MyGUI_Button.cpp
+++ MyGUIEngine/src/MyGUI_Button.cpp
@@ -1,5 +1,5 @@
 /*
- * This source file is part of MyGUI. For the latest info, see http://mygui.info/
+ * This source file is part of MyGUI. For the latest info about this project, see http://mygui.info/
  * Distributed under the MIT License
  * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
  */
