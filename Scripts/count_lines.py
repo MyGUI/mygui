ca54e3107e844ca8134b2a6e66ab195397c65ddd
George Evmenov <altren@gmail.com>
2023-05-22 02:26:08 +0300

    update Scripts to python3

--- Scripts/count_lines.py
+++ Scripts/count_lines.py
@@ -1,28 +1,30 @@
-#!/usr/bin/env python
-# python 2.6 required (for os.path.relpath)
+#!/usr/bin/env python3
 
-import os, filecmp
+import os
+
 
 def countLines(fileName):
-  file = open(fileName, 'r')
-  count = 0
-  while file.readline() != "":
-    count = count + 1
-  file.close ()
-  return count
+    file = open(fileName, 'r')
+    count = 0
+    while file.readline() != "":
+        count = count + 1
+    file.close()
+    return count
+
 
 def CountLinesInFolder(dir_src):
-  numLines = 0
+    numLines = 0
 
-  for root, dirs, files in os.walk(dir_src):
-    for name in files:
-      if name.endswith('.h') or name.endswith('.cpp'):
-          f_src = os.path.join(root, name)
-          f_src = f_src.replace('\\','/')
-          numLines = numLines + countLines(f_src)
+    for root, dirs, files in os.walk(dir_src):
+        for name in files:
+            if name.endswith('.h') or name.endswith('.cpp'):
+                f_src = os.path.join(root, name)
+                f_src = f_src.replace('\\', '/')
+                numLines = numLines + countLines(f_src)
 
-  print "" + dir_src + " : " + str(numLines)
-  return numLines
+    print(dir_src + " : " + str(numLines))
+    return numLines
+
 
 # ----------
 
@@ -33,12 +35,11 @@ i += CountLinesInFolder('../Tools')
 i += CountLinesInFolder('../UnitTests')
 i += CountLinesInFolder('../Common')
 i += CountLinesInFolder('../Platforms/OpenGL')
+i += CountLinesInFolder('../Platforms/OpenGLES')
 i += CountLinesInFolder('../Platforms/OGRE')
 i += CountLinesInFolder('../Platforms/DirectX')
 i += CountLinesInFolder('../Platforms/DirectX11')
 i += CountLinesInFolder('../Platforms/OpenGL3')
 i += CountLinesInFolder('../Plugins')
 i += CountLinesInFolder('../Wrappers')
-print "Total: "+ str(i)
-
-raw_input("Press enter to continue.")
+print("Total: " + str(i))
--- Scripts/cppcheck/cppcheck.py
+++ Scripts/cppcheck/cppcheck.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python3
 
-# run from root sources directory: python Scripts/cppcheck/cppcheck.py
+# run from root sources directory: python3 Scripts/cppcheck/cppcheck.py
 import os
-
-# ignoredContent = [
-# 	"is not safe, destructor throws exception",
-# #	"::initialise' can be const.", # ignore, because some are not const and we keep all non-const for similarity
-# #	"::shutdown' can be const.", # ignore, because some are not const and we keep all non-const for similarity
-# 	"(style) The function '", # "Function is never used". False positive for library project
-# 	"MyGUI_UString", # warnings from UString, gnore because it wasn't written by the MyGUI developers
-# 	#"pugixml", # warnings from pugixml, ignore because it wasn't written by the MyGUI developers
-# 	#"sigslot", # warnings from sigslot, ignore because it wasn't written by the MyGUI developers
-# #	"Technically the member function 'input::", # useless, because with other input system this function could be non-const
-# #	"Cppcheck cannot find all the include files",
-# 	"is assigned in constructor body. Consider performing initialization in initialization list.", # sometimes it makes sense, when non-trivial logic involved
-# 	"(performance) Prefer prefix ++/-- operators for non-primitive types", # fine for us - leave this optimisation to compiler
-# #	"(error) Reference to temporary returned." # only false positives (revert later)
-# 	#"does not have a copy constructor which is recommended since the class contains a pointer to allocated memory." # ignore, not important
-# ]
 
 
 def printOutput():
-	file = open("temp.cppcheck", 'r')
+    file = open("temp.cppcheck", 'r')
 
-	line = file.readline()
-	while line != "":
-		line = line[0:len(line)-1]
-		print(line)
-		line = file.readline()
-	file.close ()
+    line = file.readline()
+    while line != "":
+        line = line[0:len(line) - 1]
+        print(line)
+        line = file.readline()
+    file.close()
 
-def checkFolderSources(folder) :
-	os.system("cppcheck -j 8 --suppressions-list=Scripts/cppcheck/suppressions.txt --enable=all --project=\"" + folder + "/compile_commands.json\"" + " 2>temp.cppcheck")
-	printOutput()
 
-checkFolderSources('build')
+def checkFolderSources(folder):
+    os.system(
+        "cppcheck -j 8 --suppressions-list=Scripts/cppcheck/suppressions.txt --enable=all --std=c++17 --quiet --project=\"" + folder + "/compile_commands.json\"")
+    printOutput()
+
+
+checkFolderSources('cmake-build-debug')
