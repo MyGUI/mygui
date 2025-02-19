7a3866c740d5988f108ab6fefe5155e30d9e0dda
elsid <elsid.mail@gmail.com>
2025-02-19 22:35:58 +0100

    Fix build with libc++
    std::char_traits support for non char types was removed from libc++19:
    https://reviews.llvm.org/D157058.

--- MyGUIEngine/include/MyGUI_UString.h
+++ MyGUIEngine/include/MyGUI_UString.h
@@ -163,10 +163,10 @@ namespace MyGUI
 		static const size_type npos = std::numeric_limits<size_t>::max();
 
 		//! a single 32-bit Unicode character
-		using unicode_char = uint32;
+		using unicode_char = char32_t;
 
 		//! a single UTF-16 code point
-		using code_point = uint16;
+		using code_point = char16_t;
 
 		//! value type typedef for use in iterators
 		using value_type = code_point;
