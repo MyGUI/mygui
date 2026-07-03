d020f6d8eec37aff3e169f18e060712239f68235
George Evmenov <altren@gmail.com>
2026-07-04 01:43:54 +0300

    reorganize, cleanup and split root CMakeLists.txt

---
+++ CMake/CompilerWarnings.cmake
@@ -0,0 +1,90 @@
+#######################################################################
+# Compiler warning flags for GCC, Clang, Intel, and MSVC
+#######################################################################
+
+# Prevent CMake from adding "#pragma clang system_header" to the precompiled headers
+# (otherwise all warnings from PCH-included headers are suppressed)
+set(CMAKE_PCH_PROLOGUE "")
+
+if (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+	if (MYGUI_HIGH_LEVEL_WARNINGS)
+		add_compile_options(-Werror)
+		if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+			add_compile_options(
+				-Weverything
+			)
+			# might be useful
+			add_compile_options(
+				-Wno-reorder
+				-Wno-nrvo
+			)
+			# not useful
+			add_compile_options(
+				-Wno-unsafe-buffer-usage
+				-Wno-unknown-warning-option
+				-Wno-documentation
+				-Wno-documentation-unknown-command
+				-Wno-conversion
+				-Wno-old-style-cast
+				-Wno-poison-system-directories
+				-Wno-c++98-compat-pedantic
+				-Wno-c++11-extensions
+				-Wno-float-equal
+				-Wno-padded
+				-Wno-weak-vtables
+				-Wno-duplicate-enum
+				-Wno-exit-time-destructors
+				-Wno-unused-parameter
+				-Wno-global-constructors
+				-Wno-switch-default # conflicts with Wcovered-switch-default
+				-Wno-lifetime-safety-intra-tu-suggestions
+				-Wno-lifetime-safety-cross-tu-suggestions
+			)
+		else ()
+			add_compile_options(
+				-Wall
+				-Winit-self
+				-Wcast-qual
+				-Wwrite-strings
+				-Wextra
+				-pedantic
+				-Wshadow
+			)
+			add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-Wno-reorder;-Woverloaded-virtual;-Wsuggest-override>")
+			if (NOT CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+				add_compile_options(-fdiagnostics-show-option)
+			endif ()
+			# disable some
+			add_compile_options(
+				-Wno-unused-parameter
+			)
+		endif ()
+	endif ()
+endif ()
+
+if (MSVC)
+	add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
+
+	# Use the highest warning level for visual studio.
+	if (MYGUI_HIGH_LEVEL_WARNINGS)
+		set(WARNING_LEVEL "/W4")
+		add_compile_options("/WX")
+	else ()
+		set(WARNING_LEVEL "/W3")
+	endif ()
+	# disable: warning C4100: '***' : unreferenced formal parameter
+	add_compile_options(/wd4100)
+
+	# TODO: move to Tools only
+	# disable: warning C4275: non dll-interface class '***' used as base for dll-interface clas '***'
+	add_compile_options(/wd4275)
+	# warning C4267: '=': conversion from '***' to '***', possible loss of data
+	add_compile_options(/wd4267)
+	# warning C4244: 'argument': conversion from '***' to '***', possible loss of data
+	add_compile_options(/wd4244)
+
+	# Remove any default warning level from CMAKE_CXX_FLAGS and use add_compile_options instead
+	string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+	add_compile_options(${WARNING_LEVEL})
+
+endif ()
---
+++ CMake/Sanitizers.cmake
@@ -0,0 +1,24 @@
+#######################################################################
+# Sanitizer options (Clang only)
+#######################################################################
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+	# usage:
+	# UBSAN_OPTIONS=print_stacktrace=1,suppressions=sanitizers/UBSanitizer.supp <command>
+	option(MYGUI_UB_SANITIZER "Compile with undefined behaviour sanitizer" FALSE)
+	if (MYGUI_UB_SANITIZER)
+		add_compile_options("-fno-omit-frame-pointer" "-fno-optimize-sibling-calls")
+		add_compile_options("-fsanitize=undefined" "-fno-sanitize-recover=undefined")
+		add_link_options("-fsanitize=undefined")
+	endif()
+
+	# usage:
+	# ASAN_OPTIONS=alloc_dealloc_mismatch=0,check_initialization_order=1,detect_stack_use_after_return=1 LSAN_OPTIONS=suppressions=sanitizers/LeaksSanitizer.supp <command>
+	# alloc_dealloc_mismatch is here due to issues with Ogre and no clear way to suppress the issue
+	option(MYGUI_ADDRESS_SANITIZER "Compile with address sanitizer" FALSE)
+	if (MYGUI_ADDRESS_SANITIZER)
+		add_compile_options("-fno-omit-frame-pointer" "-fno-optimize-sibling-calls")
+		add_compile_options("-fsanitize=address" "-fsanitize-address-use-after-scope")
+		add_link_options("-fsanitize=address")
+	endif()
+endif ()
