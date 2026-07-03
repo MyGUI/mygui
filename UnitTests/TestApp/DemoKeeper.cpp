3f330188bbfe4c6bc61932af1a8de9411e6b1509
George Evmenov <altren@gmail.com>
2026-07-03 18:25:32 +0300

    remove draft code from TestApp, use it as a minimal working test for builds

--- UnitTests/TestApp/DemoKeeper.cpp
+++ UnitTests/TestApp/DemoKeeper.cpp
@@ -1,96 +1,30 @@
 #include "Precompiled.h"
 #include "DemoKeeper.h"
 #include "Base/Main.h"
-#include "DataInfoManager.h"
-#include "DataManager.h"
-#include "ActionManager.h"
-#include "sigslot.h"
 
 namespace demo
 {
-	class A
-	{
-	public:
-		sigslot::signal1<A*> event;
-	};
-
-	class B : public sigslot::has_slots<>
-	{
-	public:
-		void advise(A* _a)
-		{
-			if (!_a->event.exist(this, &B::onEvent))
-				_a->event.connect(this, &B::onEvent);
-		}
-
-		void onEvent(A* _sender)
-		{
-		}
-	};
-
-	DemoKeeper::DemoKeeper() :
-		mDataListUI(nullptr)
-	{
-	}
 
 	void DemoKeeper::setupResources()
 	{
 		base::BaseManager::setupResources();
 		addResourceLocation(getRootMedia() + "/UnitTests/TestApp");
-		addResourceLocation(getRootMedia() + "/UnitTests/TestApp/DataInfo");
+		addResourceLocation(getRootMedia() + "/Common/Demos");
 	}
 
 	void DemoKeeper::createScene()
 	{
 		base::BaseDemoManager::createScene();
-		new tools::DataInfoManager();
-		new tools::DataManager();
-		new tools::ActionManager();
+		MyGUI::LayoutManager::getInstance().loadLayout("Wallpaper.layout");
+		const MyGUI::VectorWidgetPtr& root = MyGUI::LayoutManager::getInstance().loadLayout("HelpPanel.layout");
+		if (root.size() == 1)
+			root.at(0)->findWidget("Text")->castType<MyGUI::TextBox>()->setCaption("Sample of widgets.");
 
-		tools::DataInfoManager::getInstance().initialise();
-		tools::DataManager::getInstance().initialise();
-		tools::ActionManager::getInstance().initialise();
-
-		tools::DataInfoManager::getInstance().load("ImageDataInfo.xml");
-
-		mDataListUI = new DataListUI();
-
-		A a;
-		B b;
-
-		b.advise(&a);
-		b.advise(&a);
-
-
-		MyGUI::IndexImage index;
-		index.name = "MyIndex1";
-		index.rate = 0;
-		index.frames.push_back(MyGUI::IntPoint(0, 0));
-
-		MyGUI::GroupImage group;
-		group.name = "MyGroup1";
-		group.texture = "MyTexture1.png";
-		group.size = MyGUI::texture_utility::getTextureSize(group.texture, false);
-		group.indexes.push_back(index);
-
-		const std::string& category = MyGUI::ResourceManager::getInstance().getCategoryName();
-		MyGUI::ResourceImageSet* imageSet =
-			MyGUI::FactoryManager::getInstance().createObject<MyGUI::ResourceImageSet>(category);
-		imageSet->setResourceName("ResourceImageSet_Manual1");
-		imageSet->AddGroupImage(group);
-
-		MyGUI::ResourceManager::getInstance().addResource(imageSet);
+		MyGUI::LayoutManager::getInstance().loadLayout("TestApp.layout");
 	}
 
 	void DemoKeeper::destroyScene()
 	{
-		tools::DataInfoManager::getInstance().shutdown();
-		tools::DataManager::getInstance().shutdown();
-		tools::ActionManager::getInstance().shutdown();
-
-		delete tools::DataManager::getInstancePtr();
-		delete tools::DataInfoManager::getInstancePtr();
-		delete tools::ActionManager::getInstancePtr();
 	}
 
 } // namespace demo
