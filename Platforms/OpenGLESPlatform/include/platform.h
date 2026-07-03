0593a4372c1667e5745fc9d10b626c81e028102f
George Evmenov <altren@gmail.com>
2026-07-03 16:24:38 +0300

    fix reserved identifier warning

--- Platforms/OpenGLESPlatform/include/platform.h
+++ Platforms/OpenGLESPlatform/include/platform.h
@@ -2,12 +2,12 @@
 #define CHECK_GL_ERROR_DEBUG() \
 	do \
 	{ \
-		GLenum __error = glGetError(); \
-		if (__error) \
+		GLenum _error = glGetError(); \
+		if (_error) \
 		{ \
 			MYGUI_PLATFORM_LOG( \
 				Info, \
-				"OpenGLES error 0x%04X in " << __error << " " << __FILE__ << " " << __FUNCTION__ << " " << __LINE__); \
+				"OpenGLES error 0x%04X in " << _error << " " << __FILE__ << " " << __FUNCTION__ << " " << __LINE__); \
 		} \
 	} while (false)
 #endif
