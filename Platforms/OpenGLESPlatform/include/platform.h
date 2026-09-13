2ee7f35f6768b0df57a2f91df281c73540acfbdd
George Evmenov <altren@gmail.com>
2026-09-13 04:28:31 +0300

    format code

--- Platforms/OpenGLESPlatform/include/platform.h
+++ Platforms/OpenGLESPlatform/include/platform.h
@@ -1,13 +1,14 @@
 #ifndef CHECK_GL_ERROR_DEBUG //()
-#define CHECK_GL_ERROR_DEBUG() \
-	do \
-	{ \
-		GLenum _error = glGetError(); \
-		if (_error) \
+	#define CHECK_GL_ERROR_DEBUG() \
+		do \
 		{ \
-			MYGUI_PLATFORM_LOG( \
-				Info, \
-				"OpenGLES error 0x%04X in " << _error << " " << __FILE__ << " " << __FUNCTION__ << " " << __LINE__); \
-		} \
-	} while (false)
+			GLenum _error = glGetError(); \
+			if (_error) \
+			{ \
+				MYGUI_PLATFORM_LOG( \
+					Info, \
+					"OpenGLES error 0x%04X in " \
+						<< _error << " " << __FILE__ << " " << __FUNCTION__ << " " << __LINE__); \
+			} \
+		} while (false)
 #endif
